64d8738cc2cd6dbbfb8ce1019ce28b4527de38ad
Richard North <rich.north@gmail.com>
2015-08-08 21:41:54 +0900

    WIP: add a few comments

--- modules/docker-compose/src/main/java/org/testcontainers/containers/DockerComposeContainer.java
+++ modules/docker-compose/src/main/java/org/testcontainers/containers/DockerComposeContainer.java
@@ -24,11 +24,16 @@ public class DockerComposeContainer extends GenericContainer {
     public DockerComposeContainer(File composeFile, String command) {
         super("dduportal/docker-compose:1.3.1");
 
+        // Create a unique identifier and tell compose
         identifier = Base58.randomString(6).toLowerCase();
         addEnv("COMPOSE_PROJECT_NAME", identifier);
 
+        // Map the docker compose file into the container
         addEnv("COMPOSE_FILE", "/compose/" + composeFile.getAbsoluteFile().getName());
         addFileSystemBind(composeFile.getAbsoluteFile().getParentFile().getAbsolutePath(),"/compose", READ_ONLY);
+
+        // Ensure that compose can access docker. Since the container is assumed to be running on the same machine
+        //  as the docker daemon, just mapping the docker control socket is OK.
         addFileSystemBind("/var/run/docker.sock","/var/run/docker.sock", READ_WRITE);
         setCommand(command);
     }
@@ -37,6 +42,9 @@ public class DockerComposeContainer extends GenericContainer {
     public void stop() {
         super.stop();
 
+        /**
+         * Kill all service containers that were launched by compose
+         */
         try {
             List<Container> containers = dockerClient.listContainers(DockerClient.ListContainersParam.allContainers());
 
--- modules/docker-compose/src/main/java/org/testcontainers/junit/DockerComposeContainerRule.java
+++ modules/docker-compose/src/main/java/org/testcontainers/junit/DockerComposeContainerRule.java
@@ -12,18 +12,17 @@ import java.util.Map;
  */
 public class DockerComposeContainerRule extends GenericContainerRule {
 
-    private final File composeFile;
     private Map<String, GenericContainer> ambassadorContainers = new HashMap<>();
 
     public DockerComposeContainerRule(File composeFile) {
         super(new DockerComposeContainer(composeFile, "up"));
-        this.composeFile = composeFile;
     }
 
     @Override
     protected void before() throws Throwable {
         super.before();
 
+        // Start any ambassador containers we need
         for (GenericContainer ambassadorContainer : ambassadorContainers.values()) {
             ambassadorContainer.start();
         }
@@ -33,40 +32,68 @@ public class DockerComposeContainerRule extends GenericContainerRule {
     protected void after() {
         super.after();
 
+        // Kill all the ambassador containers
         for (GenericContainer ambassadorContainer : ambassadorContainers.values()) {
             ambassadorContainer.stop();
         }
-//        new DockerComposeContainer(composeFile, "kill").start();
-//        new DockerComposeContainer(composeFile, "rm").start();
     }
 
     @Override
     public DockerComposeContainerRule withExposedPorts(int... ports) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("Use withExposedService instead");
     }
 
     @Override
     public DockerComposeContainerRule withExposedPorts(String... ports) {
-         throw new UnsupportedOperationException();
+         throw new UnsupportedOperationException("Use withExposedService instead");
     }
 
     public DockerComposeContainerRule withExposedService(String serviceName, String servicePort) {
 
-        String identifier = ((DockerComposeContainer) container).getIdentifier();
+        /**
+         * For every service/port pair that needs to be exposed, we have to start an 'ambassador container'.
+         *
+         * The ambassador container's role is to link (within the Docker network) to one of the
+         * compose services, and proxy TCP network I/O out to a port that the ambassador container
+         * exposes.
+         *
+         * This avoids the need for the docker compose file to explicitly expose ports on all the
+         * services.
+         */
+
 
         GenericContainer ambassadorContainer = new GenericContainer("richnorth/ambassador:latest");
+
+        /**
+         * Use the unique 'identifier' (random compose project name) so that the ambassador can see
+         * the container it's supposed to be proxying.
+         */
+        String identifier = ((DockerComposeContainer) container).getIdentifier();
         String otherContainerName = identifier + "_" + serviceName;
 
+        // Link
         ambassadorContainer.addLink(otherContainerName, otherContainerName);
+
+        // Expose ambassador's port
         ambassadorContainer.addExposedPort(servicePort);
+
+        // Tell the proxy what to connect to within the docker network
         ambassadorContainer.addEnv("SERVICE_NAME", otherContainerName);
         ambassadorContainer.addEnv("SERVICE_PORT", servicePort);
 
+        // Ambassador containers will all be started together after docker compose has started
         ambassadorContainers.put(serviceName + ":" + servicePort, ambassadorContainer);
 
         return this;
     }
 
+    /**
+     * Get the host (e.g. IP address or hostname) that the service can be found at, from the host machine
+     * (i.e. should be the machine that's running this Java process)
+     * @param serviceName
+     * @param servicePort
+     * @return
+     */
     public String getServiceHost(String serviceName, String servicePort) {
         return ambassadorContainers.get(serviceName + ":" + servicePort).getIpAddress();
     }
