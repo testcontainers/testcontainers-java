1f7eaf0a1fcaac588f9a263d348733ca0dd96955
Richard North <rich.north@gmail.com>
2016-02-07 18:45:36 +0000

    Add an ignored test for future help in diagnosis of #51

---
+++ modules/selenium/src/test/java/org/testcontainers/junit/FlakyContainerCreationTest.java
@@ -0,0 +1,31 @@
+package org.testcontainers.junit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+import java.io.File;
+
+/**
+ * Created by rnorth on 30/01/2016.
+ */
+public class FlakyContainerCreationTest {
+
+    @Test @Ignore
+    public void testCreationOfManyContainers() {
+        for (int i = 0; i < 50; i++) {
+            BrowserWebDriverContainer container = new BrowserWebDriverContainer()
+                    .withDesiredCapabilities(DesiredCapabilities.chrome())
+                    .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING, new File("target"));
+
+            container.start();
+            RemoteWebDriver driver = container.getWebDriver();
+
+            driver.get("http://www.google.com");
+
+            container.stop();
+        }
+    }
+}
