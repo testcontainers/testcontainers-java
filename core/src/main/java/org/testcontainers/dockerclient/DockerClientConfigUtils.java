701035074f9eb1b137b4e37d1d504779e1ac322f
Matthieu Baechler <matthieu.baechler@gmail.com>
2016-02-26 12:37:23 +0100

    Issue-82 handle unix socket case in DockerClientFactory.instance().dockerHostIpAddress()

--- core/src/main/java/org/testcontainers/dockerclient/DockerClientConfigUtils.java
+++ core/src/main/java/org/testcontainers/dockerclient/DockerClientConfigUtils.java
@@ -4,6 +4,15 @@ import com.github.dockerjava.core.DockerClientConfig;
 
 public class DockerClientConfigUtils {
     public static String getDockerHostIpAddress(DockerClientConfig config) {
-        return config.getUri().getHost();
+        switch (config.getUri().getScheme()) {
+        case "http":
+        case "https":
+        case "tcp":
+            return config.getUri().getHost();
+        case "unix":
+            return "localhost";
+        default:
+            return null;
+        }
     }
 }
