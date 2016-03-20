735623aa714eaf9d6e99908f61bacbf7c86d5288
Richard North <rich.north@gmail.com>
2016-03-20 19:47:48 +0000

    Refs #90: Proposed solution for environments where container host cannot be determined automatically and needs to be overidden by environment variable/system property

--- core/src/main/java/org/testcontainers/dockerclient/DockerClientConfigUtils.java
+++ core/src/main/java/org/testcontainers/dockerclient/DockerClientConfigUtils.java
@@ -4,6 +4,12 @@ import com.github.dockerjava.core.DockerClientConfig;
 
 public class DockerClientConfigUtils {
     public static String getDockerHostIpAddress(DockerClientConfig config) {
+
+        String overrideContainerHost = System.getProperty("testcontainers.container.host", System.getenv("TESTCONTAINERS_CONTAINER_HOST"));
+        if (overrideContainerHost != null) {
+            return overrideContainerHost;
+        }
+
         switch (config.getUri().getScheme()) {
         case "http":
         case "https":
