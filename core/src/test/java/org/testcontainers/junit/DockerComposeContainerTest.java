f336800f4b2e600d9fdf67288cf59135ec65deaa
Richard North <rich.north@gmail.com>
2016-06-10 13:48:00 +0900

    Switch Docker Compose tests to use Jedis rather than Redisson to avoid Netty dependencies clash

--- core/src/test/java/org/testcontainers/junit/DockerComposeContainerTest.java
+++ core/src/test/java/org/testcontainers/junit/DockerComposeContainerTest.java
@@ -1,9 +1,13 @@
 package org.testcontainers.junit;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.testcontainers.containers.DockerComposeContainer;
+import redis.clients.jedis.Jedis;
 
 import java.io.File;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
 /**
  * Created by rnorth on 08/08/2015.
@@ -17,35 +21,30 @@ public class DockerComposeContainerTest {
             .withExposedService("redis_1", REDIS_PORT)
             .withExposedService("db_1", 3306)
             ;
-//
-//    @Test
-//    public void simpleTest() {
-//        Config config = new Config();
-//        config.useSingleServer().setAddress(environment.getServiceHost("redis_1", REDIS_PORT) + ":" + environment.getServicePort("redis_1", REDIS_PORT));
-//        Redisson redisson = Redisson.create(config);
-//
-//        RAtomicLong test = redisson.getAtomicLong("test");
-//        test.incrementAndGet();
-//        test.incrementAndGet();
-//        test.incrementAndGet();
-//
-//        assertEquals("A redis instance defined in compose can be used in isolation", 3, (int) test.get());
-//    }
-//
-//    @Test
-//    public void secondTest() {
-//        // used in manual checking for cleanup in between tests
-//        Config config = new Config();
-//        config.useSingleServer().setAddress(environment.getServiceHost("redis_1", REDIS_PORT) + ":" + environment.getServicePort("redis_1", REDIS_PORT));
-//        Redisson redisson = Redisson.create(config);
-//
-//        RAtomicLong test = redisson.getAtomicLong("test");
-//        test.incrementAndGet();
-//        test.incrementAndGet();
-//        test.incrementAndGet();
-//
-//        assertEquals("Tests use fresh container instances", 3, (int) test.get());
-//        // if these end up using the same container one of the test methods will fail.
-//        // However, @Rule creates a separate DockerComposeContainer instance per test, so this just shouldn't happen
-//    }
+
+    @Test
+    public void simpleTest() {
+
+        Jedis jedis = new Jedis(environment.getServiceHost("redis_1", REDIS_PORT), environment.getServicePort("redis_1", REDIS_PORT));
+
+        jedis.incr("test");
+        jedis.incr("test");
+        jedis.incr("test");
+
+        assertEquals("A redis instance defined in compose can be used in isolation", 3, Integer.valueOf(jedis.get("test")));
+    }
+
+    @Test
+    public void secondTest() {
+        // used in manual checking for cleanup in between tests
+        Jedis jedis = new Jedis(environment.getServiceHost("redis_1", REDIS_PORT), environment.getServicePort("redis_1", REDIS_PORT));
+
+        jedis.incr("test");
+        jedis.incr("test");
+        jedis.incr("test");
+
+        assertEquals("Tests use fresh container instances", 3, Integer.valueOf(jedis.get("test")));
+        // if these end up using the same container one of the test methods will fail.
+        // However, @Rule creates a separate DockerComposeContainer instance per test, so this just shouldn't happen
+    }
 }
