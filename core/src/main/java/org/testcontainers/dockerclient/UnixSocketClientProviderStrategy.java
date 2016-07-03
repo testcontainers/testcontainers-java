c48f7aefc2d4a8e60872dca65ba4ed8ae9819b3d
Richard North <rich.north@gmail.com>
2016-07-03 14:00:04 +0100

    Relax unix socket validation code by using bitmasking rather than a specific expected file mode

--- core/src/main/java/org/testcontainers/dockerclient/UnixSocketClientProviderStrategy.java
+++ core/src/main/java/org/testcontainers/dockerclient/UnixSocketClientProviderStrategy.java
@@ -13,7 +13,7 @@ public class UnixSocketClientProviderStrategy extends DockerClientProviderStrate
 
     protected static final String DOCKER_SOCK_PATH = "/var/run/docker.sock";
     private static final String SOCKET_LOCATION = "unix://" + DOCKER_SOCK_PATH;
-    private static final int EXPECTED_UNIX_SOCKET_MODE = 0140755;
+    private static final int SOCKET_FILE_MODE_MASK = 0xc000;
 
     @Override
     public void test()
@@ -42,7 +42,7 @@ public class UnixSocketClientProviderStrategy extends DockerClientProviderStrate
             throw new InvalidConfigurationException("Could not find unix domain socket", e);
         }
 
-        if (mode != EXPECTED_UNIX_SOCKET_MODE) { // simple comparison; could be improved with bitmasking
+        if ((mode & 0xc000) != SOCKET_FILE_MODE_MASK) {
             throw new InvalidConfigurationException("Found docker unix domain socket but file mode was not as expected (expected: srwxr-xr-x). This problem is possibly due to occurrence of this issue in the past: https://github.com/docker/docker/issues/13121");
         }
 
