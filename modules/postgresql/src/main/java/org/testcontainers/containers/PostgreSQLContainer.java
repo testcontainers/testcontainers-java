2a58f75ff3821210926c824df5d3ea26b05f5e1f
Barlog <barlog@tanelorn.li>
2016-09-09 18:16:24 +0300

    More customizable postgres container

--- modules/postgresql/src/main/java/org/testcontainers/containers/PostgreSQLContainer.java
+++ modules/postgresql/src/main/java/org/testcontainers/containers/PostgreSQLContainer.java
@@ -4,31 +4,47 @@ package org.testcontainers.containers;
  * @author richardnorth
  */
 public class PostgreSQLContainer<SELF extends PostgreSQLContainer<SELF>> extends JdbcDatabaseContainer<SELF> {
-
     public static final String NAME = "postgresql";
     public static final String IMAGE = "postgres";
-    public static final Integer POSTGRESQL_PORT = 5432;
+    public static final Integer DEFAULT_PORT = 5432;
+    final String databaseName;
+    final String username;
+    final String password;
 
     public PostgreSQLContainer() {
-        super(IMAGE + ":latest");
+        this(IMAGE + ":latest");
     }
 
-    public PostgreSQLContainer(String dockerImageName) {
+    public PostgreSQLContainer(final String dockerImageName) {
         super(dockerImageName);
+        this.databaseName = "test";
+        this.username = "test";
+        this.password = "test";
+    }
+
+    public PostgreSQLContainer(
+        final String dockerImageName,
+        final String databaseName,
+        final String username,
+        final String password
+    ) {
+        super(dockerImageName);
+        this.databaseName = databaseName;
+        this.username = username;
+        this.password = password;
     }
 
     @Override
     protected Integer getLivenessCheckPort() {
-        return getMappedPort(POSTGRESQL_PORT);
+        return getMappedPort(DEFAULT_PORT);
     }
 
     @Override
     protected void configure() {
-
-        addExposedPort(POSTGRESQL_PORT);
-        addEnv("POSTGRES_DATABASE", "test");
-        addEnv("POSTGRES_USER", "test");
-        addEnv("POSTGRES_PASSWORD", "test");
+        addExposedPort(DEFAULT_PORT);
+        addEnv("POSTGRES_DB", databaseName);
+        addEnv("POSTGRES_USER", username);
+        addEnv("POSTGRES_PASSWORD", password);
         setCommand("postgres");
     }
 
@@ -39,17 +55,17 @@ public class PostgreSQLContainer<SELF extends PostgreSQLContainer<SELF>> extends
 
     @Override
     public String getJdbcUrl() {
-        return "jdbc:postgresql://" + getContainerIpAddress() + ":" + getMappedPort(POSTGRESQL_PORT) + "/test";
+        return "jdbc:postgresql://" + this.getContainerIpAddress() + ":" + this.getMappedPort(DEFAULT_PORT) + "/" + databaseName;
     }
 
     @Override
     public String getUsername() {
-        return "test";
+        return username;
     }
 
     @Override
     public String getPassword() {
-        return "test";
+        return password;
     }
 
     @Override
