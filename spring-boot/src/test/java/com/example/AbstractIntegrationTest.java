269c58ca5108e0d779af0968c59899d9167b04d7
Sergei Egorov <bsideup@gmail.com>
2016-09-09 14:31:56 +0300

    Simplify Spring Boot example

--- spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
+++ spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
@@ -2,42 +2,30 @@ package com.example;
 
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.EnvironmentTestUtils;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.mock.env.MockPropertySource;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.containers.GenericContainer;
 
-import javax.annotation.PostConstruct;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(DemoApplicationTest.TestConfiguration.class)
+@SpringApplicationConfiguration(value = DemoApplication.class, initializers = AbstractIntegrationTest.Initializer.class)
 @WebIntegrationTest(randomPort = true)
 public abstract class AbstractIntegrationTest {
 
     @ClassRule
     public static GenericContainer redis = new GenericContainer("redis:3.0.6").withExposedPorts(6379);
 
-    @Configuration
-    @Import(DemoApplication.class)
-    static class TestConfiguration {
-
-        @Autowired
-        ConfigurableEnvironment environment;
-
-        @PostConstruct
-        public void init() {
-            MockPropertySource propertySource = new MockPropertySource("TestContainers");
-
-            propertySource.setProperty("spring.redis.host", redis.getContainerIpAddress());
-            propertySource.setProperty("spring.redis.port", redis.getMappedPort(6379));
-
-            environment.getPropertySources().addFirst(propertySource);
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            EnvironmentTestUtils.addEnvironment("testcontainers", configurableApplicationContext.getEnvironment(),
+                    "spring.redis.host=" + redis.getContainerIpAddress(),
+                    "spring.redis.port=" + redis.getMappedPort(6379)
+            );
         }
     }
 
