8453fcd72d617733fc52a4d255fc1c7ab48f8dce
Richard North <rich.north@gmail.com>
2016-10-16 22:17:42 +0100

    Relocate MariaDB tests to jdbc-test module

--- modules/jdbc-test/src/test/java/org/testcontainers/jdbc/JDBCDriverTest.java
+++ modules/jdbc-test/src/test/java/org/testcontainers/jdbc/JDBCDriverTest.java
@@ -27,6 +27,55 @@ public class JDBCDriverTest {
     }
 
     @Test
+    public void testMySQLWithVersion() throws SQLException {
+        performSimpleTest("jdbc:tc:mysql:5.5.43://hostname/databasename");
+    }
+
+    @Test
+    public void testMySQLWithNoSpecifiedVersion() throws SQLException {
+        performSimpleTest("jdbc:tc:mysql://hostname/databasename");
+    }
+
+    @Test
+    public void testMySQLWithCustomIniFile() throws SQLException {
+        assumeFalse(SystemUtils.IS_OS_WINDOWS);
+        HikariDataSource ds = getDataSource("jdbc:tc:mysql:5.6://hostname/databasename?TC_MY_CNF=somepath/mysql_conf_override", 1);
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute("SELECT @@GLOBAL.innodb_file_format");
+        ResultSet resultSet = statement.getResultSet();
+
+        resultSet.next();
+        String result = resultSet.getString(1);
+
+        assertEquals("The InnoDB file format has been set by the ini file content", "Barracuda", result);
+    }
+
+    @Test
+    public void testMySQLWithClasspathInitScript() throws SQLException {
+        performSimpleTest("jdbc:tc:mysql://hostname/databasename?TC_INITSCRIPT=somepath/init_mysql.sql");
+
+        performTestForScriptedSchema("jdbc:tc:mysql://hostname/databasename?TC_INITSCRIPT=somepath/init_mysql.sql");
+    }
+
+    @Test
+    public void testMySQLWithClasspathInitFunction() throws SQLException {
+        performSimpleTest("jdbc:tc:mysql://hostname/databasename?TC_INITFUNCTION=org.testcontainers.jdbc.JDBCDriverTest::sampleInitFunction");
+
+        performTestForScriptedSchema("jdbc:tc:mysql://hostname/databasename?TC_INITFUNCTION=org.testcontainers.jdbc.JDBCDriverTest::sampleInitFunction");
+    }
+
+    @Test
+    public void testMySQLWithQueryParams() throws SQLException {
+        performSimpleTestWithCharacterSet("jdbc:tc:mysql://hostname/databasename?useUnicode=yes&characterEncoding=utf8");
+    }
+
+    @Test
+    public void testPostgreSQLWithNoSpecifiedVersion() throws SQLException {
+        performSimpleTest("jdbc:tc:postgresql://hostname/databasename");
+    }
+
+
+    @Test
     public void testMariaDBWithVersion() throws SQLException {
         performSimpleTest("jdbc:tc:mariadb:10.1.16://hostname/databasename");
     }
