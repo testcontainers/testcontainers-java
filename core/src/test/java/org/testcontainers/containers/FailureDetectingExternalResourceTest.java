ab56a277460486bf0bfac25daed7beb05984296f
tbcs <tbcs@users.noreply.github.com>
2017-05-29 13:25:57 +0200

    Cleanup if start failed (#336)
    Closes #335
    
    * prevent potential NPE in DockerComposeContainer
    
    * add regression test for #335
    
    * ensure cleanup if starting fails in FailureDetectingExternalResource (#335)

---
+++ core/src/test/java/org/testcontainers/containers/FailureDetectingExternalResourceTest.java
@@ -0,0 +1,28 @@
+package org.testcontainers.containers;
+
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+public class FailureDetectingExternalResourceTest {
+
+    @Test
+    public void finishedIsCalledForCleanupIfStartingThrows() throws Throwable {
+        FailureDetectingExternalResource res = spy(FailureDetectingExternalResource.class);
+        Statement stmt = res.apply(mock(Statement.class), Description.EMPTY);
+        doThrow(new RuntimeException()).when(res).starting(any());
+        try {
+            stmt.evaluate();
+        } catch (Throwable t) {
+            // ignore
+        }
+        verify(res).starting(any());
+        verify(res).finished(any());
+    }
+
+}
