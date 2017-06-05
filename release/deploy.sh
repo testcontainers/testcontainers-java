1ae18311514e4cb3845577aed1243d66cfa44e7e
Richard North <rich.north@gmail.com>
2017-06-05 11:56:44 +0100

    Remove jdbc-test module from profile used during deploy

--- release/deploy.sh
+++ release/deploy.sh
@@ -4,4 +4,4 @@ openssl aes-256-cbc -K $encrypted_291cb5a97669_key -iv $encrypted_291cb5a97669_i
 gpg --fast-import release/codesigning.asc
 
 mvn versions:set -DnewVersion=$TRAVIS_TAG
-mvn deploy -P sign,build-extras -DskipTests --settings release/settings.xml
+mvn deploy -P sign,build-extras,!test-only -DskipTests --settings release/settings.xml
