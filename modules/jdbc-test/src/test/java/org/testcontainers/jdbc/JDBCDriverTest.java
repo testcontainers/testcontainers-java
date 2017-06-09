86070d3fe724556c25f60cb34380b85ce94a83c0
Kevin Wittek <kevin.wittek@styracosoft.de>
2017-06-09 23:26:48 +0200

    Extract MariaDB into a separate repository. See #337

--- modules/jdbc-test/src/test/java/org/testcontainers/jdbc/JDBCDriverTest.java
+++ modules/jdbc-test/src/test/java/org/testcontainers/jdbc/JDBCDriverTest.java
@@ -42,13 +42,7 @@ public class JDBCDriverTest {
                         {"jdbc:tc:mysql://hostname/databasename?useUnicode=yes&characterEncoding=utf8", false, true, false},
                         {"jdbc:tc:mysql://hostname/databasename", false, false, false},
                         {"jdbc:tc:postgresql://hostname/databasename", false, false, false},
-                        {"jdbc:tc:mariadb:10.1.16://hostname/databasename", false, false, false},
-                        {"jdbc:tc:mariadb://hostname/databasename", false, false, false},
-                        {"jdbc:tc:mariadb://hostname/databasename?useUnicode=yes&characterEncoding=utf8", false, true, false},
-                        {"jdbc:tc:mariadb://hostname/databasename?TC_INITSCRIPT=somepath/init_mariadb.sql", true, false, false},
-                        {"jdbc:tc:mariadb://hostname/databasename?TC_INITFUNCTION=org.testcontainers.jdbc.JDBCDriverTest::sampleInitFunction", true, false, false},
                         {"jdbc:tc:mysql:5.6://hostname/databasename?TC_MY_CNF=somepath/mysql_conf_override", false, false, true},
-                        {"jdbc:tc:mariadb:10.1.16://hostname/databasename?TC_MY_CNF=somepath/mariadb_conf_override", false, false, true}
                 });
     }
 
