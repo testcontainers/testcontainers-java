a087e047a2c94f414f58a5d3c053179471a16b70
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2017-06-11 11:58:16 +0300

    Allow to start daemon database container (#360)
    * Allow to start daemon database container
    * Add documentation for TC_DAEMON parameter
    * Add tests for TC_DAEMON parameter
    * Update changelog

---
+++ modules/jdbc-test/src/test/java/org/testcontainers/jdbc/DatabaseDriverShutdownTest.java
@@ -0,0 +1,50 @@
+package org.testcontainers.jdbc;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertNotNull;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertNull;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
+
+/**
+ * Created by inikolaev on 08/06/2017.
+ */
+public class DatabaseDriverShutdownTest {
+    @AfterClass
+    public static void testCleanup() {
+        ContainerDatabaseDriver.killContainers();
+    }
+
+    @Test
+    public void shouldStopContainerWhenAllConnectionsClosed() throws SQLException {
+        final String jdbcUrl = "jdbc:tc:postgresql://hostname/databasename";
+
+        getConnectionAndClose(jdbcUrl);
+
+        JdbcDatabaseContainer<?> container = ContainerDatabaseDriver.getContainer(jdbcUrl);
+        assertNull("Database container instance is null as expected", container);
+    }
+
+    @Test
+    public void shouldNotStopDaemonContainerWhenAllConnectionsClosed() throws SQLException {
+        final String jdbcUrl = "jdbc:tc:postgresql://hostname/databasename?TC_DAEMON=true";
+
+        getConnectionAndClose(jdbcUrl);
+
+        JdbcDatabaseContainer<?> container = ContainerDatabaseDriver.getContainer(jdbcUrl);
+        assertNotNull("Database container instance is not null as expected", container);
+        assertTrue("Database container is running as expected", container.isRunning());
+    }
+
+    private void getConnectionAndClose(String jdbcUrl) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
+            assertNotNull("Obtained connection as expected", connection);
+        }
+    }
+}
