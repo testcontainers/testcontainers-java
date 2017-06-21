dd43736579e3b0f76aedf0f39fb39d69917015f8
Richard North <rich.north@gmail.com>
2017-06-21 06:43:12 +0100

    Add additional guards for cases where AuditLogger params may be null (#370)
    Also add @Nullable/@NotNull annotations to enable detection of bugs in future

--- core/src/main/java/org/testcontainers/utility/AuditLogger.java
+++ core/src/main/java/org/testcontainers/utility/AuditLogger.java
@@ -6,10 +6,13 @@ import com.github.dockerjava.api.command.DockerCmd;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.MDC;
 
 import java.util.List;
+
+import static com.google.common.base.Strings.nullToEmpty;
 
 /**
  * Logger for tracking potentially destructive actions, intended for usage in a shared Docker environment where
@@ -25,14 +28,28 @@ public class AuditLogger {
     private static final ObjectMapper objectMapper = new ObjectMapper();
     public static final String MDC_PREFIX = AuditLogger.class.getCanonicalName();
 
-    public static void doLog(String action, String image, String containerId, DockerCmd<?> cmd) {
+    public static void doLog(@NotNull String action,
+                             @Nullable String image,
+                             @Nullable String containerId,
+                             @NotNull DockerCmd<?> cmd) {
+
         doLog(action, image, containerId, cmd, null);
     }
 
-    public static void doLog(String action, String image, String containerId, DockerCmd<?> cmd, @Nullable Exception e) {
-        MDC.put(MDC_PREFIX + ".Action", action);
-        MDC.put(MDC_PREFIX + ".Image", image);
-        MDC.put(MDC_PREFIX + ".ContainerId", containerId);
+    public static void doLog(@NotNull String action,
+                             @Nullable String image,
+                             @Nullable String containerId,
+                             @NotNull DockerCmd<?> cmd,
+                             @Nullable Exception e) {
+
+        if (! log.isTraceEnabled()) {
+            return;
+        }
+
+        MDC.put(MDC_PREFIX + ".Action", nullToEmpty(action));
+        MDC.put(MDC_PREFIX + ".Image", nullToEmpty(image));
+        MDC.put(MDC_PREFIX + ".ContainerId", nullToEmpty(containerId));
+
         try {
             MDC.put(MDC_PREFIX + ".Command", objectMapper.writeValueAsString(cmd));
         } catch (JsonProcessingException ignored) {
@@ -52,11 +69,21 @@ public class AuditLogger {
         MDC.remove(MDC_PREFIX + ".Exception");
     }
 
-    public static void doComposeLog(String[] commandParts, List<String> env) {
+    public static void doComposeLog(@NotNull String[] commandParts,
+                                    @Nullable List<String> env) {
+
+        if (! log.isTraceEnabled()) {
+            return;
+        }
+
         MDC.put(MDC_PREFIX + ".Action", "COMPOSE");
+
+        if (env != null) {
+            MDC.put(MDC_PREFIX + ".Compose.Env", env.toString());
+        }
+
         final String command = StringUtils.join(commandParts, ' ');
         MDC.put(MDC_PREFIX + ".Compose.Command", command);
-        MDC.put(MDC_PREFIX + ".Compose.Env", env.toString());
 
         log.trace("COMPOSE action with command: {}, env: {}", command, env);
 
