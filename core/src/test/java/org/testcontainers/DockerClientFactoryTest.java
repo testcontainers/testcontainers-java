c1427e4c86f3e8b6bbad208485e134b751ac5d1a
Sergei Egorov <bsideup@gmail.com>
2017-06-27 21:58:05 +0200

    Fix number overflow in the disk space detection. Closes #379 (#380)
    * Fix number overflow in the disk space detection. Closes #379

--- core/src/test/java/org/testcontainers/DockerClientFactoryTest.java
+++ core/src/test/java/org/testcontainers/DockerClientFactoryTest.java
@@ -1,6 +1,8 @@
 package org.testcontainers;
 
 import org.junit.Test;
+import org.rnorth.visibleassertions.VisibleAssertions;
+import org.testcontainers.DockerClientFactory.DiskSpaceUsage;
 import org.testcontainers.dockerclient.LogToStringContainerCallback;
 import org.testcontainers.utility.TestcontainersConfiguration;
 
@@ -27,4 +29,13 @@ public class DockerClientFactoryTest {
                 .toString()
     );
   }
+
+    @Test
+    public void shouldHandleBigDiskSize() throws Exception {
+        String dfOutput = "/dev/disk1     2982480572 1491240286 2982480572    31%    /";
+        DiskSpaceUsage usage = DockerClientFactory.instance().parseAvailableDiskSpace(dfOutput);
+
+        VisibleAssertions.assertEquals("Available MB is correct", 2982480572L / 1024L, usage.availableMB.orElse(0L));
+        VisibleAssertions.assertEquals("Available percentage is correct", 31, usage.usedPercent.orElse(0));
+    }
 }
