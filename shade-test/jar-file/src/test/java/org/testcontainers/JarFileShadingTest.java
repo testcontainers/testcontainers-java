13e3dda5e8f716fc9c348c975ac8c06c7b77c5ac
bsideup <bsideup@gmail.com>
2017-07-02 14:25:16 +0200

    fix test

--- shade-test/jar-file/src/test/java/org/testcontainers/JarFileShadingTest.java
+++ shade-test/jar-file/src/test/java/org/testcontainers/JarFileShadingTest.java
@@ -33,12 +33,22 @@ public class JarFileShadingTest {
     @Test
     public void testPackages() throws Exception {
         assertThatFileList(root).containsOnly(
+                "docker-java.properties",
                 "org/",
-                "META-INF/"
+                "META-INF/",
+                "com/"
         );
 
         assertThatFileList(root.resolve("org")).containsOnly(
                 "testcontainers/"
+        );
+
+        assertThatFileList(root.resolve("com")).containsOnly(
+                "github/"
+        );
+
+        assertThatFileList(root.resolve("com").resolve("github")).containsOnly(
+                "dockerjava/"
         );
     }
 
