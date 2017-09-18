7cf6763e11e6219c123368a44be95477c399c4f0
Nick Babcock <nbabcock19@hotmail.com>
2017-09-18 16:09:15 -0500

    Jdbc containers with user, passwd, and db customizations (#400)

--- modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainer.java
+++ modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainer.java
@@ -8,7 +8,10 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
     public static final String NAME = "mysql";
     public static final String IMAGE = "mysql";
     private static final String MY_CNF_CONFIG_OVERRIDE_PARAM_NAME = "TC_MY_CNF";
-    private static final Integer MYSQL_PORT = 3306;
+    public static final Integer MYSQL_PORT = 3306;
+    private String databaseName = "test";
+    private String username = "test";
+    private String password = "test";
 
     public MySQLContainer() {
         super(IMAGE + ":latest");
@@ -28,9 +31,9 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
         optionallyMapResourceParameterAsVolume(MY_CNF_CONFIG_OVERRIDE_PARAM_NAME, "/etc/mysql/conf.d", "mysql-default-conf");
 
         addExposedPort(3306);
-        addEnv("MYSQL_DATABASE", "test");
-        addEnv("MYSQL_USER", "test");
-        addEnv("MYSQL_PASSWORD", "test");
+        addEnv("MYSQL_DATABASE", databaseName);
+        addEnv("MYSQL_USER", username);
+        addEnv("MYSQL_PASSWORD", password);
         addEnv("MYSQL_ROOT_PASSWORD", "test");
         setCommand("mysqld");
         setStartupAttempts(3);
@@ -43,17 +46,17 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
 
     @Override
     public String getJdbcUrl() {
-        return "jdbc:mysql://" + getContainerIpAddress() + ":" + getMappedPort(MYSQL_PORT) + "/test";
+        return "jdbc:mysql://" + getContainerIpAddress() + ":" + getMappedPort(MYSQL_PORT) + "/" + databaseName;
     }
 
     @Override
     public String getUsername() {
-        return "test";
+        return username;
     }
 
     @Override
     public String getPassword() {
-        return "test";
+        return password;
     }
 
     @Override
@@ -63,6 +66,24 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
 
     public SELF withConfigurationOverride(String s) {
         parameters.put(MY_CNF_CONFIG_OVERRIDE_PARAM_NAME, s);
+        return self();
+    }
+
+    @Override
+    public SELF withDatabaseName(final String databaseName) {
+        this.databaseName = databaseName;
+        return self();
+    }
+
+    @Override
+    public SELF withUsername(final String username) {
+        this.username = username;
+        return self();
+    }
+
+    @Override
+    public SELF withPassword(final String password) {
+        this.password = password;
         return self();
     }
 }
