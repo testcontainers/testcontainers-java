977572bf9101d32aa801d00922810568afb3dc05
Ihor Banadiga <banadiga@gmail.com>
2017-09-20 20:16:24 +0300

    Create tmp dir on MacOs.

--- core/src/main/java/org/testcontainers/utility/MountableFile.java
+++ core/src/main/java/org/testcontainers/utility/MountableFile.java
@@ -37,6 +37,7 @@ import org.testcontainers.images.builder.Transferable;
 public class MountableFile implements Transferable {
 
     private static final String TESTCONTAINERS_TMP_DIR_PREFIX = ".testcontainers-tmp-";
+    public static final String OS_MAC_TMP_DIR = "/tmp";
     private static final int BASE_FILE_MODE = 0100000;
     private static final int BASE_DIR_MODE = 0040000;
 
@@ -238,7 +239,10 @@ public class MountableFile implements Transferable {
 
     private File createTempDirectory() {
         try {
-            return Files.createTempDirectory(Paths.get(System.getProperty("java.io.tmpdir")), TESTCONTAINERS_TMP_DIR_PREFIX).toFile();
+            if (SystemUtils.IS_OS_MAC) {
+                return Files.createTempDirectory(Paths.get(OS_MAC_TMP_DIR), TESTCONTAINERS_TMP_DIR_PREFIX).toFile();
+            }
+            return Files.createTempDirectory(TESTCONTAINERS_TMP_DIR_PREFIX).toFile();
         } catch  (IOException e) {
             return new File(TESTCONTAINERS_TMP_DIR_PREFIX + Base58.randomString(5));
         }
