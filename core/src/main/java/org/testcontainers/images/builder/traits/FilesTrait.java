da3abbd8baaa6ea6b8dd2e81d381c09300a44cbb
Gleb Stsenov <gleb.stsenov@fortumo.com>
2017-11-11 20:35:43 +0000

    #446 'set permission when copying'

--- core/src/main/java/org/testcontainers/images/builder/traits/FilesTrait.java
+++ core/src/main/java/org/testcontainers/images/builder/traits/FilesTrait.java
@@ -11,12 +11,46 @@ import java.nio.file.Path;
  */
 public interface FilesTrait<SELF extends FilesTrait<SELF> & BuildContextBuilderTrait<SELF>> {
 
+    /**
+     * Adds file to tarball copied into container.
+     * @param path in tarball
+     * @param file in host filesystem
+     * @return self
+     */
     default SELF withFileFromFile(String path, File file) {
-        return withFileFromPath(path, file.toPath());
+        return withFileFromPath(path, file.toPath(), 0);
     }
 
+    /**
+     * Adds file to tarball copied into container.
+     * @param path in tarball
+     * @param filePath in host filesystem
+     * @return self
+     */
     default SELF withFileFromPath(String path, Path filePath) {
-        final MountableFile mountableFile = MountableFile.forHostPath(filePath);
+        return withFileFromPath(path, filePath, 0);
+    }
+
+    /**
+     * Adds file with given mode to tarball copied into container.
+     * @param path in tarball
+     * @param file in host filesystem
+     * @param mode octal value of posix file mode (000..777)
+     * @return self
+     */
+    default SELF withFileFromFile(String path, File file, int mode) {
+        return withFileFromPath(path, file.toPath(), mode);
+    }
+
+    /**
+     * Adds file with given mode to tarball copied into container.
+     * @param path in tarball
+     * @param filePath in host filesystem
+     * @param mode octal value of posix file mode (000..777)
+     * @return self
+     */
+    default SELF withFileFromPath(String path, Path filePath, int mode) {
+        final MountableFile mountableFile = MountableFile.forHostPath(filePath, mode);
         return ((SELF) this).withFileFromTransferable(path, mountableFile);
     }
 }
--- core/src/test/java/org/testcontainers/utility/MountableFileTest.java
+++ core/src/test/java/org/testcontainers/utility/MountableFileTest.java
@@ -8,10 +8,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertFalse;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class MountableFileTest {
+
+    private static final int TEST_FILE_MODE = 0532;
+    private static final int BASE_FILE_MODE = 0100000;
+    private static final int BASE_DIR_MODE = 0040000;
 
     @Test
     public void forClasspathResource() throws Exception {
@@ -60,6 +65,31 @@ public class MountableFileTest {
         assertFalse("The resolved path does not contain an escaped space", mountableFile.getResolvedPath().contains("\\ "));
     }
 
+    @Test
+    public void forClasspathResourceWithPermission() throws Exception {
+        final MountableFile mountableFile = MountableFile.forClasspathResource("mappable-resource/test-resource.txt",
+                TEST_FILE_MODE);
+
+        performChecks(mountableFile);
+        assertEquals("Valid file mode.", BASE_FILE_MODE | TEST_FILE_MODE, mountableFile.getFileMode());
+    }
+
+    @Test
+    public void forHostFilePathWithPermission() throws Exception {
+        final Path file = createTempFile("somepath");
+        final MountableFile mountableFile = MountableFile.forHostPath(file.toString(), TEST_FILE_MODE);
+        performChecks(mountableFile);
+        assertEquals("Valid file mode.", BASE_FILE_MODE | TEST_FILE_MODE, mountableFile.getFileMode());
+    }
+
+    @Test
+    public void forHostDirPathWithPermission() throws Exception {
+        final Path dir = createTempDir();
+        final MountableFile mountableFile = MountableFile.forHostPath(dir.toString(), TEST_FILE_MODE);
+        performChecks(mountableFile);
+        assertEquals("Valid dir mode.", BASE_DIR_MODE | TEST_FILE_MODE, mountableFile.getFileMode());
+    }
+
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @NotNull
     private Path createTempFile(final String name) throws IOException {
@@ -70,6 +100,11 @@ public class MountableFileTest {
 
         Files.copy(MountableFileTest.class.getResourceAsStream("/mappable-resource/test-resource.txt"), file);
         return file;
+    }
+
+    @NotNull
+    private Path createTempDir() throws IOException {
+        return Files.createTempDirectory("testcontainers");
     }
 
     private void performChecks(final MountableFile mountableFile) {
