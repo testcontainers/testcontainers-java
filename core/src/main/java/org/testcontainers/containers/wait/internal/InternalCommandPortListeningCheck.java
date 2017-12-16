6ecd4143e4e1cf99e798bef1275d7d2829aaf52c
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2017-12-16 19:11:21 +0100

    Fix case-sensitivity bug for port numbers in command
    This bug shows itself when a port number contains letters in the hexadecimal conversion.

--- core/src/main/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheck.java
+++ core/src/main/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheck.java
@@ -29,7 +29,7 @@ public class InternalCommandPortListeningCheck implements java.util.concurrent.C
 
     private void tryPort(Integer internalPort) {
         String[][] commands = {
-                {"/bin/sh", "-c", format("cat /proc/net/tcp | awk '{print $2}' | grep :%x && echo %s", internalPort, SUCCESS_MARKER)},
+                {"/bin/sh", "-c", format("cat /proc/net/tcp | awk '{print $2}' | grep :%X && echo %s", internalPort, SUCCESS_MARKER)},
                 {"/bin/sh", "-c", format("nc -vz -w 1 localhost %d && echo %s", internalPort, SUCCESS_MARKER)},
                 {"/bin/bash", "-c", format("</dev/tcp/localhost/%d && echo %s", internalPort, SUCCESS_MARKER)}
         };
--- core/src/test/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheckTest.java
+++ core/src/test/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheckTest.java
@@ -3,6 +3,7 @@ package org.testcontainers.containers.wait.internal;
 import com.google.common.collect.ImmutableSet;
 import org.junit.Rule;
 import org.junit.Test;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertThrows;
@@ -10,24 +11,28 @@ import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class InternalCommandPortListeningCheckTest {
 
-    @Rule
-    public GenericContainer nginx = new GenericContainer<>("nginx:1.9.4");
+  // Linking a custom configuration into the container so that nginx is listening on port 8080. This is necessary to proof
+  // that the command formatting uses the correct casing for hexadecimal numberd (i.e. 1F90 and not 1f90).
+  @Rule
+  public GenericContainer nginx = new GenericContainer<>("nginx:1.9.4")
+      .withClasspathResourceMapping("nginx.conf", "/etc/nginx/conf.d/default.conf", BindMode.READ_ONLY);
 
-    @Test
-    public void singleListening() {
-        final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(nginx, ImmutableSet.of(80));
+  @Test
+  public void singleListening() {
+    final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(nginx, ImmutableSet.of(8080));
 
-        final Boolean result = check.call();
+    final Boolean result = check.call();
 
-        assertTrue("InternalCommandPortListeningCheck identifies a single listening port", result);
-    }
+    assertTrue("InternalCommandPortListeningCheck identifies a single listening port", result);
+  }
 
-    @Test
-    public void nonListening() {
-        final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(nginx, ImmutableSet.of(80, 1234));
+  @Test
+  public void nonListening() {
+    final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(nginx, ImmutableSet.of(8080, 1234));
 
-        assertThrows("InternalCommandPortListeningCheck detects a non-listening port among many",
-                IllegalStateException.class,
-                (Runnable) check::call);
-    }
+    assertThrows("InternalCommandPortListeningCheck detects a non-listening port among many",
+        IllegalStateException.class,
+        (Runnable) check::call);
+  }
+
 }
