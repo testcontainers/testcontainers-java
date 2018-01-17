5bb2e25f432f54a728fbe342ea71939d540269fd
Sergei Egorov <sergei@uvita.eu>
2018-01-17 11:24:26 +0100

    make it possible to use KafkaContainer with external Zookeeper

--- modules/kafka/src/main/java/org.testcontainers.containers/KafkaContainer.java
+++ modules/kafka/src/main/java/org.testcontainers.containers/KafkaContainer.java
@@ -10,6 +10,8 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
 
     public static final int ZOOKEEPER_PORT = 2181;
 
+    protected String externalZookeeperConnect = null;
+
     protected SocatContainer proxy;
 
     public KafkaContainer() {
@@ -21,7 +23,7 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
 
         withNetwork(Network.newNetwork());
         withNetworkAliases("kafka-" + Base58.randomString(6));
-        withExposedPorts(KAFKA_PORT, ZOOKEEPER_PORT);
+        withExposedPorts(KAFKA_PORT);
 
         withEnv("KAFKA_BROKER_ID", "1");
         withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092,BROKER://127.0.0.1:9093");
@@ -31,10 +33,16 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
         withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1");
         withEnv("KAFKA_OFFSETS_TOPIC_NUM_PARTITIONS", "1");
         withEnv("KAFKA_LOG_FLUSH_INTERVAL_MESSAGES", Long.MAX_VALUE + "");
+    }
 
-        withEnv("KAFKA_ZOOKEEPER_CONNECT", "localhost:2181");
-        withClasspathResourceMapping("tc-zookeeper.properties", "/zookeeper.properties", BindMode.READ_ONLY);
-        withCommand("sh", "-c", "zookeeper-server-start /zookeeper.properties & /etc/confluent/docker/run");
+    public KafkaContainer withEmbeddedZookeeper() {
+        externalZookeeperConnect = null;
+        return self();
+    }
+
+    public KafkaContainer withExternalZookeeper(String connectString) {
+        externalZookeeperConnect = connectString;
+        return self();
     }
 
     public String getBootstrapServers() {
@@ -50,6 +58,15 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
 
         proxy.start();
         withEnv("KAFKA_ADVERTISED_LISTENERS", "BROKER://127.0.0.1:9093,PLAINTEXT://" + proxy.getContainerIpAddress() + ":" + proxy.getFirstMappedPort());
+
+        if (externalZookeeperConnect != null) {
+            withEnv("KAFKA_ZOOKEEPER_CONNECT", externalZookeeperConnect);
+        } else {
+            addExposedPort(ZOOKEEPER_PORT);
+            withEnv("KAFKA_ZOOKEEPER_CONNECT", "localhost:2181");
+            withClasspathResourceMapping("tc-zookeeper.properties", "/zookeeper.properties", BindMode.READ_ONLY);
+            withCommand("sh", "-c", "zookeeper-server-start /zookeeper.properties & /etc/confluent/docker/run");
+        }
 
         super.start();
     }
