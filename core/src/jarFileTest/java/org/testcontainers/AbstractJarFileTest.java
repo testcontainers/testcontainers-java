7e0fc19b2588ca2d81beb8c4f61969d0ccbd6fb2
Sergei Egorov <sergei@uvita.eu>
2018-02-03 18:10:31 +0100

    polish Gradle config, fix publishing, add Delombok, move JAR file tests to Gradle

--- core/src/jarFileTest/java/org/testcontainers/AbstractJarFileTest.java
+++ core/src/jarFileTest/java/org/testcontainers/AbstractJarFileTest.java
@@ -1,11 +1,9 @@
 package org.testcontainers;
 
-import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static java.util.Collections.emptyMap;
 
@@ -14,12 +12,10 @@ public abstract class AbstractJarFileTest {
     public static Path root;
 
     static {
-        Path path = Paths.get("..", "..", "core", "target", "testcontainers-0-SNAPSHOT.jar");
-
         try {
-            FileSystem fileSystem = FileSystems.newFileSystem(URI.create("jar:" + path.toUri()), emptyMap());
+            FileSystem fileSystem = FileSystems.newFileSystem(URI.create("jar:file://" + System.getProperty("jarFile")), emptyMap());
             root = fileSystem.getPath("/");
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
