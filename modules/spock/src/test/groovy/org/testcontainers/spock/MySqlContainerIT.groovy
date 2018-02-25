8d7956264c0271beae4506e992d4b93d33d362e2
Kevin Wittek <kevin.wittek@styracosoft.de>
2018-02-25 16:06:31 +0100

    Add spock module

---
+++ modules/spock/src/test/groovy/org/testcontainers/spock/MySqlContainerIT.groovy
@@ -0,0 +1,26 @@
+package org.testcontainers.spock
+
+import org.testcontainers.containers.MySQLContainer
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * This test verifies, that setup and cleanup of containers works correctly.
+ * It's easily achieved using the <code>MySQLContainer</code>, since it will fail
+ * if the same image is running.
+ *
+ * @see <a href="https://github.com/testcontainers/testcontainers-spock/issues/19">Second container is started when stopping old container</a>
+ */
+@Testcontainers
+class MySqlContainerIT extends Specification {
+
+    @Shared
+    MySQLContainer mySQLContainer = new MySQLContainer()
+
+    def "dummy test"() {
+        expect:
+        mySQLContainer.isRunning()
+    }
+
+
+}
