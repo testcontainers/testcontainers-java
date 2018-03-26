e2c1f0721dd67024a8e595bb08af6779e48edb97
Barry Commins <barry.commins@gmail.com>
2018-03-26 22:04:22 +0100

    Fixed getServicePort on DockerComposeContainer throws NullPointerException if service instance number in not used. (#619)

--- core/src/test/java/org/testcontainers/junit/DockerComposeContainerTest.java
+++ core/src/test/java/org/testcontainers/junit/DockerComposeContainerTest.java
@@ -1,9 +1,13 @@
 package org.testcontainers.junit;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.testcontainers.containers.DockerComposeContainer;
 
 import java.io.File;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertNotNull;
 
 /**
  * Created by rnorth on 08/08/2015.
@@ -19,5 +23,14 @@ public class DockerComposeContainerTest extends BaseDockerComposeTest {
     @Override
     protected DockerComposeContainer getEnvironment() {
         return environment;
+    }
+
+    @Test
+    public void testGetServicePort() {
+        int serviceWithInstancePort = environment.getServicePort("redis_1", REDIS_PORT);
+        assertNotNull("Port is set for service with instance number", serviceWithInstancePort);
+        int serviceWithoutInstancePort = environment.getServicePort("redis", REDIS_PORT);
+        assertNotNull("Port is set for service with instance number", serviceWithoutInstancePort);
+        assertEquals("Service ports are the same", serviceWithInstancePort, serviceWithoutInstancePort);
     }
 }
