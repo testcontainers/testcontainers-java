ebd492091e649abc7eca2c8456cd4f7a981bcae2
Sergei Egorov <bsideup@gmail.com>
2018-05-20 13:59:47 +0200

    split files, support more protocols, fix blocking call

---
+++ core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/OkHttpInvocationBuilder.java
@@ -0,0 +1,344 @@
+package org.testcontainers.dockerclient.transport.okhttp;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.exception.*;
+import com.github.dockerjava.api.model.Frame;
+import com.github.dockerjava.core.InvocationBuilder;
+import com.github.dockerjava.netty.handler.FramedResponseStreamHandler;
+import com.github.dockerjava.netty.handler.JsonResponseCallbackHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import okhttp3.internal.connection.RealConnection;
+import okio.BufferedSink;
+import okio.BufferedSource;
+import okio.Okio;
+import org.testcontainers.DockerClientFactory;
+
+import javax.annotation.Nullable;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@FieldDefaults(makeFinal = true)
+public class OkHttpInvocationBuilder implements InvocationBuilder {
+
+    ObjectMapper objectMapper;
+
+    OkHttpClient okHttpClient;
+
+    Request.Builder requestBuilder;
+
+    public OkHttpInvocationBuilder(ObjectMapper objectMapper, OkHttpClient okHttpClient, HttpUrl httpUrl) {
+        this.objectMapper = objectMapper;
+        this.okHttpClient = okHttpClient;
+
+        requestBuilder = new Request.Builder()
+            .url(httpUrl);
+    }
+
+    @Override
+    public OkHttpInvocationBuilder accept(com.github.dockerjava.core.MediaType mediaType) {
+        return header("accept", mediaType.getMediaType());
+    }
+
+    @Override
+    public OkHttpInvocationBuilder header(String name, String value) {
+        requestBuilder.header(name, value);
+        return this;
+    }
+
+    @Override
+    @SneakyThrows
+    public void delete() {
+        Request request = requestBuilder
+            .delete()
+            .build();
+
+        execute(request).get().close();
+    }
+
+    @Override
+    @SneakyThrows
+    public void get(ResultCallback<Frame> resultCallback) {
+        Request request = requestBuilder
+            .get()
+            .build();
+
+        execute(request).whenCompleteAsync((response, e) -> {
+            if (e != null) {
+                resultCallback.onError(e);
+            } else {
+                handleStreamedResponse(
+                    response,
+                    resultCallback,
+                    new FramedResponseStreamHandler(resultCallback)
+                );
+            }
+        });
+    }
+
+    @Override
+    @SneakyThrows(IOException.class)
+    public <T> T get(TypeReference<T> typeReference) {
+        try (InputStream inputStream = get()) {
+            return objectMapper.readValue(inputStream, typeReference);
+        }
+    }
+
+    @Override
+    public <T> void get(TypeReference<T> typeReference, ResultCallback<T> resultCallback) {
+        // FIXME
+        throw new IllegalStateException("doesn't seem to be used in docker-java");
+    }
+
+    @Override
+    @SneakyThrows
+    public InputStream post(Object entity) {
+        Request request = requestBuilder
+            .post(RequestBody.create(null, objectMapper.writeValueAsBytes(entity)))
+            .build();
+
+        return execute(request).get().body().byteStream();
+    }
+
+    @Override
+    @SneakyThrows
+    public <T> T post(Object entity, TypeReference<T> typeReference) {
+        Request request = requestBuilder
+            .post(RequestBody.create(MediaType.parse("application/json"), objectMapper.writeValueAsBytes(entity)))
+            .build();
+
+        try (Response response = execute(request).get()) {
+            String inputStream = response.body().string();
+            return objectMapper.readValue(inputStream, typeReference);
+        }
+    }
+
+    @Override
+    @SneakyThrows(JsonProcessingException.class)
+    public <T> void post(Object entity, TypeReference<T> typeReference, ResultCallback<T> resultCallback) {
+        post(typeReference, resultCallback, new ByteArrayInputStream(objectMapper.writeValueAsBytes(entity)));
+    }
+
+    @Override
+    @SneakyThrows(IOException.class)
+    public <T> T post(TypeReference<T> typeReference, InputStream body) {
+        try (InputStream inputStream = post(body)) {
+            return objectMapper.readValue(inputStream, typeReference);
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public void post(Object entity, InputStream stdin, ResultCallback<Frame> resultCallback) {
+        Request request = requestBuilder
+            .post(RequestBody.create(MediaType.parse("application/json"), objectMapper.writeValueAsBytes(entity)))
+            .build();
+
+        OkHttpClient okHttpClient = this.okHttpClient;
+
+        if (stdin != null) {
+            // FIXME there must be a better way of handling it
+            okHttpClient = okHttpClient.newBuilder()
+                .addNetworkInterceptor(new Interceptor() {
+                    @Override
+                    @SneakyThrows
+                    public Response intercept(Chain chain) {
+                        RealConnection connection = (RealConnection) chain.connection();
+
+                        Field sinkField = RealConnection.class.getDeclaredField("sink");
+                        sinkField.setAccessible(true);
+                        BufferedSink sink = (BufferedSink) sinkField.get(connection);
+
+                        Thread thread = new Thread(DockerClientFactory.TESTCONTAINERS_THREAD_GROUP, () -> {
+                            try {
+                                sink.writeAll(Okio.source(stdin));
+                                sink.flush();
+
+                                Thread.sleep(100);
+                                sink.close();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                        thread.start();
+
+                        return chain.proceed(chain.request());
+                    }
+                })
+                .build();
+        }
+
+        execute(okHttpClient, request).whenCompleteAsync((response, e) -> {
+            if (e != null) {
+                resultCallback.onError(e);
+            } else {
+                handleStreamedResponse(
+                    response,
+                    resultCallback,
+                    new FramedResponseStreamHandler(resultCallback)
+                );
+            }
+        });
+    }
+
+    @Override
+    public <T> void post(TypeReference<T> typeReference, ResultCallback<T> resultCallback, InputStream body) {
+        Request request = requestBuilder
+            .post(toRequestBody(body, null))
+            .build();
+
+        execute(request).whenCompleteAsync((response, e) -> {
+            if (e != null) {
+                resultCallback.onError(e);
+            } else {
+
+                handleStreamedResponse(
+                    response,
+                    resultCallback,
+                    new JsonResponseCallbackHandler<>(typeReference, resultCallback)
+                );
+            }
+        });
+    }
+
+    @Override
+    @SneakyThrows
+    public void postStream(InputStream body) {
+        Request request = requestBuilder
+            .post(toRequestBody(body, null))
+            .build();
+
+        execute(request).get().close();
+    }
+
+    @Override
+    @SneakyThrows
+    public InputStream get() {
+        Request request = requestBuilder
+            .get()
+            .build();
+
+        return execute(request).get().body().byteStream();
+    }
+
+    @Override
+    @SneakyThrows
+    public void put(InputStream body, com.github.dockerjava.core.MediaType mediaType) {
+        Request request = requestBuilder
+            .put(toRequestBody(body, mediaType.toString()))
+            .build();
+
+        execute(request).get().close();
+    }
+
+    protected RequestBody toRequestBody(InputStream body, @Nullable String mediaType) {
+        return new RequestBody() {
+            @Nullable
+            @Override
+            public MediaType contentType() {
+                if (mediaType == null) {
+                    return null;
+                }
+                return MediaType.parse(mediaType);
+            }
+
+            @Override
+            public void writeTo(BufferedSink sink) throws IOException {
+                sink.writeAll(Okio.source(body));
+            }
+        };
+    }
+
+    protected CompletableFuture<Response> execute(Request request) {
+        return execute(okHttpClient, request);
+    }
+
+    protected CompletableFuture<Response> execute(OkHttpClient okHttpClient, Request request) {
+        CompletableFuture<Response> future = new CompletableFuture<>();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                future.completeExceptionally(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                int code = response.code();
+                if (code < 200 || code >= 300) {
+                    try {
+                        String body = response.body().string();
+                        switch (code) {
+                            case 304:
+                                throw new NotModifiedException(body);
+                            case 400:
+                                throw new BadRequestException(body);
+                            case 401:
+                                throw new UnauthorizedException(body);
+                            case 404:
+                                throw new NotFoundException(body);
+                            case 406:
+                                throw new NotAcceptableException(body);
+                            case 409:
+                                throw new ConflictException(body);
+                            case 500:
+                                throw new InternalServerErrorException(body);
+                            default:
+                                throw new DockerException(body, code);
+                        }
+                    } catch (Exception e) {
+                        future.completeExceptionally(e);
+                    } finally {
+                        response.close();
+                    }
+                } else {
+                    future.complete(response);
+                }
+            }
+        });
+        return future;
+    }
+
+    protected <T> void handleStreamedResponse(Response response, ResultCallback<T> callback, SimpleChannelInboundHandler<ByteBuf> handler) {
+        try {
+            // TODO proper thread management
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                @SneakyThrows
+                public void run() {
+                    BufferedSource source = response.body().source();
+                    InputStream inputStream = source.inputStream();
+
+                    byte[] buffer = new byte[4 * 1024];
+                    while (!source.exhausted() && !Thread.interrupted()) {
+                        int bytesReceived = inputStream.read(buffer);
+
+                        handler.channelRead(null, Unpooled.wrappedBuffer(buffer, 0, bytesReceived));
+                    }
+                    callback.onComplete();
+                }
+            });
+
+            callback.onStart(() -> {
+                thread.interrupt();
+                response.close();
+            });
+
+            thread.start();
+        } catch (Exception e) {
+            callback.onError(e);
+        }
+    }
+}
---
+++ core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/OkHttpWebTarget.java
@@ -0,0 +1,175 @@
+package org.testcontainers.dockerclient.transport.okhttp;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.InvocationBuilder;
+import com.github.dockerjava.core.SSLConfig;
+import com.github.dockerjava.core.WebTarget;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.SetMultimap;
+import de.gesellix.docker.client.filesocket.UnixSocket;
+import de.gesellix.docker.client.filesocket.UnixSocketFactory;
+import lombok.SneakyThrows;
+import lombok.Value;
+import lombok.experimental.Wither;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.internal.Internal;
+import org.apache.commons.lang.StringUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+import java.net.URI;
+import java.security.cert.X509Certificate;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Wither
+@Value
+public class OkHttpWebTarget implements WebTarget {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    DockerClientConfig dockerClientConfig;
+
+    ImmutableList<String> path;
+
+    SetMultimap<String, String> queryParams;
+
+    @Override
+    @SneakyThrows
+    public InvocationBuilder request() {
+        String resource = StringUtils.join(path, "/");
+
+        if (!resource.startsWith("/")) {
+            resource = "/" + resource;
+        }
+
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+
+        SSLConfig sslConfig = dockerClientConfig.getSSLConfig();
+        if (sslConfig != null) {
+            SSLContext sslContext = sslConfig.getSSLContext();
+            if (sslContext != null) {
+                clientBuilder
+                    .sslSocketFactory(sslContext.getSocketFactory(), new TrustAllX509TrustManager());
+            }
+        }
+
+        HttpUrl.Builder urlBuilder;
+
+        URI dockerHost = dockerClientConfig.getDockerHost();
+        switch (dockerHost.getScheme()) {
+            case "npipe":
+                // TODO support it
+                throw new IllegalArgumentException("npipe protocol is not supported yet");
+            case "unix":
+                UnixSocketFactory unixSocketFactory = new UnixSocketFactory();
+                clientBuilder
+                    .socketFactory(unixSocketFactory)
+                    .dns(unixSocketFactory);
+
+                urlBuilder = new HttpUrl.Builder()
+                    .scheme("http")
+                    .host(new UnixSocket().encodeHostname(dockerHost.getPath()));
+                break;
+            case "tcp":
+                urlBuilder = new HttpUrl.Builder()
+                    .scheme(sslConfig == null ? "http" : "https")
+                    .host(dockerHost.getHost())
+                    .port(dockerHost.getPort());
+                break;
+            default:
+                urlBuilder = Internal.instance.getHttpUrlChecked(dockerHost.toString()).newBuilder();
+        }
+
+        urlBuilder
+            .encodedPath(resource);
+
+        for (Map.Entry<String, Collection<String>> queryParamEntry : queryParams.asMap().entrySet()) {
+            String key = queryParamEntry.getKey();
+            for (String paramValue : queryParamEntry.getValue()) {
+                urlBuilder.addQueryParameter(key, paramValue);
+            }
+        }
+
+        return new OkHttpInvocationBuilder(
+            MAPPER,
+            clientBuilder.build(),
+            urlBuilder.build()
+        );
+    }
+
+    @Override
+    public OkHttpWebTarget path(String... components) {
+        return this.withPath(
+            ImmutableList.<String>builder()
+                .addAll(path)
+                .add(components)
+                .build()
+        );
+    }
+
+    @Override
+    public OkHttpWebTarget resolveTemplate(String name, Object value) {
+        ImmutableList.Builder<String> newPath = ImmutableList.builder();
+        for (String component : path) {
+            component = component.replaceAll("\\{" + name + "\\}", value.toString());
+            newPath.add(component);
+        }
+        return this.withPath(newPath.build());
+    }
+
+    @Override
+    public OkHttpWebTarget queryParam(String name, Object value) {
+        if (value == null) {
+            return this;
+        }
+
+        SetMultimap<String, String> newQueryParams = HashMultimap.create(queryParams);
+        newQueryParams.put(name, value.toString());
+
+        return this.withQueryParams(newQueryParams);
+    }
+
+    @Override
+    public OkHttpWebTarget queryParamsSet(String name, Set<?> values) {
+        SetMultimap<String, String> newQueryParams = HashMultimap.create(queryParams);
+        newQueryParams.replaceValues(name, values.stream().filter(Objects::nonNull).map(Object::toString).collect(Collectors.toSet()));
+
+        return this.withQueryParams(newQueryParams);
+    }
+
+    @Override
+    @SneakyThrows(JsonProcessingException.class)
+    public OkHttpWebTarget queryParamsJsonMap(String name, Map<String, String> values) {
+        if (values == null || values.isEmpty()) {
+            return this;
+        }
+
+        // when param value is JSON string
+        return queryParam(name, MAPPER.writeValueAsString(values));
+    }
+
+    private static class TrustAllX509TrustManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
+
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+}
