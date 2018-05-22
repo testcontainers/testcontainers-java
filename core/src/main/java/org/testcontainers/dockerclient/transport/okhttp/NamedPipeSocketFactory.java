32ac15dbe3ec1185420eb98db21f3218ceb7003f
Sergei Egorov <bsideup@gmail.com>
2018-05-22 07:51:37 +0200

    fix pipe name

--- core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/NamedPipeSocketFactory.java
+++ core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/NamedPipeSocketFactory.java
@@ -18,7 +18,8 @@ public class NamedPipeSocketFactory extends SocketFactory {
     @Override
     @SneakyThrows
     public Socket createSocket() {
-        return new Win32NamedPipeSocket(socketPath) {
+        return new Win32NamedPipeSocket(socketPath.replace("/", "\\\\")) {
+
             @Override
             public void connect(SocketAddress endpoint, int timeout) throws IOException {
             }
