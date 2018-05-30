55859d14790107dc0de1c1b453905b0b15ac0dbc
Ali Ahmed <alahmed.se@gmail.com>
2018-05-30 11:19:43 -0700

    Reafctor container class

--- modules/pulsar/src/main/java/org/testcontainers/containers/PulsarContainer.java
+++ modules/pulsar/src/main/java/org/testcontainers/containers/PulsarContainer.java
@@ -18,27 +18,13 @@ public class PulsarContainer extends GenericContainer<PulsarContainer> {
 
     public PulsarContainer(String pulsarVersion) {
         super(TestcontainersConfiguration.getInstance().getPulsarImage() + ":" + pulsarVersion);
-
-        withNetwork(Network.newNetwork());
-        String networkAlias = "pulsar-" + Base58.randomString(6);
-        withNetworkAliases(networkAlias);
         withExposedPorts(PULSAR_PORT);
-
+        withClasspathResourceMapping("proxy.conf", "/pulsar/conf/proxy.conf", BindMode.READ_ONLY);
+        withCommand("/bin/bash", "-c", "bin/pulsar standalone & bin/pulsar proxy --zookeeper-servers localhost:2181 --global-zookeeper-servers localhost:2181");
     }
 
     public String getPulsarBrokerUrl() {
         return String.format("pulsar://%s:%s", this.getContainerIpAddress(), this.getFirstMappedPort());
     }
 
-    @Override
-    public void start() {
-        withClasspathResourceMapping("proxy.conf", "/pulsar/conf/proxy.conf", BindMode.READ_ONLY);
-        withCommand("/bin/bash", "-c", "bin/pulsar standalone & bin/pulsar proxy --zookeeper-servers localhost:2181 --global-zookeeper-servers localhost:2181");
-        super.start();
-    }
-
-    @Override
-    public void stop() {
-        Stream.<Runnable>of(super::stop).parallel().forEach(Runnable::run);
-    }
 }
