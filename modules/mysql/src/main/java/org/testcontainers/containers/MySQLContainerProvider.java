dbb92674f3dd3de0f8b8de5c2c1c88b39dd72f00
Manik Magar <manik.magar@gmail.com>
2018-06-13 16:22:44 -0400

    Enhancements and Fixes around JDBC URL Based Containers (#617)
    * Enhancements and Fixes around JDBC URL Based Containers
    (Rebased and Squashed)
    
    * Reinstate environment for Oracle module
    
    * Clean up diff

--- modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainerProvider.java
+++ modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainerProvider.java
@@ -1,9 +1,18 @@
 package org.testcontainers.containers;
 
+import org.testcontainers.jdbc.ConnectionUrl;
+
+import java.util.Objects;
+
 /**
  * Factory for MySQL containers.
  */
 public class MySQLContainerProvider extends JdbcDatabaseContainerProvider {
+
+    private static final String USER_PARAM = "user";
+
+    private static final String PASSWORD_PARAM = "password";
+
     @Override
     public boolean supports(String databaseType) {
         return databaseType.equals(MySQLContainer.NAME);
@@ -16,6 +25,32 @@ public class MySQLContainerProvider extends JdbcDatabaseContainerProvider {
 
     @Override
     public JdbcDatabaseContainer newInstance(String tag) {
-        return new MySQLContainer(MySQLContainer.IMAGE + ":" + tag);
+        if (tag != null) {
+            return new MySQLContainer(MySQLContainer.IMAGE + ":" + tag);
+        } else {
+            return newInstance();
+        }
     }
+
+    @Override
+    public JdbcDatabaseContainer newInstance(ConnectionUrl connectionUrl) {
+        Objects.requireNonNull(connectionUrl, "Connection URL cannot be null");
+
+        final String databaseName = connectionUrl.getDatabaseName().orElse("test");
+        final String user = connectionUrl.getQueryParameters().getOrDefault(USER_PARAM, "test");
+        final String password = connectionUrl.getQueryParameters().getOrDefault(PASSWORD_PARAM, "test");
+
+        final JdbcDatabaseContainer instance;
+        if (connectionUrl.getImageTag().isPresent()) {
+            instance = newInstance(connectionUrl.getImageTag().get());
+        } else {
+            instance = newInstance();
+        }
+
+        return instance
+            .withDatabaseName(databaseName)
+            .withUsername(user)
+            .withPassword(password);
+    }
+
 }
