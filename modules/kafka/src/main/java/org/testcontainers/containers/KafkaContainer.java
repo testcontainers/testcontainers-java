testcontainers/testcontainers-java

commit 16fdc6566cc5484c3f65d52ff41b992ce921d822

use standard port 9092 in KafkaContainer (#733)
* use standard port 9092 in KafkaContainer

* use constants where possible

modules/kafka/src/main/java/org/testcontainers/containers/KafkaContainer.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
  */
 public class KafkaContainer extends GenericContainer<KafkaContainer> {
 
-    public static final int KAFKA_PORT = 9092;
+    public static final int KAFKA_PORT = 9093;
 
     public static final int ZOOKEEPER_PORT = 2181;
 
@@ -27,13 +27,12 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
         super(TestcontainersConfiguration.getInstance().getKafkaImage() + ":" + confluentPlatformVersion);
 
         withNetwork(Network.newNetwork());
-        String networkAlias = "kafka-" + Base58.randomString(6);
-        withNetworkAliases(networkAlias);
+        withNetworkAliases("kafka-" + Base58.randomString(6));
         withExposedPorts(KAFKA_PORT);
 
         // Use two listeners with different names, it will force Kafka to communicate with itself via internal
         // listener when KAFKA_INTER_BROKER_LISTENER_NAME is set, otherwise Kafka will try to use the advertised listener
-        withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092,BROKER://" + networkAlias + ":9093");
+        withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:" + KAFKA_PORT + ",BROKER://0.0.0.0:9092");
         withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT");
         withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER");
 
@@ -62,11 +61,11 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
         String networkAlias = getNetworkAliases().get(0);
         proxy = new SocatContainer()
                 .withNetwork(getNetwork())
-                .withTarget(9092, networkAlias)
-                .withTarget(2181, networkAlias);
+                .withTarget(KAFKA_PORT, networkAlias)
+                .withTarget(ZOOKEEPER_PORT, networkAlias);
 
         proxy.start();
-        withEnv("KAFKA_ADVERTISED_LISTENERS", "BROKER://" + networkAlias + ":9093,PLAINTEXT://" + proxy.getContainerIpAddress() + ":" + proxy.getFirstMappedPort());
+        withEnv("KAFKA_ADVERTISED_LISTENERS", "BROKER://" + networkAlias + ":" + KAFKA_PORT +",PLAINTEXT://" + proxy.getContainerIpAddress() + ":" + proxy.getFirstMappedPort());
 
         if (externalZookeeperConnect != null) {
             withEnv("KAFKA_ZOOKEEPER_CONNECT", externalZookeeperConnect);

commit fd147ec46e583a243758639c2698791d8f6fab9a

Fix Kafka's inner communication bug introduced in #733

modules/kafka/src/main/java/org/testcontainers/containers/KafkaContainer.java
@@ -65,7 +65,7 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
                 .withTarget(ZOOKEEPER_PORT, networkAlias);
 
         proxy.start();
-        withEnv("KAFKA_ADVERTISED_LISTENERS", "BROKER://" + networkAlias + ":" + KAFKA_PORT +",PLAINTEXT://" + proxy.getContainerIpAddress() + ":" + proxy.getFirstMappedPort());
+        withEnv("KAFKA_ADVERTISED_LISTENERS", "BROKER://" + networkAlias + ":9092" + "," + getBootstrapServers());
 
         if (externalZookeeperConnect != null) {
             withEnv("KAFKA_ZOOKEEPER_CONNECT", externalZookeeperConnect);