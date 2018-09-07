352bcb8b241efe3663fb56a4a71be58766ade9e2
Richard North <rich.north@gmail.com>
2018-09-07 08:57:39 +0100

    Update to docker-java 3.1.0-rc-4

--- core/src/main/java/org/testcontainers/dockerclient/transport/TestcontainersDockerCmdExecFactory.java
+++ core/src/main/java/org/testcontainers/dockerclient/transport/TestcontainersDockerCmdExecFactory.java
@@ -80,16 +80,21 @@ public class TestcontainersDockerCmdExecFactory extends AbstractDockerCmdExecFac
         bootstrap = new Bootstrap();
 
         String scheme = dockerClientConfig.getDockerHost().getScheme();
+        String host = "";
 
         if ("unix".equals(scheme)) {
             nettyInitializer = new UnixDomainSocketInitializer();
+            host = "DUMMY";
         } else if ("tcp".equals(scheme)) {
             nettyInitializer = new InetSocketInitializer();
+            host = dockerClientConfig.getDockerHost().getHost() + ":"
+                + Integer.toString(dockerClientConfig.getDockerHost().getPort());
         }
 
         eventLoopGroup = nettyInitializer.init(bootstrap, dockerClientConfig);
 
-        baseResource = new NettyWebTarget(this::connect).path(dockerClientConfig.getApiVersion().asWebPathPart());
+        baseResource = new NettyWebTarget(this::connect, host)
+            .path(dockerClientConfig.getApiVersion().asWebPathPart());
     }
 
     private DuplexChannel connect() {
