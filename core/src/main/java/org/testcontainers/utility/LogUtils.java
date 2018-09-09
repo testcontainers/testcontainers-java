d127fd799bccbb4ee4d006dc2edd0f56c0c908c2
Richard North <rich.north@gmail.com>
2018-09-09 13:08:09 +0100

    Add support for docker hub private registry credentials (#845)
    * Fix authentication for use of base64-encoded credentials and credential helpers with private registries and docker hub
    
    * Update following review by @bsideup
    
    * Update to docker-java 3.1.0-rc-4
    
    * fix shading

--- core/src/main/java/org/testcontainers/utility/LogUtils.java
+++ core/src/main/java/org/testcontainers/utility/LogUtils.java
@@ -2,12 +2,15 @@ package org.testcontainers.utility;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.LogContainerCmd;
+import com.github.dockerjava.api.model.AuthConfig;
+import com.google.common.base.MoreObjects;
 import lombok.experimental.UtilityClass;
 import org.testcontainers.containers.output.FrameConsumerResultCallback;
 import org.testcontainers.containers.output.OutputFrame;
 
 import java.util.function.Consumer;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.testcontainers.containers.output.OutputFrame.OutputType.STDERR;
 import static org.testcontainers.containers.output.OutputFrame.OutputType.STDOUT;
 
@@ -39,4 +42,5 @@ public class LogUtils {
     public void followOutput(DockerClient dockerClient, String containerId, Consumer<OutputFrame> consumer) {
         followOutput(dockerClient, containerId, consumer, STDOUT, STDERR);
     }
+
 }
