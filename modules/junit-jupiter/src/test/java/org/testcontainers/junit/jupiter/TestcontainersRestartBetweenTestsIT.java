fff8dd0bac943d3d9c55082b83485b18fdd7e0b6
Benedikt Ritter <beneritter@gmail.com>
2018-09-28 13:03:55 +0200

    Adapt TestcontainersRestatBetweenTestsIT from spock extension

---
+++ modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestcontainersRestartBetweenTestsIT.java
@@ -0,0 +1,34 @@
+package org.testcontainers.junit.jupiter;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@Testcontainers
+class TestcontainersRestartBetweenTestsIT {
+
+    private GenericContainer genericContainer = new GenericContainer("httpd:2.4-alpine")
+            .withExposedPorts(80);
+
+    private static String LAST_CONTAINER_ID;
+
+    @Test
+    void first_test() {
+        if (LAST_CONTAINER_ID == null) {
+            LAST_CONTAINER_ID = genericContainer.getContainerId();
+        }  else {
+            assertNotEquals(LAST_CONTAINER_ID, genericContainer.getContainerId());
+        }
+    }
+
+    @Test
+    void second_test() {
+        if (LAST_CONTAINER_ID == null) {
+            LAST_CONTAINER_ID = genericContainer.getContainerId();
+        }  else {
+            assertNotEquals(LAST_CONTAINER_ID, genericContainer.getContainerId());
+        }
+    }
+
+}
