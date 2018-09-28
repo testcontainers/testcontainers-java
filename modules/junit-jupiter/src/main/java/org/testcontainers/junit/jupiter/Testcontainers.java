6b7bad6871d14986c5ece4f7136911e0b9cbc516
Benedikt Ritter <beneritter@gmail.com>
2018-09-28 17:01:04 +0200

    Add JavaDoc

--- modules/junit-jupiter/src/main/java/org/testcontainers/junit/jupiter/Testcontainers.java
+++ modules/junit-jupiter/src/main/java/org/testcontainers/junit/jupiter/Testcontainers.java
@@ -7,6 +7,42 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * {@code @Testcontainers} is a JUnit Jupiter extension to activate automatic
+ * startup and stop of test containers used in a test case.
+ *
+ * <p>The test containers extension finds all fields of typ
+ * {@link org.testcontainers.lifecycle.Startable} and calls their container
+ * lifecylce methods. Containers declared as static fields will be shared
+ * between test methods. They will be started only once before any test method
+ * is executed and stopped after the last test method has executed. Containers
+ * declared as instance fields will be started and stopped for every test
+ * method.</p>
+ *
+ * <p>Example:
+ *
+ * <pre>
+ * &#64;Testcontainers
+ * class MyTestcontainersTests {
+ *
+ *     // will be shared between test methods
+ *     private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer();
+ *
+ *     // will be started before and stopped after each test method
+ *     private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
+ *             .withDatabaseName("foo")
+ *             .withUsername("foo")
+ *             .withPassword("secret");
+ *
+ *     &#64;Test
+ *     void test() {
+ *         assertTrue(MY_SQL_CONTAINER.isRunning());
+ *         assertTrue(postgresqlContainer.isRunning());
+ *     }
+ * }
+ * </pre>
+ * </p>
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(TestcontainersExtension.class)
