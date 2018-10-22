3281ac54f1b623b3220140843b2ca09296dc349d
Alexander Sparkowsky <info@roamingthings.de>
2018-10-22 16:29:48 +0200

    Update test to only start one container per test

--- modules/selenium/src/test/java/org/testcontainers/junit/BrowserWebDriverContainerTest.java
+++ modules/selenium/src/test/java/org/testcontainers/junit/BrowserWebDriverContainerTest.java
@@ -1,6 +1,5 @@
 package org.testcontainers.junit;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testcontainers.containers.BrowserWebDriverContainer;
@@ -9,26 +8,38 @@ import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
 public class BrowserWebDriverContainerTest {
 
+    private static final String NO_PROXY_KEY = "no_proxy";
+
     private static final String NO_PROXY_VALUE = "localhost,.noproxy-domain.com";
-
-    @Rule
-    public BrowserWebDriverContainer chromeWithNoProxySet = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
-        .withDesiredCapabilities(DesiredCapabilities.chrome())
-        .withEnv("no_proxy", NO_PROXY_VALUE);
-
-    @Rule
-    public BrowserWebDriverContainer chromeWithoutNoProxySet = new BrowserWebDriverContainer()
-        .withDesiredCapabilities(DesiredCapabilities.chrome());
 
     @Test
     public void honorPresetNoProxyEnvironment() {
-        Object no_proxy = chromeWithNoProxySet.getEnvMap().get("no_proxy");
-        assertEquals("no_proxy should be preserved by the container rule", NO_PROXY_VALUE, no_proxy);
+        BrowserWebDriverContainer chromeWithNoProxySet = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+            .withDesiredCapabilities(DesiredCapabilities.chrome())
+            .withEnv(NO_PROXY_KEY, NO_PROXY_VALUE);
+
+        try {
+            chromeWithNoProxySet.start();
+
+            Object noProxy = chromeWithNoProxySet.getEnvMap().get(NO_PROXY_KEY);
+            assertEquals("no_proxy should be preserved by the container rule", NO_PROXY_VALUE, noProxy);
+        } finally {
+            chromeWithNoProxySet.stop();
+        }
     }
 
     @Test
     public void provideDefaultNoProxyEnvironmentIfNotSet() {
-        Object no_proxy = chromeWithoutNoProxySet.getEnvMap().get("no_proxy");
-        assertEquals("no_proxy should be set to default if not already present", "localhost", no_proxy);
+        BrowserWebDriverContainer chromeWithoutNoProxySet = new BrowserWebDriverContainer()
+            .withDesiredCapabilities(DesiredCapabilities.chrome());
+
+        try {
+            chromeWithoutNoProxySet.start();
+
+            Object noProxy = chromeWithoutNoProxySet.getEnvMap().get(NO_PROXY_KEY);
+            assertEquals("no_proxy should be set to default if not already present", "localhost", noProxy);
+        } finally {
+            chromeWithoutNoProxySet.stop();
+        }
     }
 }
