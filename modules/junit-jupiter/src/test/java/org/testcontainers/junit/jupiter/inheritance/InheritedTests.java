6fcca79ea73d98fcf498e2fccba11335ea27976c
Benedikt Ritter <beneritter@gmail.com>
2018-11-02 09:54:59 +0100

    Add JUnit Jupiter (JUnit 5) extension (#887)

---
+++ modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/inheritance/InheritedTests.java
@@ -0,0 +1,28 @@
+package org.testcontainers.junit.jupiter.inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+class InheritedTests extends AbstractTestBase {
+
+    @Container
+    private RedisContainer myRedis = new RedisContainer();
+
+    @Test
+    void step1() {
+        assertEquals(1, redisPerClass.getJedis().incr("key").longValue());
+        assertEquals(1, redisPerTest.getJedis().incr("key").longValue());
+        assertEquals(1, myRedis.getJedis().incr("key").longValue());
+    }
+
+    @Test
+    void step2() {
+        assertEquals(2, redisPerClass.getJedis().incr("key").longValue());
+        assertEquals(1, redisPerTest.getJedis().incr("key").longValue());
+        assertEquals(1, myRedis.getJedis().incr("key").longValue());
+    }
+}
