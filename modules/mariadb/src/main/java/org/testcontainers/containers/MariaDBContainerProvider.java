6a65509beaef8c05242633d2225662f38a897802
Oded Arbel <oded@geek.co.il>
2018-11-10 18:20:14 +0200

    Support using MariaDB JDBC URL to set database name, user and password (#950)
    Implement feature compatibility with MySQL container requested in issue #949

--- modules/mariadb/src/main/java/org/testcontainers/containers/MariaDBContainerProvider.java
+++ modules/mariadb/src/main/java/org/testcontainers/containers/MariaDBContainerProvider.java
@@ -1,9 +1,18 @@
 package org.testcontainers.containers;
 
+import org.testcontainers.jdbc.ConnectionUrl;
+
+import java.util.Objects;
+
 /**
  * Factory for MariaDB org.testcontainers.containers.
  */
 public class MariaDBContainerProvider extends JdbcDatabaseContainerProvider {
+    
+    private static final String USER_PARAM = "user";
+
+    private static final String PASSWORD_PARAM = "password";
+
     @Override
     public boolean supports(String databaseType) {
         return databaseType.equals(MariaDBContainer.NAME);
@@ -18,4 +27,26 @@ public class MariaDBContainerProvider extends JdbcDatabaseContainerProvider {
     public JdbcDatabaseContainer newInstance(String tag) {
         return new MariaDBContainer(MariaDBContainer.IMAGE + ":" + tag);
     }
+    
+    @Override
+    public JdbcDatabaseContainer newInstance(ConnectionUrl connectionUrl) {
+        Objects.requireNonNull(connectionUrl, "Connection URL cannot be null");
+
+        final String databaseName = connectionUrl.getDatabaseName().orElse("test");
+        final String user = connectionUrl.getQueryParameters().getOrDefault(USER_PARAM, "test");
+        final String password = connectionUrl.getQueryParameters().getOrDefault(PASSWORD_PARAM, "test");
+
+        final JdbcDatabaseContainer instance;
+        if (connectionUrl.getImageTag().isPresent()) {
+            instance = newInstance(connectionUrl.getImageTag().get());
+        } else {
+            instance = newInstance();
+        }
+
+        return instance
+            .withDatabaseName(databaseName)
+            .withUsername(user)
+            .withPassword(password);
+    }
+    
 }
