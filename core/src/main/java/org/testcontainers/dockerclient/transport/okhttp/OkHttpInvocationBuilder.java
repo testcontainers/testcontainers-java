1ee83e0e8e5a81f2f9bff329fa29a341d9775803
Victor Verbitsky <vektor@krista.ru>
2018-12-27 10:31:20 +0300

    Issue #1100. Remove the redundant import.

--- core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/OkHttpInvocationBuilder.java
+++ core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/OkHttpInvocationBuilder.java
@@ -35,7 +35,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
-import java.nio.ByteBuffer;
 import java.util.Objects;
 import java.util.function.Consumer;
 
