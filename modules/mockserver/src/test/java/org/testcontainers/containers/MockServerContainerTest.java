c4028729a50e22d2a4e92f1c21aaebcd232f7478
Kirill Merkushev <lanwen@yandex.ru>
2019-01-02 10:24:38 +0100

    test default mockserver constructor

--- modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
+++ modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
@@ -1,39 +1,49 @@
 package org.testcontainers.containers;
 
 import lombok.Cleanup;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.mockserver.client.MockServerClient;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
-import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertThat;
 
-@Slf4j
 public class MockServerContainerTest {
 
     @ClassRule
-    public static MockServerContainer mockServer = new MockServerContainer(MockServerClient.class.getPackage().getImplementationVersion())
-        .withLogConsumer(new Slf4jLogConsumer(log));
+    public static MockServerContainer mockServer = new MockServerContainer(MockServerClient.class.getPackage().getImplementationVersion());
+
+    @ClassRule
+    public static MockServerContainer mockServerDefault = new MockServerContainer();
 
     @Test
-    public void testBasicScenario() throws Exception {
-        new MockServerClient(mockServer.getContainerIpAddress(), mockServer.getServerPort())
-            .when(request("/hello"))
-            .respond(response("Hello World!"));
+    public void shouldCallActualMockserverVersion() throws Exception {
+        shouldCallMockserver(mockServer, "Hello World!", "/hello");
+    }
 
-        URLConnection urlConnection = new URL(mockServer.getEndpoint() + "/hello").openConnection();
+    @Test
+    public void shouldCallDefaultMockserverVersion() throws Exception {
+        shouldCallMockserver(mockServerDefault, "Hello Default World!", "/hellodefault");
+    }
+
+    private static void shouldCallMockserver(MockServerContainer mockServer, String expectedBody, String path) throws IOException {
+        new MockServerClient(mockServer.getContainerIpAddress(), mockServer.getServerPort())
+            .when(request(path))
+            .respond(response(expectedBody));
+
+        URLConnection urlConnection = new URL(mockServer.getEndpoint() + path).openConnection();
         @Cleanup BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
         String line = reader.readLine();
         System.out.println(line);
 
-        assertTrue("MockServer returns correct result", line.contains("Hello World!"));
+        assertThat("MockServer returns correct result", line, containsString(expectedBody));
     }
 }
