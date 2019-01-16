9039acb89f92074acf92e2fc390b80ca972592f0
stsefanenko <stsefanenko@gmail.com>
2019-01-16 17:55:05 +0300

    "exposing host port" example. Closes #1136 (#1152)
    * added spring boot webapp test with exposed host port for BrowserWebDriverContainer
    
    * fixes after review
    
    * fixes after review update to random port
    
    * fixes after review update to static resource
    
    * fixes after review update initializer
    
    * fixes after review remove unused dependency

--- examples/selenium-container/src/test/java/SeleniumContainerTest.java
+++ examples/selenium-container/src/test/java/SeleniumContainerTest.java
@@ -1,8 +1,19 @@
+import com.example.DemoApplication;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 
 import java.io.File;
@@ -14,7 +25,13 @@ import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordi
 /**
  * Simple example of plain Selenium usage.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = SeleniumContainerTest.Initializer.class)
 public class SeleniumContainerTest {
+
+    @LocalServerPort
+    private int port;
 
     @Rule
     public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
@@ -25,9 +42,19 @@ public class SeleniumContainerTest {
     public void simplePlainSeleniumTest() {
         RemoteWebDriver driver = chrome.getWebDriver();
 
-        driver.get("https://wikipedia.org");
-        List<WebElement> searchInput = driver.findElementsByName("search");
+        driver.get("http://host.testcontainers.internal:" + port + "/foo.html");
+        List<WebElement> hElement = driver.findElementsByTagName("h");
 
-        assertTrue("The search input box is found", searchInput != null && searchInput.size() > 0);
+        assertTrue("The h element is found", hElement != null && hElement.size() > 0);
     }
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            applicationContext.addApplicationListener((ApplicationListener<WebServerInitializedEvent>) event -> {
+                Testcontainers.exposeHostPorts(event.getWebServer().getPort());
+            });
+        }
+    }
+
 }
