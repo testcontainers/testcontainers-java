2cf521f81ef75b7fb8bd3d27ab79a05b30ea2674
Author Name <david.bereslavich@gmail.com>
2019-01-16 22:25:22 +0200

    mysql 8 container can't start
    mysql 8 container needs "mysqld --default-authentication-plugin=mysql_native_password" command to start

--- modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainerProvider.java
+++ modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainerProvider.java
@@ -8,10 +8,9 @@ import java.util.Objects;
  * Factory for MySQL containers.
  */
 public class MySQLContainerProvider extends JdbcDatabaseContainerProvider {
-
     private static final String USER_PARAM = "user";
-
     private static final String PASSWORD_PARAM = "password";
+    private static final int MIN_MYSQL_VERSION = 8;
 
     @Override
     public boolean supports(String databaseType) {
@@ -46,11 +45,32 @@ public class MySQLContainerProvider extends JdbcDatabaseContainerProvider {
         } else {
             instance = newInstance();
         }
-
+        
+        if (mySqlNativePasswordIsNeeded(connectionUrl)) {
+            instance.withCommand("mysqld --default-authentication-plugin=mysql_native_password");
+        }
+        
         return instance
             .withDatabaseName(databaseName)
             .withUsername(user)
             .withPassword(password);
     }
-
+    
+    public boolean mySqlNativePasswordIsNeeded(ConnectionUrl connectionUrl) {
+        String tag  = connectionUrl.getImageTag().get();
+        int version = 0;
+        
+        if(Objects.isNull(tag) || Objects.equals(tag, "latest") || tag.isEmpty()) {
+            return true;
+        }
+        try {
+            version = Integer.parseInt(tag.split("\\.")[0]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (version >= MIN_MYSQL_VERSION) {
+            return true;
+        }
+        return false;
+    }
 }
