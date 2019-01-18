c39cd81eac469fef3a235c859aaec8ca4141df44
Artjom Kalita <artjom.kalita@gmail.com>
2019-01-18 15:04:21 +0200

    Use static approach for configuring redis container

--- examples/spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
+++ examples/spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
@@ -1,33 +1,21 @@
 package com.example;
 
-import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.containers.GenericContainer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = DemoApplication.class,webEnvironment = WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIntegrationTest {
 
-    @ClassRule
-    public static GenericContainer redis = new GenericContainer("redis:3.0.6").withExposedPorts(6379);
+    static {
+        GenericContainer redis = new GenericContainer("redis:3-alpine")
+            .withExposedPorts(6379);
+        redis.start();
 
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        @Override
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues values = TestPropertyValues.of(
-                    "spring.redis.host=" + redis.getContainerIpAddress(),
-                    "spring.redis.port=" + redis.getMappedPort(6379)
-            );
-            values.applyTo(configurableApplicationContext);
-        }
+        System.setProperty("spring.redis.host", redis.getContainerIpAddress());
+        System.setProperty("spring.redis.port", redis.getFirstMappedPort() + "");
     }
-
 }
