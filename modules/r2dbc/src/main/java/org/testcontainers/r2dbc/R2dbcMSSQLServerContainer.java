e21790f6eb1d4109dbb3de12e45c5c9835e003ac
humblehound <lmyslinski@yameo.pl>
2019-01-20 00:28:14 +0100

    r2dbc single module support

---
+++ modules/r2dbc/src/main/java/org/testcontainers/r2dbc/R2dbcMSSQLServerContainer.java
@@ -0,0 +1,22 @@
+package org.testcontainers.r2dbc;
+
+import io.r2dbc.mssql.MssqlConnectionConfiguration;
+import io.r2dbc.mssql.MssqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import org.testcontainers.containers.MSSQLServerContainer;
+
+/**
+ * @author humblehound
+ */
+public class R2dbcMSSQLServerContainer extends MSSQLServerContainer implements R2dbcSupport {
+
+    @Override
+    public ConnectionFactory getR2dbcConnectionFactory() {
+        return new MssqlConnectionFactory(MssqlConnectionConfiguration.builder()
+            .host(getContainerIpAddress())
+            .password(getPassword())
+            .username(getUsername())
+            .port(getFirstMappedPort())
+            .build());
+    }
+}
---
+++ modules/r2dbc/src/test/java/SimpleR2dbcMSSQLServerTest.java
@@ -0,0 +1,22 @@
+import io.r2dbc.client.R2dbc;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.r2dbc.R2dbcMSSQLServerContainer;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
+
+/**
+ * @author humblehound
+ */
+public class SimpleR2dbcMSSQLServerTest {
+
+    @Rule
+    public R2dbcMSSQLServerContainer mssqlServer = new R2dbcMSSQLServerContainer();
+
+    @Test
+    public void testR2dbcConnectionSuccessful() {
+        R2dbc r2dbc = new R2dbc(mssqlServer.getR2dbcConnectionFactory());
+        Object resultSetInt =  r2dbc.inTransaction(handle -> handle.createQuery("SELECT 1").mapRow(row -> row.get(""))).blockFirst();
+        assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
+    }
+}
