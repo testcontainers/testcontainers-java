adb4bac5dfaf11ba9f64af899644857c649196bb
Artjom Kalita <artjom.kalita@gmail.com>
2019-01-23 18:30:25 +0200

    Cleanup + moving test specific configuration from main config

--- examples/spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
+++ examples/spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
@@ -3,6 +3,7 @@ package com.example;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.containers.GenericContainer;
 
@@ -11,13 +12,13 @@ import org.testcontainers.containers.GenericContainer;
 properties = {
     "spring.datasource.url=jdbc:tc:postgresql:11-alpine://localhost/databasename"
 })
+@ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
 
     static {
         GenericContainer redis = new GenericContainer("redis:3-alpine")
             .withExposedPorts(6379);
         redis.start();
-
 
         System.setProperty("spring.redis.host", redis.getContainerIpAddress());
         System.setProperty("spring.redis.port", redis.getFirstMappedPort() + "");
