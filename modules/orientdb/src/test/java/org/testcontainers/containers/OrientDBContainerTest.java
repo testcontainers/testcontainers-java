e3798c4c1ba25949f2dac151d12f85f0c509a03c
robfrank <ro.franchini@gmail.com>
2019-02-16 20:28:33 +0100

    initial commit for OrientDB container support module

---
+++ modules/orientdb/src/test/java/org/testcontainers/containers/OrientDBContainerTest.java
@@ -0,0 +1,59 @@
+package org.testcontainers.containers;
+
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrientDBContainerTest {
+
+
+    @Test
+    public void shouldReturnTheSameSession() {
+        OrientDBContainer container = new OrientDBContainer();
+        container.start();
+
+        final ODatabaseSession session = container.getSession();
+        final ODatabaseSession session2 = container.getSession();
+
+        assertThat(session).isSameAs(session2);
+    }
+
+    @Test
+    public void shouldInitializeWithCommands() {
+
+        OrientDBContainer container = new OrientDBContainer();
+        container.start();
+
+        final ODatabaseSession session = container.getSession();
+
+        session.command("CREATE CLASS Person EXTENDS V");
+        session.command("INSERT INTO Person set name='john'");
+        session.command("INSERT INTO Person set name='jane'");
+
+        assertThat(session.query("SELECT FROM Person").stream()).hasSize(2);
+
+        container.stop();
+
+    }
+
+    @Test
+    public void shouldInitializeDatabaseFromScript() {
+
+        OrientDBContainer container = new OrientDBContainer()
+            .withScriptPath("initscript.osql")
+            .withDatabaseName("persons");
+
+        container.start();
+
+        assertThat(container.getDbUrl())
+            .isEqualTo("remote:" + container.getContainerIpAddress() + ":" + container.getMappedPort(2424) + "/persons");
+
+        final ODatabaseSession session = container.getSession();
+
+        assertThat(session.query("SELECT FROM Person").stream()).hasSize(4);
+
+        container.stop();
+
+    }
+}
