1fe2804714887c0a43077901d5446daeb2c75ab1
Manik Magar <manik.magar@gmail.com>
2019-03-17 11:54:11 -0400

    Add JDBC custom credential support for postgres (#1293)

--- modules/mariadb/src/main/java/org/testcontainers/containers/MariaDBContainerProvider.java
+++ modules/mariadb/src/main/java/org/testcontainers/containers/MariaDBContainerProvider.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * Factory for MariaDB org.testcontainers.containers.
  */
 public class MariaDBContainerProvider extends JdbcDatabaseContainerProvider {
-    
+
     private static final String USER_PARAM = "user";
 
     private static final String PASSWORD_PARAM = "password";
@@ -27,26 +27,10 @@ public class MariaDBContainerProvider extends JdbcDatabaseContainerProvider {
     public JdbcDatabaseContainer newInstance(String tag) {
         return new MariaDBContainer(MariaDBContainer.IMAGE + ":" + tag);
     }
-    
+
     @Override
     public JdbcDatabaseContainer newInstance(ConnectionUrl connectionUrl) {
-        Objects.requireNonNull(connectionUrl, "Connection URL cannot be null");
-
-        final String databaseName = connectionUrl.getDatabaseName().orElse("test");
-        final String user = connectionUrl.getQueryParameters().getOrDefault(USER_PARAM, "test");
-        final String password = connectionUrl.getQueryParameters().getOrDefault(PASSWORD_PARAM, "test");
-
-        final JdbcDatabaseContainer instance;
-        if (connectionUrl.getImageTag().isPresent()) {
-            instance = newInstance(connectionUrl.getImageTag().get());
-        } else {
-            instance = newInstance();
-        }
-
-        return instance
-            .withDatabaseName(databaseName)
-            .withUsername(user)
-            .withPassword(password);
+       return newInstanceFromConnectionUrl(connectionUrl, USER_PARAM, PASSWORD_PARAM);
     }
-    
+
 }
