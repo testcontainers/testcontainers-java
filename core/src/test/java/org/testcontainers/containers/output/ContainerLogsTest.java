e0d4ae69c8628830c2ad756d0e3f3d86582cd75f
Richard North <rich.north@gmail.com>
2019-03-23 22:32:16 +0000

    Update tests to include long-running container

--- core/src/test/java/org/testcontainers/containers/output/ContainerLogsTest.java
+++ core/src/test/java/org/testcontainers/containers/output/ContainerLogsTest.java
@@ -1,36 +1,66 @@
 package org.testcontainers.containers.output;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 import static org.testcontainers.containers.output.OutputFrame.OutputType.STDERR;
 import static org.testcontainers.containers.output.OutputFrame.OutputType.STDOUT;
 
 public class ContainerLogsTest {
 
-    @Rule
-    public GenericContainer container = new GenericContainer("alpine:3.3")
-        .withCommand("/bin/sh", "-c", "echo -n 'stdout' && echo -n 'stderr' 1>&2")
-        .withStartupCheckStrategy(new OneShotStartupCheckStrategy());
-
     @Test
     public void getLogsReturnsAllLogsToDate() {
-        final String logs = container.getLogs();
-        assertEquals("stdout and stderr are reflected in the returned logs", "stdout\nstderr", logs);
+        try (GenericContainer container = shortLivedContainer()) {
+            container.start();
+
+            final String logs = container.getLogs();
+            assertEquals("stdout and stderr are reflected in the returned logs", "stdout\nstderr", logs);
+        }
     }
 
     @Test
     public void getLogsReturnsStdOutToDate() {
-        final String logs = container.getLogs(STDOUT);
-        assertEquals("stdout and stderr are reflected in the returned logs", "stdout", logs);
+        try (GenericContainer container = shortLivedContainer()) {
+            container.start();
+
+            final String logs = container.getLogs(STDOUT);
+            assertEquals("stdout and stderr are reflected in the returned logs", "stdout", logs);
+        }
     }
 
     @Test
     public void getLogsReturnsStdErrToDate() {
-        final String logs = container.getLogs(STDERR);
-        assertEquals("stdout and stderr are reflected in the returned logs", "stderr", logs);
+        try (GenericContainer container = shortLivedContainer()) {
+            container.start();
+
+            final String logs = container.getLogs(STDERR);
+            assertEquals("stdout and stderr are reflected in the returned logs", "stderr", logs);
+        }
+    }
+
+    @Test
+    public void getLogsForLongRunningContainer() throws InterruptedException {
+        try (GenericContainer container = longRunningContainer()) {
+            container.start();
+
+            Thread.sleep(1000L);
+
+            final String logs = container.getLogs(STDOUT);
+            assertTrue("stdout is reflected in the returned logs for a running container", logs.contains("seq=0"));
+        }
+    }
+
+    private static GenericContainer shortLivedContainer() {
+        return new GenericContainer("alpine:3.3")
+            .withCommand("/bin/sh", "-c", "echo -n 'stdout' && echo -n 'stderr' 1>&2")
+            .withStartupCheckStrategy(new OneShotStartupCheckStrategy());
+    }
+
+    private static GenericContainer longRunningContainer() {
+        return new GenericContainer("alpine:3.3")
+            .withCommand("ping -c 100 127.0.0.1");
     }
 }
