e5a158d344675ecad04e721044c7bc16892d0a80
Alexander Sparkowsky <info@roamingthings.de>
2019-03-27 09:07:31 +0100

    Add test for post condition when signalling lifecycleaware containers
    This kind of test is a bit tricky since the post condition occurs
    after the original test has been finished. Also it's not nice to
    pass data between two tests.

--- modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareSignallingTest.java
+++ modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareSignallingTest.java
@@ -1,17 +1,31 @@
 package org.testcontainers.junit.jupiter;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.Assert.assertEquals;
 
 @Testcontainers
+@TestMethodOrder(OrderAnnotation.class)
 class TestLifecycleAwareSignallingTest {
 
     @Container
     private final TestLifecycleAwareContainerMock testContainer = new TestLifecycleAwareContainerMock();
 
+    static private TestLifecycleAwareContainerMock startedTestContainer;
+
     @Test
+    @Order(1)
     void should_call_beforeTest_on_TestLifecycleAware_container() {
         assertEquals(1, testContainer.getNumBeforeTestsCalls());
+        startedTestContainer = testContainer;
+    }
+
+    @Test
+    @Order(2)
+    void should_call_afterTest_on_TestLifecycleAware_container() {
+        assertEquals(1, startedTestContainer.getNumAfterTestsCalls());
     }
 }
