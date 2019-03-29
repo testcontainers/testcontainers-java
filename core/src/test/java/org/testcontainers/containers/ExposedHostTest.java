da28a40b0aa6d5158c8b9fd140bc767dbb9573f7
Bodo <Bodo@LAPTOP-CTHGBKOT.intern.dasburo.com>
2019-03-29 15:37:03 +0100

    hostPort != containerPort

--- core/src/test/java/org/testcontainers/containers/ExposedHostTest.java
+++ core/src/test/java/org/testcontainers/containers/ExposedHostTest.java
@@ -9,12 +9,15 @@ import org.testcontainers.Testcontainers;
 
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
 public class ExposedHostTest {
 
     private static HttpServer server;
+    private static HttpServer serverPortMapping;    
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -30,31 +33,59 @@ public class ExposedHostTest {
 
         server.start();
         Testcontainers.exposeHostPorts(server.getAddress().getPort());
+        
+        serverPortMapping = HttpServer.create(new InetSocketAddress(0), 0);
+        serverPortMapping.createContext("/", exchange -> {
+            byte[] content = "Hello World!".getBytes();
+            exchange.sendResponseHeaders(200, content.length);
+            try (OutputStream responseBody = exchange.getResponseBody()) {
+                responseBody.write(content);
+                responseBody.flush();
+            }
+        });
+
+        serverPortMapping.start();
+        Map<Integer, Integer> portMapping = new HashMap<>();
+        portMapping.put(serverPortMapping.getAddress().getPort(), 80);
+        Testcontainers.exposeHostPorts(portMapping);        
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
         server.stop(0);
+        serverPortMapping.stop(0);        
     }
 
     @Test
     public void testExposedHost() throws Exception {
-        assertResponse(new GenericContainer().withCommand("top"));
+        assertResponse(new GenericContainer().withCommand("top"), server.getAddress().getPort());
     }
 
     @Test
     public void testExposedHostWithNetwork() throws Exception {
         try (Network network = Network.newNetwork()) {
-            assertResponse(new GenericContainer().withNetwork(network).withCommand("top"));
+            assertResponse(new GenericContainer().withNetwork(network).withCommand("top"), server.getAddress().getPort());
         }
     }
+    
+    @Test
+    public void testExposedHostPortMapping() throws Exception {
+        assertResponse(new GenericContainer().withCommand("top"), 80);
+    }
+
+    @Test
+    public void testExposedHostWithNetworkPortMapping() throws Exception {
+        try (Network network = Network.newNetwork()) {
+            assertResponse(new GenericContainer().withNetwork(network).withCommand("top"), 80);
+        }
+    }    
 
     @SneakyThrows
-    protected void assertResponse(GenericContainer container) {
+    protected void assertResponse(GenericContainer container, int port) {
         try {
             container.start();
 
-            String response = container.execInContainer("wget", "-O", "-", "http://host.testcontainers.internal:" + server.getAddress().getPort()).getStdout();
+            String response = container.execInContainer("wget", "-O", "-", "http://host.testcontainers.internal:" + port).getStdout();
 
             assertEquals("received response", "Hello World!", response);
         } finally {
