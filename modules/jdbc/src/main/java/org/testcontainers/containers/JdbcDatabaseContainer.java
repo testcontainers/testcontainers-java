3faff3a245aaddcdd5f5eb64911336d8e8d91131
Richard North <rich.north@gmail.com>
2019-04-28 21:45:21 +0100

    Catch correct exception class

--- modules/jdbc/src/main/java/org/testcontainers/containers/JdbcDatabaseContainer.java
+++ modules/jdbc/src/main/java/org/testcontainers/containers/JdbcDatabaseContainer.java
@@ -2,6 +2,7 @@ package org.testcontainers.containers;
 
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import lombok.NonNull;
+import org.awaitility.core.ConditionTimeoutException;
 import org.jetbrains.annotations.NotNull;
 import org.testcontainers.containers.traits.LinkableContainer;
 import org.testcontainers.delegate.DatabaseDelegate;
@@ -189,7 +190,7 @@ public abstract class JdbcDatabaseContainer<SELF extends JdbcDatabaseContainer<S
                 .pollDelay(0, SECONDS)
                 .pollInterval(5, SECONDS)
                 .until(() -> jdbcDriverInstance.connect(url, info), __ -> true);
-        } catch (Exception e) {
+        } catch (ConditionTimeoutException e) {
             throw new SQLException("Could not create new connection", e);
         }
     }
