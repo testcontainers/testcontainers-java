c7baa8fc65e36f0e3f6edbe4f1bf4fe998d68edc
Martin Greber <martin.greber@gmail.com>
2019-05-04 17:27:03 +1000

    Removed unused exception class

--- modules/rabbitmq/src/main/java/org/testcontainers/containers/ContainerConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package org.testcontainers.containers;
-
-public class ContainerConfigurationException extends RuntimeException {
-    public ContainerConfigurationException(String message) {
-        super(message);
-    }
-}
