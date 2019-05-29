0e70832933991009f5b4d0c134bdfd36a9459426
Greg Biles <greg.biles@niceincontact.com>
2019-05-29 09:23:29 -0600

    Added a simple secretsmanager test that creates and fetches a secret.

--- modules/localstack/src/test/java/org/testcontainers/containers/localstack/SimpleLocalstackTest.java
+++ modules/localstack/src/test/java/org/testcontainers/containers/localstack/SimpleLocalstackTest.java
@@ -6,10 +6,16 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.CreateQueueResult;
 import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.CreateSecretRequest;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -17,12 +23,14 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SECRETSMANAGER;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
 public class SimpleLocalstackTest {
@@ -34,7 +42,7 @@ public class SimpleLocalstackTest {
 
     @ClassRule
     public static LocalStackContainer localstack = new LocalStackContainer()
-        .withServices(S3, SQS);
+        .withServices(S3, SECRETSMANAGER, SQS);
 
     @Test
     public void simpleS3Test() throws IOException {
@@ -85,5 +93,26 @@ public class SimpleLocalstackTest {
 
         assertTrue("the message queue contains a message", messages.size() > 0);
         assertEquals("the first message is the one we sent", "ping", messages.get(0).getBody());
+    }
+
+    @Test
+    public void simpleSecretsManagerTest() throws IOException {
+        // Create and fetch a secret
+        AWSSecretsManager secretsManager = AWSSecretsManagerClientBuilder
+            .standard()
+            .withEndpointConfiguration(localstack.getEndpointConfiguration(SECRETSMANAGER))
+            .withCredentials(localstack.getDefaultCredentialsProvider())
+            .build();
+
+        CreateSecretRequest createSecretRequest = new CreateSecretRequest();
+        createSecretRequest.setName("my-secret-name");
+        createSecretRequest.setSecretString("{ \"value\": \"this is a secret thing\" }");
+        secretsManager.createSecret(createSecretRequest);
+
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId("my-secret-name");
+        String result = secretsManager.getSecretValue(getSecretValueRequest).getSecretString();
+        Map<String, String> map = new ObjectMapper().readValue(result, new TypeReference<Map<String,String>>(){});
+        assertEquals("The secret was successfully read", "this is a secret thing", map.get("value"));
+
     }
 }
