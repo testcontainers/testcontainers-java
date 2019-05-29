2d1cb81a7ad50e11fe2f31520a354d3c28774347
Greg Biles <greg.biles@niceincontact.com>
2019-05-29 10:27:24 -0600

    Removed the secretsmanager test as it is not needed because there is no special testscontainers code needed to work with that localstack service.  I've replaced the test with a comment explaining which types of tests are needed around Localstack.

--- modules/localstack/src/test/java/org/testcontainers/containers/localstack/SimpleLocalstackTest.java
+++ modules/localstack/src/test/java/org/testcontainers/containers/localstack/SimpleLocalstackTest.java
@@ -6,16 +6,10 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.CreateQueueResult;
 import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.amazonaws.services.secretsmanager.model.CreateSecretRequest;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -23,16 +17,18 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SECRETSMANAGER;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
+/**
+ * The only tests that should go here are for those Localstack services that require special testcontainers code to work with them.
+ * For example, S3 needs a test to ensure special HTTP routing to the S3 container works.
+ */
 public class SimpleLocalstackTest {
 
     private static final String BUCKET_1 = "bucket1";
@@ -42,7 +38,7 @@ public class SimpleLocalstackTest {
 
     @ClassRule
     public static LocalStackContainer localstack = new LocalStackContainer()
-        .withServices(S3, SECRETSMANAGER, SQS);
+        .withServices(S3, SQS);
 
     @Test
     public void simpleS3Test() throws IOException {
@@ -93,26 +89,5 @@ public class SimpleLocalstackTest {
 
         assertTrue("the message queue contains a message", messages.size() > 0);
         assertEquals("the first message is the one we sent", "ping", messages.get(0).getBody());
-    }
-
-    @Test
-    public void simpleSecretsManagerTest() throws IOException {
-        // Create and fetch a secret
-        AWSSecretsManager secretsManager = AWSSecretsManagerClientBuilder
-            .standard()
-            .withEndpointConfiguration(localstack.getEndpointConfiguration(SECRETSMANAGER))
-            .withCredentials(localstack.getDefaultCredentialsProvider())
-            .build();
-
-        CreateSecretRequest createSecretRequest = new CreateSecretRequest();
-        createSecretRequest.setName("my-secret-name");
-        createSecretRequest.setSecretString("{ \"value\": \"this is a secret thing\" }");
-        secretsManager.createSecret(createSecretRequest);
-
-        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId("my-secret-name");
-        String result = secretsManager.getSecretValue(getSecretValueRequest).getSecretString();
-        Map<String, String> map = new ObjectMapper().readValue(result, new TypeReference<Map<String,String>>(){});
-        assertEquals("The secret was successfully read", "this is a secret thing", map.get("value"));
-
     }
 }
