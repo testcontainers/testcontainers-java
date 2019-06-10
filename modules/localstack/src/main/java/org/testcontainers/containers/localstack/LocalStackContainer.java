119fdd30cd8ac5f38d065de1e0a2bc1e899ca73a
Richard North <rich.north@gmail.com>
2019-06-10 18:59:31 +0100

    Update localstack to 0.9.4

--- modules/localstack/src/main/java/org/testcontainers/containers/localstack/LocalStackContainer.java
+++ modules/localstack/src/main/java/org/testcontainers/containers/localstack/LocalStackContainer.java
@@ -28,7 +28,7 @@ import java.util.stream.Collectors;
  */
 public class LocalStackContainer extends GenericContainer<LocalStackContainer> {
 
-    public static final String VERSION = "0.9.2";
+    public static final String VERSION = "0.9.4";
 
     private final List<Service> services = new ArrayList<>();
 
