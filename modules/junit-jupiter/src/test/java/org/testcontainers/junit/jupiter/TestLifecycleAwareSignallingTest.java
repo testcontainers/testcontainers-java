ee1dae744008ca7e92b993d6290e1cfed845d525
Alexander Sparkowsky <info@roamingthings.de>
2019-07-23 22:30:03 +0200

    Update test for lifecycle aware containers to cover shared case (#1326)
    In order to check that the afterAll callback has signalled
    lifecycleaware containers correctly a second extension is used. The
    order of the extension annotation ensures that the assertion is run
    after the extension under test.

--- modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareSignallingTest.java
+++ modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareSignallingTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static org.junit.Assert.assertEquals;
 
+// The order of @ExtendsWith and @Testcontainers is crucial in order for the static case to be tested
+@ExtendWith({TestLifecycleAwareSignallingTest.ExtensionSharedContainerTestExtension.class})
 @Testcontainers
 @TestMethodOrder(OrderAnnotation.class)
 class TestLifecycleAwareSignallingTest {
@@ -14,7 +19,10 @@ class TestLifecycleAwareSignallingTest {
     @Container
     private final TestLifecycleAwareContainerMock testContainer = new TestLifecycleAwareContainerMock();
 
-    static private TestLifecycleAwareContainerMock startedTestContainer;
+    @Container
+    private static final TestLifecycleAwareContainerMock SHARED_CONTAINER = new TestLifecycleAwareContainerMock();
+
+    private static TestLifecycleAwareContainerMock startedTestContainer;
 
     @Test
     @Order(1)
@@ -27,5 +35,15 @@ class TestLifecycleAwareSignallingTest {
     @Order(2)
     void should_call_afterTest_on_TestLifecycleAware_container() {
         assertEquals(1, startedTestContainer.getNumAfterTestsCalls());
-    } 
+    }
+
+    static class ExtensionSharedContainerTestExtension implements AfterAllCallback {
+
+        @Override
+        public void afterAll(ExtensionContext context) {
+            assertEquals(1, SHARED_CONTAINER.getNumAfterTestsCalls());
+        }
+    }
 }
+
+
