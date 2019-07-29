dfd3394f4d459521b9cfa77372bd689a17de652d
Richard North <rich.north@gmail.com>
2019-07-29 20:29:05 +0100

    Add some targeted @Flaky annotations

--- modules/jdbc-test/src/test/java/org/testcontainers/jdbc/DatabaseDriverTmpfsTest.java
+++ modules/jdbc-test/src/test/java/org/testcontainers/jdbc/DatabaseDriverTmpfsTest.java
@@ -1,18 +1,26 @@
 package org.testcontainers.jdbc;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.testsupport.Flaky;
+import org.testcontainers.testsupport.FlakyTestJUnit4RetryRule;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import org.junit.Test;
-import org.testcontainers.containers.Container;
-import org.testcontainers.containers.JdbcDatabaseContainer;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
 public class DatabaseDriverTmpfsTest {
 
+    @Rule
+    public FlakyTestJUnit4RetryRule retry = new FlakyTestJUnit4RetryRule();
+
     @Test
+    @Flaky(rationale = "") // TODO: describe
     public void tmpfs() throws IOException, InterruptedException, SQLException {
         final String jdbcUrl = "jdbc:tc:postgresql:9.6.8://hostname/databasename?TC_TMPFS=/testtmpfs:rw";
         try (Connection ignored = DriverManager.getConnection(jdbcUrl)) {
