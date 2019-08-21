918af4ea094cce63c0b347941b014af2f37287a3
Dan Markhasin <dan.markhasin@intel.com>
2019-08-21 09:39:26 +0300

    use redis instead of alpine in example

--- docs/examples/src/test/java/generic/ImagePullPolicyTest.java
+++ docs/examples/src/test/java/generic/ImagePullPolicyTest.java
@@ -11,13 +11,13 @@ public class ImagePullPolicyTest {
 
     // image_pull_policy {
     @ClassRule
-    public static GenericContainer containerAlwaysPull = new GenericContainer("alpine:3.6")
+    public static GenericContainer containerAlwaysPull = new GenericContainer("redis:3.0.2")
         .withImagePullPolicy(PullPolicy.alwaysPull());
     // }
 
     // custom_image_pull_policy {
     @ClassRule
-    public static GenericContainer containerCustomPullPolicy = new GenericContainer("alpine:3.6")
+    public static GenericContainer containerCustomPullPolicy = new GenericContainer("redis:3.0.2")
         .withImagePullPolicy(image -> System.getenv("ALWAYS_PULL_IMAGE") != null);
     // }
 
