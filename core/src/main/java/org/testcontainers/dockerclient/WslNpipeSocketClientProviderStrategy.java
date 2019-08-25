8c9384a53a526db74dd07fc766a4a847a5f209d1
Joseph Petersen <josephp90@gmail.com>
2019-08-25 19:52:57 +0200

    add WSL 2 Named pipe strategy

--- core/src/main/java/org/testcontainers/dockerclient/WslNpipeSocketClientProviderStrategy.java
+++ core/src/main/java/org/testcontainers/dockerclient/WslNpipeSocketClientProviderStrategy.java
@@ -2,24 +2,23 @@ package org.testcontainers.dockerclient;
 
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.SystemUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.net.URI;
-
 @Slf4j
-public class NpipeSocketClientProviderStrategy extends DockerClientProviderStrategy {
+public class WslNpipeSocketClientProviderStrategy extends DockerClientProviderStrategy {
 
-    protected static final String DOCKER_SOCK_PATH = "//./pipe/docker_engine";
+    protected static final String DOCKER_SOCK_PATH = "//./pipe/docker_wsl";
     private static final String SOCKET_LOCATION = "npipe://" + DOCKER_SOCK_PATH;
 
     private static final String PING_TIMEOUT_DEFAULT = "10";
     private static final String PING_TIMEOUT_PROPERTY_NAME = "testcontainers.npipesocketprovider.timeout";
 
-    public static final int PRIORITY = EnvironmentAndSystemPropertyClientProviderStrategy.PRIORITY - 30;
+    public static final int PRIORITY = EnvironmentAndSystemPropertyClientProviderStrategy.PRIORITY - 20;
 
     @Override
     protected boolean isApplicable() {
@@ -61,7 +60,7 @@ public class NpipeSocketClientProviderStrategy extends DockerClientProviderStrat
 
     @Override
     public String getDescription() {
-        return "local Npipe socket (" + SOCKET_LOCATION + ")";
+        return "local WSL Npipe socket (" + SOCKET_LOCATION + ")";
     }
 
     @Override
