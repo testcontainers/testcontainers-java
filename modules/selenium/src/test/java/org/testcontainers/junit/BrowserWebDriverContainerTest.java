710fdd953968bc02d183c0618a7c3f42efdd3dc0
Guillaume Le Floch <glfloch@gmail.com>
2019-09-19 13:25:14 +0200

    add tests

--- modules/selenium/src/test/java/org/testcontainers/junit/BrowserWebDriverContainerTest.java
+++ modules/selenium/src/test/java/org/testcontainers/junit/BrowserWebDriverContainerTest.java
@@ -1,10 +1,13 @@
 package org.testcontainers.junit;
 
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class BrowserWebDriverContainerTest {
 
@@ -39,4 +42,33 @@ public class BrowserWebDriverContainerTest {
             assertEquals("no_proxy should be set to default if not already present", "localhost", noProxy);
         }
     }
+
+
+    @Test
+    public void createContainerWithShmVolume() {
+        try (
+            BrowserWebDriverContainer webDriverContainer = new BrowserWebDriverContainer()
+        ) {
+            webDriverContainer.start();
+
+            assertEquals("Shm mounts present", webDriverContainer.getContainerInfo().getMounts().size(), 1);
+            final InspectContainerResponse.Mount shmMount = webDriverContainer.getContainerInfo().getMounts().get(0);
+            assertEquals("Shm mount source is correct", "/dev/shm", shmMount.getSource());
+            assertEquals("Shm mount destination is correct", "/dev/shm", shmMount.getDestination().getPath());
+            assertEquals("Shm mount mode is correct", shmMount.getMode(), "rw");
+        }
+    }
+
+    @Test
+    public void createContainerWithoutShmVolume() {
+        try (
+            BrowserWebDriverContainer webDriverContainer = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+             .withSharedMemorySize(512 * FileUtils.ONE_MB)
+        ) {
+            webDriverContainer.start();
+            assertEquals("Shared memory size is configured", 512 * FileUtils.ONE_MB, webDriverContainer.getShmSize());
+            assertEquals("No mounts present", webDriverContainer.getContainerInfo().getMounts().size(), 0);
+        }
+    }
+
 }
