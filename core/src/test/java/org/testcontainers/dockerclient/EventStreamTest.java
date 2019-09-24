7971bfe981f3b5ff7aacfc48bbdabeb8d40dd306
Sergei Egorov <bsideup@gmail.com>
2019-09-24 11:15:28 +0300

    Make EventStreamTest resilient to host/daemon time differences

--- core/src/test/java/org/testcontainers/dockerclient/EventStreamTest.java
+++ core/src/test/java/org/testcontainers/dockerclient/EventStreamTest.java
@@ -5,7 +5,6 @@ import com.github.dockerjava.api.model.Event;
 import com.github.dockerjava.core.command.EventsResultCallback;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
-import org.rnorth.visibleassertions.VisibleAssertions;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
@@ -13,6 +12,8 @@ import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 /**
  * Test that event streaming from the {@link DockerClient} works correctly
@@ -24,41 +25,39 @@ public class EventStreamTest {
      */
     @Test
     public void test() throws IOException, InterruptedException {
-        DockerClient client = DockerClientFactory.instance().client();
-        Instant startTime = Instant.now();
-        final AtomicBoolean received = new AtomicBoolean(false);
+        AtomicBoolean received = new AtomicBoolean(false);
 
-        // Start a one-shot Container
         try (
             GenericContainer container = new GenericContainer<>()
-                .withCommand("/bin/sh", "-c", "sleep 0")
-                .withStartupCheckStrategy(new OneShotStartupCheckStrategy())) {
-
+                .withCommand("true")
+                .withStartupCheckStrategy(new OneShotStartupCheckStrategy())
+        ) {
             container.start();
-            Instant endTime = Instant.now();
+            String createdAt = container.getContainerInfo().getCreated();
+            String finishedAt = container.getCurrentContainerInfo().getState().getFinishedAt();
 
             // Request all events between startTime and endTime for the container
-            try (EventsResultCallback response = client.eventsCmd()
-                .withContainerFilter(container.getContainerId())
-                .withEventFilter("create")
-                .withSince(Long.toString(startTime.getEpochSecond()))
-                .withUntil(Long.toString(endTime.getEpochSecond()))
-                .exec(new EventsResultCallback() {
-                    @Override
-                    public void onNext(@NotNull Event event) {
-                        // Check that a create event for the container is received
-                        if (event.getId().equals(container.getContainerId()) && event.getStatus().equals("create")) {
-                            received.set(true);
+            try (
+                EventsResultCallback response = DockerClientFactory.instance().client().eventsCmd()
+                    .withContainerFilter(container.getContainerId())
+                    .withEventFilter("create")
+                    .withSince(Instant.parse(createdAt).getEpochSecond() + "")
+                    .withUntil(Instant.parse(finishedAt).getEpochSecond() + "")
+                    .exec(new EventsResultCallback() {
+                        @Override
+                        public void onNext(@NotNull Event event) {
+                            // Check that a create event for the container is received
+                            if (event.getId().equals(container.getContainerId()) && event.getStatus().equals("create")) {
+                                received.set(true);
+                            }
                         }
-                    }
-                })) {
-
+                    })
+            ) {
                 response.awaitCompletion();
             }
-
         }
 
-        VisibleAssertions.assertTrue("Events has been captured", received.get());
+        assertTrue("Events has been captured", received.get());
     }
 
 }
