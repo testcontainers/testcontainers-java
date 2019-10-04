9b0f02adf0989d2eeecc466886299ddc3875b4ed
Richard North <rich.north@gmail.com>
2019-10-04 20:17:14 +0100

    Make test resilient to future changes in default mounts by only checking counts of SHM volumes

--- modules/selenium/src/test/java/org/testcontainers/junit/BrowserWebDriverContainerTest.java
+++ modules/selenium/src/test/java/org/testcontainers/junit/BrowserWebDriverContainerTest.java
@@ -1,15 +1,11 @@
 package org.testcontainers.junit;
 
-import com.github.dockerjava.api.command.InspectContainerResponse;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 
-import java.util.List;
-
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
-import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class BrowserWebDriverContainerTest {
 
@@ -53,37 +49,47 @@ public class BrowserWebDriverContainerTest {
         ) {
             webDriverContainer.start();
 
-            final List<InspectContainerResponse.Mount> mounts = webDriverContainer.getContainerInfo().getMounts();
-            assertEquals("Shm mounts present", mounts.size(), 1);
-
-            final InspectContainerResponse.Mount shmMount = mounts.get(0);
-
-            /* source path on Linux/OS X should be /dev/shm
-               source path on Windows is likely to be different: /host_mnt/c/dev/shm has been observed, but other paths
-               may be possible. As such, be liberal when asserting that the source path is correct.
-             */
-            assertTrue("Shm mount source is correct", shmMount.getSource().endsWith("/dev/shm"));
-
-            assertEquals("Shm mount destination is correct", "/dev/shm", shmMount.getDestination().getPath());
-            assertEquals("Shm mount mode is correct", shmMount.getMode(), "rw");
+            assertEquals("Only one shm mount present", countCorrectlyConfiguredShmVolumes(webDriverContainer), 1L);
+            assertEquals("No additional shm mounts present", countAnyShmVolumes(webDriverContainer), 1L);
         }
     }
 
     @Test
     public void createContainerWithoutShmVolume() {
         try (
-            BrowserWebDriverContainer webDriverContainer =  new BrowserWebDriverContainer<>()
-             .withSharedMemorySize(512 * FileUtils.ONE_MB)
+            BrowserWebDriverContainer webDriverContainer = new BrowserWebDriverContainer<>()
+                .withSharedMemorySize(512 * FileUtils.ONE_MB)
         ) {
             webDriverContainer.start();
-            assertEquals("Shared memory size is configured", 512 * FileUtils.ONE_MB, webDriverContainer.getShmSize());
 
-            final long shmMountCount = webDriverContainer.getContainerInfo().getMounts()
-                .stream()
-                .filter(m -> "/dev/shm".equals(m.getSource()))
-                .count();
-            assertEquals("No shm mounts present", shmMountCount, 0L);
+            assertEquals("Shared memory size is configured",
+                512 * FileUtils.ONE_MB,
+                webDriverContainer.getShmSize());
+
+            assertEquals("No shm mounts present", countAnyShmVolumes(webDriverContainer), 0L);
         }
     }
 
+    private long countAnyShmVolumes(final BrowserWebDriverContainer container) {
+        return container.getContainerInfo().getMounts()
+            .stream()
+            // destination path is always /dev/shm
+            .filter(m -> m.getDestination().getPath().equals("/dev/shm"))
+            .count();
+    }
+
+    private long countCorrectlyConfiguredShmVolumes(final BrowserWebDriverContainer container) {
+        return container.getContainerInfo().getMounts()
+            .stream()
+            /* source path on Linux/OS X should be /dev/shm
+               source path on Windows is likely to be different: /host_mnt/c/dev/shm has been observed, but other paths
+               may be possible. As such, be liberal when checking that the source path is correct.
+             */
+            .filter(m -> m.getSource().endsWith("/dev/shm"))
+            // destination path is always /dev/shm
+            .filter(m -> m.getDestination().getPath().equals("/dev/shm"))
+            // mode should always be r/w
+            .filter(m -> m.getMode().equals("rw"))
+            .count();
+    }
 }
