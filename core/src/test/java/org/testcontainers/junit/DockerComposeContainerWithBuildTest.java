00a3c278c38f3f324b0fbff3aeae1e3106f93656
Richard North <rich.north@gmail.com>
2019-10-04 19:42:36 +0100

    Refactor test to verify removal of images

--- core/src/test/java/org/testcontainers/junit/DockerComposeContainerWithBuildTest.java
+++ core/src/test/java/org/testcontainers/junit/DockerComposeContainerWithBuildTest.java
@@ -1,20 +1,85 @@
 package org.testcontainers.junit;
 
-import java.io.File;
-import org.junit.Rule;
+import com.github.dockerjava.api.model.Container;
+import org.junit.Test;
+import org.rnorth.ducttape.unreliables.Unreliables;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.DockerComposeContainer;
 
-public class DockerComposeContainerWithBuildTest extends BaseDockerComposeTest {
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
-    @Rule
-    public DockerComposeContainer environment = new DockerComposeContainer(new File("src/test/resources/compose-test.yml"))
-            .withExposedService("redis_1", REDIS_PORT)
-            .withExposedService("db_1", 3306)
+import static org.rnorth.visibleassertions.VisibleAssertions.*;
+import static org.testcontainers.containers.DockerComposeContainer.RemoveImages.ALL;
+import static org.testcontainers.containers.DockerComposeContainer.RemoveImages.LOCAL;
+
+public class DockerComposeContainerWithBuildTest {
+
+    @Test
+    public void defaultDoesNotRemoveAnyImages() {
+        doTest(null, true, true);
+    }
+
+    @Test
+    public void localModeRemovesBuiltImages() {
+        doTest(LOCAL, false, true);
+    }
+
+
+    @Test
+    public void allModeRemovesAllImages() {
+        doTest(ALL, false, false);
+    }
+
+    private void doTest(final DockerComposeContainer.RemoveImages removeMode,
+                        final boolean shouldBuiltImageBePresentAfterRunning,
+                        final boolean shouldPulledImageBePresentAfterRunning) {
+        final File composeFile = new File("src/test/resources/compose-build-test/docker-compose.yml");
+
+        final AtomicReference<String> builtImageName = new AtomicReference<>("");
+        final AtomicReference<String> pulledImageName = new AtomicReference<>("");
+        try (DockerComposeContainer environment = new DockerComposeContainer<>(composeFile)
+            .withExposedService("customredis", 6379)
             .withBuild(true)
-            ;
+            .withRemoveImages(removeMode)) {
 
-    @Override
-    protected DockerComposeContainer getEnvironment() {
-        return environment;
+            environment.start();
+
+            builtImageName.set(imageNameForRunningContainer("_customredis_1"));
+            final boolean isBuiltImagePresentWhileRunning = isImagePresent(builtImageName.get());
+            assertEquals("the built image is present while running", true, isBuiltImagePresentWhileRunning);
+
+            pulledImageName.set(imageNameForRunningContainer("_normalredis_1"));
+            final boolean isPulledImagePresentWhileRunning = isImagePresent(pulledImageName.get());
+            assertEquals("the pulled image is present while running", true, isPulledImagePresentWhileRunning);
+        }
+
+        Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
+            final boolean isBuiltImagePresentAfterRunning = isImagePresent(builtImageName.get());
+            assertEquals("the built image is not present after running", shouldBuiltImageBePresentAfterRunning, isBuiltImagePresentAfterRunning);
+            return null;
+        });
+
+        Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
+            final boolean isPulledImagePresentAfterRunning = isImagePresent(pulledImageName.get());
+            assertEquals("the pulled image is present after running", shouldPulledImageBePresentAfterRunning, isPulledImagePresentAfterRunning);
+            return null;
+        });
+    }
+
+    private String imageNameForRunningContainer(final String imageNameSuffix) {
+        return DockerClientFactory.instance().client().listContainersCmd().exec().stream()
+            .filter(it -> Stream.of(it.getNames()).anyMatch(name -> name.endsWith(imageNameSuffix)))
+            .findFirst()
+            .map(Container::getImage)
+            .orElseThrow(IllegalStateException::new);
+    }
+
+    private boolean isImagePresent(final String imageName) {
+        return DockerClientFactory.instance().client().listImagesCmd().withImageNameFilter(imageName).exec().stream()
+            .findFirst()
+            .isPresent();
     }
 }
