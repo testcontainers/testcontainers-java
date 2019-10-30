9a31ab351b3697dd29369f82b504c3fc43339de8
Joseph Petersen <josephp90@gmail.com>
2019-10-30 15:50:22 +0100

    Fix vault module, add new test with java client v2 (#1791)
    * fix vault module, add new test with java client
    
    (cherry picked from commit 8931f8643bdda8edc5318b76ea5f27f9cf98d271)
    
    * adds test for try with resource
    
    adds tests for default constructor
    
    sets default image to vault:latest
    
    * Update VaultContainer.java
    
    * Update VaultContainer.java
    
    * Update VaultClientTest.java
    
    * Update VaultContainer.java
    
    * Update VaultContainerTest.java
    
    * fix vault cli test
    
    * Update modules/vault/src/main/java/org/testcontainers/vault/VaultContainer.java
    
    Co-Authored-By: Richard North <rich.north@gmail.com>
    
    * * Use health endpoint for discovering vault readiness
    * Move customizations to constructor to allow overrides
    * Use a single startup attempt, as 1 should be sufficient
    
    
    Co-authored-by: Roberto Franchini <ro.franchini@gmail.com>
    Co-authored-by: Sergei Egorov <bsideup@gmail.com>
    Co-authored-by: Richard North <rich.north@gmail.com>

---
+++ modules/vault/src/test/java/org/testcontainers/vault/VaultClientTest.java
@@ -0,0 +1,58 @@
+package org.testcontainers.vault;
+
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.response.LogicalResponse;
+import org.junit.Test;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VaultClientTest {
+
+    private static final String VAULT_TOKEN = "my-root-token";
+
+    @Test
+    public void writeAndReadMultipleValues() throws VaultException {
+        try (
+            VaultContainer vaultContainer = new VaultContainer<>()
+                    .withVaultToken(VAULT_TOKEN)
+        ) {
+
+            vaultContainer.start();
+
+            final VaultConfig config = new VaultConfig()
+                .address("http://" + vaultContainer.getContainerIpAddress() + ":" + vaultContainer.getFirstMappedPort())
+                .token(VAULT_TOKEN)
+                .build();
+
+            final Vault vault = new Vault(config);
+
+            final Map<String, Object> secrets = new HashMap<>();
+            secrets.put("value", "world");
+            secrets.put("other_value", "another world");
+
+            // Write operation
+            final LogicalResponse writeResponse = vault.logical()
+                .write("secret/hello", secrets);
+
+            assertThat(writeResponse.getRestResponse().getStatus()).isEqualTo(200);
+
+            // Read operation
+            final Map<String, String> value = vault.logical()
+                .read("secret/hello")
+                .getData();
+
+
+            assertThat(value)
+                .containsEntry("value", "world")
+                .containsEntry("other_value", "another world");
+
+        }
+
+    }
+}
