37014cc0ceca2d96935b1f6c677ee520c3c92e1e
Alexander Sparkowsky <info@roamingthings.de>
2019-11-01 17:15:55 +0100

    Add tests for ordering and capturing test exceptions (#1326)

--- modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareContainerMock.java
+++ modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareContainerMock.java
@@ -6,10 +6,18 @@ import org.testcontainers.lifecycle.TestLifecycleAware;
 
 import java.util.Optional;
 
+import static java.lang.System.currentTimeMillis;
+import static java.lang.Thread.sleep;
+
 public class TestLifecycleAwareContainerMock extends GenericContainer implements TestLifecycleAware {
 
     private int numBeforeTestsCalls = 0;
     private int numAfterTestsCalls = 0;
+
+    private long beforeTestCalledAtMillis = 0;
+    private long afterTestCalledAtMillis = 0;
+
+    private Throwable capturedThrowable;
 
     TestLifecycleAwareContainerMock() {
         super("alpine:3.2");
@@ -19,11 +27,23 @@ public class TestLifecycleAwareContainerMock extends GenericContainer implements
     @Override
     public void beforeTest(TestDescription description) {
         numBeforeTestsCalls++;
+        beforeTestCalledAtMillis = currentTimeMillis();
+        ensureMillisHavePassed();
     }
 
     @Override
     public void afterTest(TestDescription description, Optional<Throwable> throwable) {
         numAfterTestsCalls++;
+        afterTestCalledAtMillis = currentTimeMillis();
+        throwable.ifPresent(capturedThrowable -> this.capturedThrowable = capturedThrowable);
+    }
+
+    private void ensureMillisHavePassed() {
+        try {
+            sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Unable to sleep for 10ms");
+        }
     }
 
     int getNumBeforeTestsCalls() {
@@ -32,5 +52,17 @@ public class TestLifecycleAwareContainerMock extends GenericContainer implements
 
     int getNumAfterTestsCalls() {
         return numAfterTestsCalls;
+    }
+
+    long getBeforeTestCalledAtMillis() {
+        return beforeTestCalledAtMillis;
+    }
+
+    long getAfterTestCalledAtMillis() {
+        return afterTestCalledAtMillis;
+    }
+
+    Throwable getCapturedThrowable() {
+        return capturedThrowable;
     }
 }
--- modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareSignallingTest.java
+++ modules/junit-jupiter/src/test/java/org/testcontainers/junit/jupiter/TestLifecycleAwareSignallingTest.java
@@ -1,6 +1,10 @@
 package org.testcontainers.junit.jupiter;
 
+import org.junit.Assume;
+import org.junit.AssumptionViolatedException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -10,6 +14,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
 // The order of @ExtendsWith and @Testcontainers is crucial in order for the static case to be tested
 @ExtendWith({TestLifecycleAwareSignallingTest.SharedContainerAfterAllTestExtension.class})
@@ -43,14 +50,39 @@ class TestLifecycleAwareSignallingTest {
         assertEquals(1, startedTestContainer.getNumAfterTestsCalls());
     }
 
+    @Test
+    @Order(3)
+    void should_call_beforeTest_before_afterTest() {
+        long beforeTestCalledAtMillis = startedTestContainer.getBeforeTestCalledAtMillis();
+        long afterTestCalledAtMillis = startedTestContainer.getAfterTestCalledAtMillis();
+        assertTrue(beforeTestCalledAtMillis < afterTestCalledAtMillis);
+    }
+
+    @Test
+    @Order(4)
+    void failing_test_should_pass_throwable_to_testContainer() {
+        startedTestContainer = testContainer;
+        assumeTrue(false);
+    }
+
+    @Test
+    @Order(5)
+    void should_have_captured_thrownException() {
+        Throwable capturedThrowable = startedTestContainer.getCapturedThrowable();
+        assertNotNull(capturedThrowable);
+        assertTrue(capturedThrowable instanceof AssumptionViolatedException);
+        assertEquals("got: <false>, expected: is <true>", capturedThrowable.getMessage());
+    }
+
     static class SharedContainerAfterAllTestExtension implements AfterAllCallback {
 
         @Override
         public void afterAll(ExtensionContext context) {
             assertEquals(1, SHARED_CONTAINER.getNumBeforeTestsCalls());
             assertEquals(1, SHARED_CONTAINER.getNumAfterTestsCalls());
+            long beforeTestCalledAtMillis = SHARED_CONTAINER.getBeforeTestCalledAtMillis();
+            long afterTestCalledAtMillis = SHARED_CONTAINER.getAfterTestCalledAtMillis();
+            assertTrue(beforeTestCalledAtMillis < afterTestCalledAtMillis);
         }
     }
 }
-
-
