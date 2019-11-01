5a9c185e9c9b870bb2e639c93b1b83bac4c61c71
Richard North <rich.north@gmail.com>
2019-11-01 20:27:40 +0000

    Switch to using parameterized tests

--- core/src/test/java/org/testcontainers/utility/DockerImageNameTest.java
+++ core/src/test/java/org/testcontainers/utility/DockerImageNameTest.java
@@ -1,96 +1,119 @@
 package org.testcontainers.utility;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.rnorth.visibleassertions.VisibleAssertions;
 
-import static org.junit.Assert.fail;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
+@RunWith(Enclosed.class)
 public class DockerImageNameTest {
 
-    @Test
-    public void validNames() {
-        testValid("myname:latest");
-        testValid("myname:latest");
-        testValid("repo/my-name:1.0");
-        testValid("registry.foo.com:1234/my-name:1.0");
-        testValid("registry.foo.com/my-name:1.0");
-        testValid("registry.foo.com:1234/repo_here/my-name:1.0");
-        testValid("registry.foo.com:1234/repo-here/my-name@sha256:1234abcd1234abcd1234abcd1234abcd");
-        testValid("registry.foo.com:1234/my-name@sha256:1234abcd1234abcd1234abcd1234abcd");
-        testValid("1.2.3.4/my-name:1.0");
-        testValid("1.2.3.4:1234/my-name:1.0");
-        testValid("1.2.3.4/repo-here/my-name:1.0");
-        testValid("1.2.3.4:1234/repo-here/my-name:1.0");
-    }
+    @RunWith(Parameterized.class)
+    public static class ValidNames {
 
-    @Test
-    public void invalidNames() {
-        testInvalid(":latest");
-        testInvalid("/myname:latest");
-        testInvalid("/myname@sha256:latest");
-        testInvalid("/myname@sha256:gggggggggggggggggggggggggggggggg");
-        testInvalid("repo:notaport/myname:latest");
-    }
+        @Parameterized.Parameters(name = "{0}")
+        public static String[] getNames() {
+            return new String[]{"myname:latest",
+                "myname:latest",
+                "repo/my-name:1.0",
+                "registry.foo.com:1234/my-name:1.0",
+                "registry.foo.com/my-name:1.0",
+                "registry.foo.com:1234/repo_here/my-name:1.0",
+                "registry.foo.com:1234/repo-here/my-name@sha256:1234abcd1234abcd1234abcd1234abcd",
+                "registry.foo.com:1234/my-name@sha256:1234abcd1234abcd1234abcd1234abcd",
+                "1.2.3.4/my-name:1.0",
+                "1.2.3.4:1234/my-name:1.0",
+                "1.2.3.4/repo-here/my-name:1.0",
+                "1.2.3.4:1234/repo-here/my-name:1.0"};
+        }
 
-    @Test
-    public void parsingForSimpleTagVersions() {
-        testParsing("", "", "myname", ":", "latest");
-        testParsing("", "", "repo/myname", ":", "latest");
-        testParsing("registry.foo.com:1234", "/", "my-name", ":", "1.0");
-        testParsing("registry.foo.com", "/", "my-name", ":", "1.0");
-        testParsing("registry.foo.com:1234", "/", "repo_here/my-name", ":", "1.0");
-        testParsing("1.2.3.4:1234", "/", "repo_here/my-name", ":", "1.0");
-        testParsing("1.2.3.4:1234", "/", "my-name", ":", "1.0");
-    }
+        @Parameterized.Parameter
+        public String imageName;
 
-    @Test
-    public void parsingForShaSumVersions() {
-        testParsing("", "", "myname", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("", "", "repo/myname", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("registry.foo.com:1234", "/", "repo-here/my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("registry.foo.com:1234", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("1.2.3.4", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("1.2.3.4:1234", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("1.2.3.4", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-        testParsing("1.2.3.4:1234", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd");
-    }
-
-    private void testValid(String s) {
-        new DockerImageName(s).assertValid();
-    }
-
-    private void testInvalid(String myname) {
-        try {
-            new DockerImageName(myname).assertValid();
-            fail();
-        } catch (IllegalArgumentException expected) {
-
+        @Test
+        public void testValidNameAccepted() {
+            new DockerImageName(imageName).assertValid();
         }
     }
 
-    private void testParsing(final String registry,
-                             final String registrySeparator,
-                             final String repo,
-                             final String versionSeparator,
-                             final String version) {
+    @RunWith(Parameterized.class)
+    public static class InvalidNames {
 
-        final String unversionedPart = registry + registrySeparator + repo;
-        final String combined = unversionedPart + versionSeparator + version;
+        @Parameterized.Parameters(name = "{0}")
+        public static String[] getNames() {
+            return new String[]{":latest",
+                "/myname:latest",
+                "/myname@sha256:latest",
+                "/myname@sha256:gggggggggggggggggggggggggggggggg",
+                "repo:notaport/myname:latest"};
+        }
 
-        VisibleAssertions.context("For " + combined);
-        VisibleAssertions.context("Using single-arg constructor:", 2);
+        @Parameterized.Parameter
+        public String imageName;
 
-        final DockerImageName imageName = new DockerImageName(combined);
-        assertEquals(combined + " has registry address: " + registry, registry, imageName.getRegistry());
-        assertEquals(combined + " has unversioned part: " + unversionedPart, unversionedPart, imageName.getUnversionedPart());
-        assertEquals(combined + " has version part: " + version, version, imageName.getVersionPart());
+        @Test(expected = IllegalArgumentException.class)
+        public void testInvalidNameRejected() {
+            new DockerImageName(imageName).assertValid();
+        }
+    }
 
-        VisibleAssertions.context("Using two-arg constructor:", 2);
+    @RunWith(Parameterized.class)
+    public static class Parsing {
 
-        final DockerImageName imageNameFromSecondaryConstructor = new DockerImageName(unversionedPart, version);
-        assertEquals(combined + " has registry address: " + registry, registry, imageNameFromSecondaryConstructor.getRegistry());
-        assertEquals(combined + " has unversioned part: " + unversionedPart, unversionedPart, imageNameFromSecondaryConstructor.getUnversionedPart());
-        assertEquals(combined + " has version part: " + version, version, imageNameFromSecondaryConstructor.getVersionPart());
+        @Parameterized.Parameters(name = "{0}{1}{2}{3}{4}")
+        public static String[][] getNames() {
+            return new String[][]{
+                {"", "", "myname", ":", "latest"},
+                {"", "", "repo/myname", ":", "latest"},
+                {"registry.foo.com:1234", "/", "my-name", ":", "1.0"},
+                {"registry.foo.com", "/", "my-name", ":", "1.0"},
+                {"registry.foo.com:1234", "/", "repo_here/my-name", ":", "1.0"},
+                {"1.2.3.4:1234", "/", "repo_here/my-name", ":", "1.0"},
+                {"1.2.3.4:1234", "/", "my-name", ":", "1.0"},
+                {"", "", "myname", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"", "", "repo/myname", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"registry.foo.com:1234", "/", "repo-here/my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"registry.foo.com:1234", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"1.2.3.4", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"1.2.3.4:1234", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"1.2.3.4", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"},
+                {"1.2.3.4:1234", "/", "my-name", "@", "sha256:1234abcd1234abcd1234abcd1234abcd"}
+            };
+        }
+
+        @Parameterized.Parameter(0)
+        public String registry;
+        @Parameterized.Parameter(1)
+        public String registrySeparator;
+        @Parameterized.Parameter(2)
+        public String repo;
+        @Parameterized.Parameter(3)
+        public String versionSeparator;
+        @Parameterized.Parameter(4)
+        public String version;
+
+        @Test
+        public void testParsing() {
+            final String unversionedPart = registry + registrySeparator + repo;
+            final String combined = unversionedPart + versionSeparator + version;
+
+            VisibleAssertions.context("For " + combined);
+            VisibleAssertions.context("Using single-arg constructor:", 2);
+
+            final DockerImageName imageName = new DockerImageName(combined);
+            assertEquals(combined + " has registry address: " + registry, registry, imageName.getRegistry());
+            assertEquals(combined + " has unversioned part: " + unversionedPart, unversionedPart, imageName.getUnversionedPart());
+            assertEquals(combined + " has version part: " + version, version, imageName.getVersionPart());
+
+            VisibleAssertions.context("Using two-arg constructor:", 2);
+
+            final DockerImageName imageNameFromSecondaryConstructor = new DockerImageName(unversionedPart, version);
+            assertEquals(combined + " has registry address: " + registry, registry, imageNameFromSecondaryConstructor.getRegistry());
+            assertEquals(combined + " has unversioned part: " + unversionedPart, unversionedPart, imageNameFromSecondaryConstructor.getUnversionedPart());
+            assertEquals(combined + " has version part: " + version, version, imageNameFromSecondaryConstructor.getVersionPart());
+        }
     }
 }
