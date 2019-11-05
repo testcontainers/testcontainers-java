ef61ef1b28f1d57fff736025acebb95e9d12fdac
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-11-05 04:19:38 +0100

    Optimize imports

--- core/src/main/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheck.java
+++ core/src/main/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheck.java
@@ -13,7 +13,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 
 import static java.lang.String.format;
-import static java.util.concurrent.Executors.*;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
 import static org.testcontainers.containers.wait.internal.ThreadFactories.prefixedThreadFactory;
 
 /**
