50e75fa09780b5762d27ffece7f585073dc24a5e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-11-08 20:08:03 +0100

    Lighten PostgreSQLConnectionURLTest

--- modules/postgresql/src/test/java/org/testcontainers/containers/PostgreSQLConnectionURLTest.java
+++ modules/postgresql/src/test/java/org/testcontainers/containers/PostgreSQLConnectionURLTest.java
@@ -1,25 +1,29 @@
 package org.testcontainers.containers;
 
-import org.junit.Rule;
 import org.junit.Test;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertFalse;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
-import static org.testcontainers.jdbc.ConnectionUrl.newInstance;
 
 public class PostgreSQLConnectionURLTest {
 
-    @Rule
-    public JdbcDatabaseContainer postgres = new PostgreSQLContainerProvider().newInstance(newInstance("jdbc:tc:postgresql://hostname/databasename"));
-
     @Test
     public void shouldCorrectlyAppendQueryString() {
+        PostgreSQLContainer postgres = new FixedJdbcUrlPostgreSQLContainer();
         String connectionUrl = postgres.constructUrlForConnection("?stringtype=unspecified&stringtype=unspecified");
         String queryString = connectionUrl.substring(connectionUrl.indexOf('?'));
 
         assertTrue("Query String contains expected params", queryString.contains("&stringtype=unspecified&stringtype=unspecified"));
         assertEquals("Query String starts with '?'", 0, queryString.indexOf('?'));
         assertFalse("Query String does not contain extra '?'", queryString.substring(1).contains("?"));
+    }
+
+    public static class FixedJdbcUrlPostgreSQLContainer extends PostgreSQLContainer {
+
+        @Override
+        public String getJdbcUrl() {
+            return "jdbc:postgresql://localhost:34532/databasename?loggerLevel=OFF";
+        }
     }
 }
