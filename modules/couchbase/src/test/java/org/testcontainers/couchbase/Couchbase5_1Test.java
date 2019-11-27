4f6f2c7cdf65f16952806ac91758ba0e3cf7e8d8
Sergei Egorov <bsideup@gmail.com>
2019-11-27 19:08:04 +0100

    Restore Flaky on couchbase tests (#2106)
    * Restore `@Flaky` in Couchbase tests (partially revert #2081)
    
    * Manually start/stop container, do not reuse environment/cluster

--- modules/couchbase/src/test/java/org/testcontainers/couchbase/Couchbase5_1Test.java
+++ modules/couchbase/src/test/java/org/testcontainers/couchbase/Couchbase5_1Test.java
@@ -1,10 +1,11 @@
 package org.testcontainers.couchbase;
 
-import org.junit.ClassRule;
+import org.junit.Rule;
 
 public class Couchbase5_1Test extends BaseCouchbaseContainerTest {
-    @ClassRule
-    public static CouchbaseContainer container = initCouchbaseContainer("couchbase/server:5.1.1");
+
+    @Rule
+    public CouchbaseContainer container = initCouchbaseContainer("couchbase/server:5.1.1");
 
     @Override
     public CouchbaseContainer getCouchbaseContainer() {
