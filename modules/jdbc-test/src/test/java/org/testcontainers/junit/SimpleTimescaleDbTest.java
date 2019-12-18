572f9231f6518678f9b6f5b43ecd907bac531d22
Silvan Brenner <silvan.brenner@easypsim.com>
2019-12-18 21:06:37 +0100

    add timescaleDb test

---
+++ modules/jdbc-test/src/test/java/org/testcontainers/junit/SimpleTimescaleDbTest.java
@@ -0,0 +1,67 @@
+package org.testcontainers.junit;
+
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertNotEquals;
+
+public class SimpleTimescaleDbTest extends AbstractContainerDatabaseTest {
+
+	private final static String TIMESCALE_IMAGE ="timescale/timescaledb:latest-pg11";
+	
+    static {
+        // Postgres JDBC driver uses JUL; disable it to avoid annoying, irrelevant, stderr logs during connection testing
+        LogManager.getLogManager().getLogger("").setLevel(Level.OFF);
+    }
+
+    @Test
+    public void testSimple() throws SQLException {
+        try (PostgreSQLContainer postgres = new PostgreSQLContainer<>(TIMESCALE_IMAGE))  {
+            postgres.start();
+
+            ResultSet resultSet = performQuery(postgres, "SELECT 1");
+            int resultSetInt = resultSet.getInt(1);
+            assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
+        }
+    }
+
+    @Test
+    public void testCommandOverride() throws SQLException {
+        try (PostgreSQLContainer postgres = new PostgreSQLContainer<>(TIMESCALE_IMAGE).withCommand("postgres -c max_connections=42")) {
+            postgres.start();
+
+            ResultSet resultSet = performQuery(postgres, "SELECT current_setting('max_connections')");
+            String result = resultSet.getString(1);
+            assertEquals("max_connections should be overriden", "42", result);
+        }
+    }
+
+    @Test
+    public void testUnsetCommand() throws SQLException {
+        try (PostgreSQLContainer postgres = new PostgreSQLContainer<>("timescale/timescaledb:latest-pg11").withCommand("postgres -c max_connections=42").withCommand()) {
+            postgres.start();
+
+            ResultSet resultSet = performQuery(postgres, "SELECT current_setting('max_connections')");
+            String result = resultSet.getString(1);
+            assertNotEquals("max_connections should not be overriden", "42", result);
+        }
+    }
+
+    @Test
+    public void testExplicitInitScript() throws SQLException {
+        try (PostgreSQLContainer postgres = new PostgreSQLContainer<>(TIMESCALE_IMAGE).withInitScript("somepath/init_timescaledb.sql")) {
+            postgres.start();
+
+            ResultSet resultSet = performQuery(postgres, "SELECT foo FROM bar");
+
+            String firstColumnValue = resultSet.getString(1);
+            assertEquals("Value from init script should equal real value", "hello world", firstColumnValue);
+        }
+    }
+}
