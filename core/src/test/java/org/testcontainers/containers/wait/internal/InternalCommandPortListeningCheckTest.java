df6c38abf512161f24abc0b75e1bbe1f75b4d34b
Alexander Block <ablock84@gmail.com>
2020-02-19 22:31:15 +0100

    Wait a few seconds for InternalCommandPortListeningCheck to pass

--- core/src/test/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheckTest.java
+++ core/src/test/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheckTest.java
@@ -1,14 +1,16 @@
 package org.testcontainers.containers.wait.internal;
 
 import com.google.common.collect.ImmutableSet;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.rnorth.ducttape.TimeoutException;
+import org.rnorth.ducttape.unreliables.Unreliables;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
+
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Arrays.asList;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertFalse;
@@ -41,6 +43,8 @@ public class InternalCommandPortListeningCheckTest {
     public void singleListening() {
         final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(container, ImmutableSet.of(8080));
 
+        Unreliables.retryUntilTrue(5, TimeUnit.SECONDS, check);
+
         final Boolean result = check.call();
 
         assertTrue("InternalCommandPortListeningCheck identifies a single listening port", result);
@@ -50,6 +54,12 @@ public class InternalCommandPortListeningCheckTest {
     public void nonListening() {
         final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(container, ImmutableSet.of(8080, 1234));
 
+        try {
+            Unreliables.retryUntilTrue(5, TimeUnit.SECONDS, check);
+        } catch (TimeoutException e) {
+            // we expect it to timeout
+        }
+
         final Boolean result = check.call();
 
         assertFalse("InternalCommandPortListeningCheck detects a non-listening port among many", result);
@@ -58,6 +68,8 @@ public class InternalCommandPortListeningCheckTest {
     @Test
     public void lowAndHighPortListening() {
         final InternalCommandPortListeningCheck check = new InternalCommandPortListeningCheck(container, ImmutableSet.of(100, 8080));
+
+        Unreliables.retryUntilTrue(5, TimeUnit.SECONDS, check);
 
         final Boolean result = check.call();
 
