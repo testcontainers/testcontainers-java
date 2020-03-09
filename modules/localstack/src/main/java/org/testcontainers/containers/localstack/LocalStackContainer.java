aa871df5adbe725df779d3a521b74010da9525bf
Mariusz Ćwikla <mariuszcwikla@gmail.com>
2020-03-09 20:55:18 +0100

    Upgrade localstack to 0.10.8

--- modules/localstack/src/main/java/org/testcontainers/containers/localstack/LocalStackContainer.java
+++ modules/localstack/src/main/java/org/testcontainers/containers/localstack/LocalStackContainer.java
@@ -29,7 +29,7 @@ import java.util.stream.Collectors;
  */
 public class LocalStackContainer extends GenericContainer<LocalStackContainer> {
 
-    public static final String VERSION = "0.9.4";
+    public static final String VERSION = "0.10.8";
     private static final String HOSTNAME_EXTERNAL_ENV_VAR = "HOSTNAME_EXTERNAL";
 
     private final List<Service> services = new ArrayList<>();
