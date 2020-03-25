69e365f9a8ee34e8487d2985b121221133fc4aa7
Anton Deriabin <ander.2142@gmail.com>
2020-03-25 14:00:27 +0300

    Bump Cucucmber example version to 5.5.0 (#2476)
    * Bump Cucucmber example version to 5.5.0
    
    * Fixed wrong imports

--- examples/cucumber/src/test/java/org/testcontainsers/examples/Stepdefs.java
+++ examples/cucumber/src/test/java/org/testcontainsers/examples/Stepdefs.java
@@ -1,11 +1,11 @@
 package org.testcontainsers.examples;
 
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
