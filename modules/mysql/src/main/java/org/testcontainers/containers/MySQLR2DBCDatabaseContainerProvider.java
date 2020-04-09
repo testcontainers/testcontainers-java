8903ed8523dc21c5ce2decb53917baba6273508f
Sergei Egorov <segorov@pivotal.io>
2020-04-09 16:51:29 +0200

    Add MySQL

---
+++ modules/mysql/src/main/java/org/testcontainers/containers/MySQLR2DBCDatabaseContainerProvider.java
@@ -0,0 +1,29 @@
+package org.testcontainers.containers;
+
+import com.google.auto.service.AutoService;
+import dev.miku.r2dbc.mysql.MySqlConnectionFactoryProvider;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.testcontainers.r2dbc.R2DBCDatabaseContainer;
+import org.testcontainers.r2dbc.R2DBCDatabaseContainerProvider;
+
+@AutoService(R2DBCDatabaseContainerProvider.class)
+public class MySQLR2DBCDatabaseContainerProvider implements R2DBCDatabaseContainerProvider {
+
+    static final String DRIVER = MySqlConnectionFactoryProvider.MYSQL_DRIVER;
+
+    @Override
+    public boolean supports(ConnectionFactoryOptions options) {
+        return DRIVER.equals(options.getRequiredValue(ConnectionFactoryOptions.DRIVER));
+    }
+
+    @Override
+    public R2DBCDatabaseContainer createContainer(ConnectionFactoryOptions options) {
+        MySQLContainer<?> container = new MySQLContainer<>(options.getRequiredValue(IMAGE_OPTION))
+            .withDatabaseName(options.getRequiredValue(ConnectionFactoryOptions.DATABASE));
+
+        if (Boolean.TRUE.equals(options.getValue(REUSABLE_OPTION))) {
+            container.withReuse(true);
+        }
+        return new MySQLR2DBCDatabaseContainer(container);
+    }
+}
---
+++ modules/mysql/src/test/java/org/testcontainers/containers/MySQLR2DBCDatabaseContainerTest.java
@@ -0,0 +1,69 @@
+package org.testcontainers.containers;
+
+import io.r2dbc.spi.Closeable;
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Result;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.Assert.*;
+
+public class MySQLR2DBCDatabaseContainerTest {
+
+    @Test
+    public void testGetOptions() {
+        try (MySQLContainer<?> container = new MySQLContainer<>()) {
+            container.start();
+
+            long result = Flux
+                .usingWhen(
+                    Mono.just(
+                        ConnectionFactories.get(
+                            MySQLR2DBCDatabaseContainer.getOptions(container)
+                        )
+                    ),
+                    connectionFactory -> {
+                        return Flux
+                            .usingWhen(
+                                connectionFactory.create(),
+                                connection -> connection.createStatement("SELECT 42").execute(),
+                                Connection::close
+                            )
+                            .flatMap(it -> it.map((row, meta) -> (Long) row.get(0)));
+                    },
+                    it -> ((Closeable) it).close()
+                )
+                .blockFirst();
+
+            assertEquals(42, result);
+        }
+    }
+
+    @Test
+    public void testUrlSupport() {
+        String url = "r2dbc:tc:mysql:///db?TC_IMAGE=mysql:5.7.22";
+        ConnectionFactory connectionFactory = ConnectionFactories.get(url);
+        try {
+            int updated = Flux
+                .usingWhen(
+                    connectionFactory.create(),
+                    connection -> {
+                        return Mono
+                            .from(connection.createStatement("CREATE TABLE test(id integer PRIMARY KEY)").execute())
+                            .thenMany(connection.createStatement("INSERT INTO test(id) VALUES(123)").execute())
+                            .flatMap(Result::getRowsUpdated);
+                    },
+                    Connection::close
+                )
+                .blockFirst();
+
+            assertEquals(updated, 1);
+        } finally {
+            Mono.from(((Closeable) connectionFactory).close()).block();
+        }
+    }
+
+}
