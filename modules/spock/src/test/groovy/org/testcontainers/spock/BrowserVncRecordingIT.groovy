59c1ac678b711611cb013d5cc8a077fc65c3fdba
Kevin Wittek <kevin@wittek.dev>
2020-04-12 19:23:41 +0200

    Use data-driven test in BrowserVncRecordingIT

--- modules/spock/src/test/groovy/org/testcontainers/spock/BrowserVncRecordingIT.groovy
+++ modules/spock/src/test/groovy/org/testcontainers/spock/BrowserVncRecordingIT.groovy
@@ -4,7 +4,11 @@ import org.intellij.lang.annotations.Language
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
+import spock.lang.Unroll
 import spock.util.EmbeddedSpecRunner
+
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING
 
 class BrowserVncRecordingIT extends Specification {
 
@@ -17,6 +21,7 @@ class BrowserVncRecordingIT extends Specification {
         recordingDir = temp.getRoot().getAbsolutePath()
     }
 
+    @Unroll("For recording mode #recordingMode and failing test is #fails records video file named '#videoFileName'")
     def "retains all recordings for RECORD_ALL if successful"() {
         given:
 
@@ -32,14 +37,14 @@ import spock.lang.Specification
 
 import java.util.concurrent.TimeUnit
 
-import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode
 
 @Testcontainers
 class BrowserWebdriverContainerIT extends Specification {
 
     BrowserWebDriverContainer browserContainer = new BrowserWebDriverContainer()
         .withCapabilities(new ChromeOptions())
-        .withRecordingMode(RECORD_ALL, new File("$recordingDir"))
+        .withRecordingMode("$recordingMode" as VncRecordingMode, new File("$recordingDir"))
 
     RemoteWebDriver driver
 
@@ -53,59 +58,7 @@ class BrowserWebdriverContainerIT extends Specification {
         driver.get("http://en.wikipedia.org/wiki/Randomness")
 
         then:
-        driver.findElementByPartialLinkText("pattern").isDisplayed()
-    }
-
-}
-
-
-"""
-
-        when:
-        EmbeddedSpecRunner runner = new EmbeddedSpecRunner(throwFailure: true)
-        runner.run(myTest)
-
-        then:
-        temp.getRoot().list().find { it.contains("BrowserWebdriverContainerIT-should+record") }
-    }
-
-    def "records nothing if RECORD_FAILING and not failing"() {
-        given:
-
-        //noinspection GrPackage
-        @Language("groovy")
-        String myTest = """
-package org.testcontainers.spock
-
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.remote.RemoteWebDriver
-import org.testcontainers.containers.BrowserWebDriverContainer
-import spock.lang.Specification
-
-import java.util.concurrent.TimeUnit
-
-import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING
-
-@Testcontainers
-class BrowserWebdriverContainerIT extends Specification {
-
-    BrowserWebDriverContainer browserContainer = new BrowserWebDriverContainer()
-        .withCapabilities(new ChromeOptions())
-        .withRecordingMode(RECORD_FAILING, new File("$recordingDir"))
-
-    RemoteWebDriver driver
-
-    def setup() {
-        driver = browserContainer.getWebDriver()
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
-    }
-
-    def "should record"() {
-        when:
-        driver.get("http://en.wikipedia.org/wiki/Randomness")
-
-        then:
-        driver.findElementByPartialLinkText("pattern").isDisplayed()
+        driver.findElementByPartialLinkText("pattern").isDisplayed() == !$fails
     }
 
 }
@@ -118,59 +71,18 @@ class BrowserWebdriverContainerIT extends Specification {
         runner.run(myTest)
 
         then:
-        temp.getRoot().list().length == 0
-    }
+        def videoDir = temp.getRoot().list()
+        if (videoFileName.isEmpty()) {
+            videoDir.length == 0
+        } else {
+            videoDir.find { it.contains(videoFileName) }
+        }
 
-    def "records file if RECORD_FAILING and failing"() {
-        given:
-
-        //noinspection GrPackage
-        @Language("groovy")
-        String myTest = """
-package org.testcontainers.spock
-
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.remote.RemoteWebDriver
-import org.testcontainers.containers.BrowserWebDriverContainer
-import spock.lang.Specification
-
-import java.util.concurrent.TimeUnit
-
-import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING
-
-@Testcontainers
-class BrowserWebdriverContainerIT extends Specification {
-
-    BrowserWebDriverContainer browserContainer = new BrowserWebDriverContainer()
-        .withCapabilities(new ChromeOptions())
-        .withRecordingMode(RECORD_FAILING, new File("$recordingDir"))
-
-    RemoteWebDriver driver
-
-    def setup() {
-        driver = browserContainer.getWebDriver()
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
-    }
-
-    def "should record"() {
-        when:
-        driver.get("http://en.wikipedia.org/wiki/Randomness")
-
-        then:
-        !driver.findElementByPartialLinkText("pattern").isDisplayed()
-    }
-
-}
-
-
-"""
-
-        when:
-        EmbeddedSpecRunner runner = new EmbeddedSpecRunner(throwFailure: false)
-        runner.run(myTest)
-
-        then:
-        temp.getRoot().list().find { it.contains("BrowserWebdriverContainerIT-should+record") }
+        where:
+        recordingMode  | fails | videoFileName
+        RECORD_ALL     | false | 'BrowserWebdriverContainerIT-should+record'
+        RECORD_FAILING | false | ''
+        RECORD_FAILING | true  | 'BrowserWebdriverContainerIT-should+record'
     }
 
 }
