testcontainers/testcontainers-java

commit 17b9ffc092629b86b301580fa3a8fba18271daa2

Move static config from `configure()` to constructors (#2473)

modules/mariadb/src/main/java/org/testcontainers/containers/MariaDBContainer.java
@@ -24,6 +24,7 @@ public class MariaDBContainer<SELF extends MariaDBContainer<SELF>> extends JdbcD
 
     public MariaDBContainer(String dockerImageName) {
         super(dockerImageName);
+        addExposedPort(MARIADB_PORT);
     }
 
     @Override
@@ -35,7 +36,6 @@ public class MariaDBContainer<SELF extends MariaDBContainer<SELF>> extends JdbcD
     protected void configure() {
         optionallyMapResourceParameterAsVolume(MY_CNF_CONFIG_OVERRIDE_PARAM_NAME, "/etc/mysql/conf.d", "mariadb-default-conf");
 
-        addExposedPort(MARIADB_PORT);
         addEnv("MYSQL_DATABASE", databaseName);
         addEnv("MYSQL_USER", username);
         if (password != null && !password.isEmpty()) {
@@ -83,7 +83,7 @@ public class MariaDBContainer<SELF extends MariaDBContainer<SELF>> extends JdbcD
         parameters.put(MY_CNF_CONFIG_OVERRIDE_PARAM_NAME, s);
         return self();
     }
-    
+
     @Override
     public SELF withDatabaseName(final String databaseName) {
         this.databaseName = databaseName;

modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainer.java
@@ -27,6 +27,7 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
 
     public MySQLContainer(String dockerImageName) {
         super(dockerImageName);
+        addExposedPort(MYSQL_PORT);
     }
 
     @NotNull
@@ -40,7 +41,6 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
         optionallyMapResourceParameterAsVolume(MY_CNF_CONFIG_OVERRIDE_PARAM_NAME, "/etc/mysql/conf.d",
                 "mysql-default-conf");
 
-        addExposedPort(MYSQL_PORT);
         addEnv("MYSQL_DATABASE", databaseName);
         addEnv("MYSQL_USER", username);
         if (password != null && !password.isEmpty()) {

modules/clickhouse/src/main/java/org/testcontainers/containers/ClickHouseContainer.java
@@ -26,10 +26,7 @@ public class ClickHouseContainer extends JdbcDatabaseContainer {
 
     public ClickHouseContainer(String dockerImageName) {
         super(dockerImageName);
-    }
 
-    @Override
-    protected void configure() {
         withExposedPorts(HTTP_PORT, NATIVE_PORT);
         waitingFor(
             new HttpWaitStrategy()

modules/db2/src/main/java/org/testcontainers/containers/Db2Container.java
@@ -37,6 +37,8 @@ public class Db2Container extends JdbcDatabaseContainer<Db2Container> {
         this.waitStrategy = new LogMessageWaitStrategy()
                 .withRegEx(".*Setup has completed\\..*")
                 .withStartupTimeout(Duration.of(10, ChronoUnit.MINUTES));
+
+        addExposedPort(DB2_PORT);
     }
 
     @Override
@@ -51,8 +53,6 @@ public class Db2Container extends JdbcDatabaseContainer<Db2Container> {
             LicenseAcceptance.assertLicenseAccepted(this.getDockerImageName());
             acceptLicense();
         }
-
-        addExposedPort(DB2_PORT);
 
         addEnv("DBNAME", databaseName);
         addEnv("DB2INSTANCE", username);

modules/influxdb/src/main/java/org/testcontainers/containers/InfluxDBContainer.java
@@ -36,12 +36,12 @@ public class InfluxDBContainer<SELF extends InfluxDBContainer<SELF>> extends Gen
         waitStrategy = new WaitAllStrategy()
             .withStrategy(Wait.forHttp("/ping").withBasicCredentials(username, password).forStatusCode(204))
             .withStrategy(Wait.forListeningPort());
+
+        addExposedPort(INFLUXDB_PORT);
     }
 
     @Override
     protected void configure() {
-        addExposedPort(INFLUXDB_PORT);
-
         addEnv("INFLUXDB_ADMIN_USER", admin);
         addEnv("INFLUXDB_ADMIN_PASSWORD", adminPassword);
 

modules/mssqlserver/src/main/java/org/testcontainers/containers/MSSQLServerContainer.java
@@ -36,6 +36,7 @@ public class MSSQLServerContainer<SELF extends MSSQLServerContainer<SELF>> exten
         super(dockerImageName);
         withStartupTimeoutSeconds(DEFAULT_STARTUP_TIMEOUT_SECONDS);
         withConnectTimeoutSeconds(DEFAULT_CONNECT_TIMEOUT_SECONDS);
+        addExposedPort(MS_SQL_SERVER_PORT);
     }
 
     @Override
@@ -45,11 +46,8 @@ public class MSSQLServerContainer<SELF extends MSSQLServerContainer<SELF>> exten
 
     @Override
     protected void configure() {
-        addExposedPort(MS_SQL_SERVER_PORT);
-
         LicenseAcceptance.assertLicenseAccepted(this.getDockerImageName());
         addEnv("ACCEPT_EULA", "Y");
-
         addEnv("SA_PASSWORD", password);
     }
 

modules/neo4j/src/main/java/org/testcontainers/containers/Neo4jContainer.java
@@ -87,6 +87,8 @@ public class Neo4jContainer<S extends Neo4jContainer<S>> extends GenericContaine
             .withStrategy(waitForBolt)
             .withStrategy(waitForHttp)
             .withStartupTimeout(Duration.ofMinutes(2));
+
+        addExposedPorts(DEFAULT_BOLT_PORT, DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT);
     }
 
     @Override
@@ -99,8 +101,6 @@ public class Neo4jContainer<S extends Neo4jContainer<S>> extends GenericContaine
 
     @Override
     protected void configure() {
-
-        addExposedPorts(DEFAULT_BOLT_PORT, DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT);
 
         boolean emptyAdminPassword = this.adminPassword == null || this.adminPassword.isEmpty();
         String neo4jAuth = emptyAdminPassword ? "none" : String.format(AUTH_FORMAT, this.adminPassword);

modules/nginx/src/main/java/org/testcontainers/containers/NginxContainer.java
@@ -16,19 +16,20 @@ public class NginxContainer<SELF extends NginxContainer<SELF>> extends GenericCo
     private static final int NGINX_DEFAULT_PORT = 80;
 
     public NginxContainer() {
-        super("nginx:1.9.4");
+        this("nginx:1.9.4");
+    }
+
+    public NginxContainer(String dockerImageName) {
+        super(dockerImageName);
+
+        addExposedPort(NGINX_DEFAULT_PORT);
+        setCommand("nginx", "-g", "daemon off;");
     }
 
     @NotNull
     @Override
     protected Set<Integer> getLivenessCheckPorts() {
         return Collections.singleton(getMappedPort(80));
-    }
-
-    @Override
-    protected void configure() {
-        addExposedPort(NGINX_DEFAULT_PORT);
-        setCommand("nginx", "-g", "daemon off;");
     }
 
     public URL getBaseUrl(String scheme, int port) throws MalformedURLException {

commit 94323fc09f4028adfc04fdea5c8dede9d0cf8f3d

Fix regression introduced in #2473 (missing `this` constructor call)

modules/mariadb/src/main/java/org/testcontainers/containers/MariaDBContainer.java
@@ -19,7 +19,7 @@ public class MariaDBContainer<SELF extends MariaDBContainer<SELF>> extends JdbcD
     private static final String MY_CNF_CONFIG_OVERRIDE_PARAM_NAME = "TC_MY_CNF";
 
     public MariaDBContainer() {
-        super(IMAGE + ":" + DEFAULT_TAG);
+        this(IMAGE + ":" + DEFAULT_TAG);
     }
 
     public MariaDBContainer(String dockerImageName) {

modules/mysql/src/main/java/org/testcontainers/containers/MySQLContainer.java
@@ -22,7 +22,7 @@ public class MySQLContainer<SELF extends MySQLContainer<SELF>> extends JdbcDatab
     private static final String MYSQL_ROOT_USER = "root";
 
     public MySQLContainer() {
-        super(IMAGE + ":" + DEFAULT_TAG);
+        this(IMAGE + ":" + DEFAULT_TAG);
     }
 
     public MySQLContainer(String dockerImageName) {