64eda9ff74663ebd41d3d83f6a9375cde0ff149a
Ashish Kumar Joy <ashishkujoy@gmail.com>
2020-04-25 15:47:49 +0530

    Adds a test to verify TestLifecycleAwareListener does not fail when startable is not of type TestLifeCycleAware

--- modules/kotest/src/test/kotlin/org/testcontainers/containers/TestLifecycleAwareListenerTest.kt
+++ modules/kotest/src/test/kotlin/org/testcontainers/containers/TestLifecycleAwareListenerTest.kt
@@ -5,18 +5,24 @@ import io.kotest.matchers.shouldBe
 
 class TestLifecycleAwareListenerTest : StringSpec({
     val startableTestLifecycleAware = StartableTestLifecycleAware()
+    val startable = TestStartable()
     val testLifecycleAwareListener = TestLifecycleAwareListener(startableTestLifecycleAware)
+    val anotherTestLifecycleAwareListener = TestLifecycleAwareListener(startable)
 
-    listener(testLifecycleAwareListener)
+    listeners(testLifecycleAwareListener, anotherTestLifecycleAwareListener)
+
+    "it should not break for listener having startable which is not of type testLifecycleAware" {
+        startable.startCount shouldBe 0
+    }
 
     "test id in test description should be combination of test name and package name" {
-        val testDescription = startableTestLifecycleAware.testDescriptions[0]
+        val testDescription = startableTestLifecycleAware.testDescriptions[1]
         testDescription?.testId shouldBe "org.testcontainers.containers.TestLifecycleAwareListenerTest/test id in test" +
             " description should be combination of test name and package name"
     }
 
     "fileSystemFriendlyName .. in /// test description should be encoded test name" {
-        val testDescription = startableTestLifecycleAware.testDescriptions[1]
+        val testDescription = startableTestLifecycleAware.testDescriptions[2]
         val encodedTestName = "fileSystemFriendlyName+..+in+%2F%2F%2F+test+description+should+be+encoded+test+name"
 
         testDescription?.filesystemFriendlyName shouldBe encodedTestName
