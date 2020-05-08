34f72fbeeac2304466406e72c9cf8793122f799d
Richard North <rich.north@gmail.com>
2020-05-08 20:43:04 +0100

    Fix start/use ordering and adopt try-with-resources

--- modules/ceph/src/test/java/org/testcontainers/containers/CephContainerTest.java
+++ modules/ceph/src/test/java/org/testcontainers/containers/CephContainerTest.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.S3Object;
 import org.apache.commons.io.IOUtils;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -24,79 +23,90 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CephContainerTest {
-
-    public CephContainer cephContainer;
-
-    private AmazonS3 amazonS3;
-
-    @Before
-    public void setUp() {
-        // creating_container {
-        cephContainer = new CephContainer("v3.2.13-stable-3.2-mimic-centos-7")
-            .withAwsAccessKey("test")
-            .withAwsSecretKey("test")
-            .withBucketName("test");
-        // }
-
-        // setting_up_s3_client {
-        amazonS3 = AmazonS3ClientBuilder.standard()
-                .withCredentials(cephContainer.getAWSCredentialsProvider())
-                .withEndpointConfiguration(cephContainer.getAWSEndpointConfiguration())
-                .withPathStyleAccessEnabled(true)
-                .withClientConfiguration(
-                        new ClientConfiguration()
-                                .withProtocol(Protocol.HTTP)
-                                .withSignerOverride("S3SignerType")
-                )
-                .build();
-        // }
-    }
-
     @Test
     public void testS3Bucket() {
-        String bucketName = "test2";
+        try (
+            // creating_container {
+            CephContainer cephContainer = new CephContainer()
+                .withAwsAccessKey("test")
+                .withAwsSecretKey("test")
+                .withBucketName("test");
+            // }
+        ) {
+            cephContainer.start();
 
-        //check current bucket
-        assertTrue(amazonS3.doesBucketExistV2(cephContainer.getBucketName()));
+            final AmazonS3 amazonS3 = buildS3Client(cephContainer);
 
-        //create another bucket
-        amazonS3.createBucket(bucketName);
-        assertTrue(amazonS3.doesBucketExistV2(bucketName));
-        List<Bucket> buckets = amazonS3.listBuckets();
-        assertEquals(2, buckets.size());
-        assertTrue(buckets.stream().anyMatch(
+            String bucketName = "test2";
+
+            //check current bucket
+            assertTrue(amazonS3.doesBucketExistV2(cephContainer.getBucketName()));
+
+            //create another bucket
+            amazonS3.createBucket(bucketName);
+            assertTrue(amazonS3.doesBucketExistV2(bucketName));
+            List<Bucket> buckets = amazonS3.listBuckets();
+            assertEquals(2, buckets.size());
+            assertTrue(buckets.stream().anyMatch(
                 bucket -> bucket.getName().equals(bucketName)
-        ));
+            ));
 
-        //remove bucket
-        amazonS3.deleteBucket(bucketName);
-        assertFalse(amazonS3.doesBucketExistV2(bucketName));
-        buckets = amazonS3.listBuckets();
-        assertEquals(1, buckets.size());
-        assertFalse(buckets.stream().anyMatch(
+            //remove bucket
+            amazonS3.deleteBucket(bucketName);
+            assertFalse(amazonS3.doesBucketExistV2(bucketName));
+            buckets = amazonS3.listBuckets();
+            assertEquals(1, buckets.size());
+            assertFalse(buckets.stream().anyMatch(
                 bucket -> bucket.getName().equals(bucketName)
-        ));
+            ));
+        }
     }
 
     @Test
     public void testS3Object() throws IOException {
-        String objectId = "test";
-        String testData = "This is test data";
+        try (
+            CephContainer cephContainer = new CephContainer()
+                .withAwsAccessKey("test")
+                .withAwsSecretKey("test")
+                .withBucketName("test");
+        ) {
+            cephContainer.start();
 
-        //put object
-        amazonS3.putObject(cephContainer.getBucketName(), objectId, testData);
-        assertEquals(1, amazonS3.listObjects(cephContainer.getBucketName()).getObjectSummaries().size());
-        S3Object object = amazonS3.getObject(cephContainer.getBucketName(), objectId);
-        assertEquals(testData, IOUtils.toString(object.getObjectContent(), StandardCharsets.UTF_8));
+            final AmazonS3 amazonS3 = buildS3Client(cephContainer);
+            String objectId = "test";
+            String testData = "This is test data";
 
-        //generate presigned url and download file
-        URL url = amazonS3.generatePresignedUrl(
+            //put object
+            amazonS3.putObject(cephContainer.getBucketName(), objectId, testData);
+            assertEquals(1, amazonS3.listObjects(cephContainer.getBucketName()).getObjectSummaries().size());
+            S3Object object = amazonS3.getObject(cephContainer.getBucketName(), objectId);
+            assertEquals(testData, IOUtils.toString(object.getObjectContent(), StandardCharsets.UTF_8));
+
+            //generate presigned url and download file
+            URL url = amazonS3.generatePresignedUrl(
                 cephContainer.getBucketName(),
                 objectId,
                 Date.from(Instant.now().plusSeconds(Duration.ofMinutes(5).toMillis())));
 
-        try (InputStream inputStream = url.openStream()) {
-            assertEquals(testData, IOUtils.toString(inputStream, StandardCharsets.UTF_8));
+            try (InputStream inputStream = url.openStream()) {
+                assertEquals(testData, IOUtils.toString(inputStream, StandardCharsets.UTF_8));
+            }
         }
+    }
+
+    private AmazonS3 buildS3Client(final CephContainer cephContainer) {
+        // setting_up_s3_client {
+        final AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+            .withCredentials(cephContainer.getAWSCredentialsProvider())
+            .withEndpointConfiguration(cephContainer.getAWSEndpointConfiguration())
+            .withPathStyleAccessEnabled(true)
+            .withClientConfiguration(
+                new ClientConfiguration()
+                    .withProtocol(Protocol.HTTP)
+                    .withSignerOverride("S3SignerType")
+            )
+            .build();
+        // }
+        return amazonS3;
     }
 }
