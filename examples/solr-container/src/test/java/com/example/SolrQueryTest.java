89c4c35dfdb7554bc728458edb197159a1238023
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2020-05-11 08:58:16 +0200

    Use container IP Address
    Co-authored-by: Richard North <rich.north@gmail.com>

--- examples/solr-container/src/test/java/com/example/SolrQueryTest.java
+++ examples/solr-container/src/test/java/com/example/SolrQueryTest.java
@@ -27,7 +27,7 @@ public class SolrQueryTest {
     @BeforeClass
     public static void setUp() throws IOException, SolrServerException {
         solrContainer.start();
-        solrClient = new Http2SolrClient.Builder("http://localhost:" + solrContainer.getSolrPort() + "/solr").build();
+        solrClient = new Http2SolrClient.Builder("http://" + solrContainer.getContainerIpAddress() + ":" + solrContainer.getSolrPort() + "/solr").build();
 
         // Add Sample Data
         solrClient.add(COLLECTION_NAME, Collections.singletonList(
