6862bea6b214ab3fe99bb75d8239b0633e648d87
Richard North <rich.north@gmail.com>
2020-05-21 19:35:40 +0100

    Revert "Revert "Added additional url params in JdbcDatabaseContainer (#1802)" (#2775)"
    This reverts commit 0385030aa4307c7041a0150edbbd2159f9060e16.

--- modules/postgresql/src/test/java/org/testcontainers/containers/PostgreSQLConnectionURLTest.java
+++ modules/postgresql/src/test/java/org/testcontainers/containers/PostgreSQLConnectionURLTest.java
@@ -15,7 +15,7 @@ public class PostgreSQLConnectionURLTest {
         String connectionUrl = postgres.constructUrlForConnection("?stringtype=unspecified&stringtype=unspecified");
         String queryString = connectionUrl.substring(connectionUrl.indexOf('?'));
 
-        assertTrue("Query String contains expected params", queryString.contains("&stringtype=unspecified&stringtype=unspecified"));
+        assertTrue("Query String contains expected params", queryString.contains("?stringtype=unspecified&stringtype=unspecified"));
         assertEquals("Query String starts with '?'", 0, queryString.indexOf('?'));
         assertFalse("Query String does not contain extra '?'", queryString.substring(1).contains("?"));
     }
