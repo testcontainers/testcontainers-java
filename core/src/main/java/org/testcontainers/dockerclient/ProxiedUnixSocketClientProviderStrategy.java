ae480b5e0a7721181323ebfdd0d55240b6d0ca4f
Sergei Egorov <bsideup@gmail.com>
2020-06-10 14:21:21 +0200

    Hard deprecate `ProxiedUnixSocketClientProviderStrategy` (#2851)

--- core/src/main/java/org/testcontainers/dockerclient/ProxiedUnixSocketClientProviderStrategy.java
+++ core/src/main/java/org/testcontainers/dockerclient/ProxiedUnixSocketClientProviderStrategy.java
@@ -1,20 +1,16 @@
 package org.testcontainers.dockerclient;
 
-import lombok.extern.slf4j.Slf4j;
-import org.rnorth.tcpunixsocketproxy.TcpToUnixSocketProxy;
-
-import java.io.File;
-
-@Slf4j
+/**
+ * @deprecated no longer needed since `docker-java` supports all transport modes now
+ */
+@Deprecated
 public class ProxiedUnixSocketClientProviderStrategy extends UnixSocketClientProviderStrategy {
 
     public static final int PRIORITY = EnvironmentAndSystemPropertyClientProviderStrategy.PRIORITY - 100;
 
-    private final File socketFile = new File(DOCKER_SOCK_PATH);
-
     @Override
     protected boolean isApplicable() {
-        return socketFile.exists();
+        return false;
     }
 
     @Override
@@ -24,26 +20,12 @@ public class ProxiedUnixSocketClientProviderStrategy extends UnixSocketClientPro
 
     @Override
     public void test() throws InvalidConfigurationException {
-        TcpToUnixSocketProxy proxy = new TcpToUnixSocketProxy(socketFile);
-
-        try {
-            int proxyPort = proxy.start().getPort();
-
-            config = tryConfiguration("tcp://localhost:" + proxyPort);
-
-            log.debug("Accessing unix domain socket via TCP proxy (" + DOCKER_SOCK_PATH + " via localhost:" + proxyPort + ")");
-        } catch (Exception e) {
-
-            proxy.stop();
-
-            throw new InvalidConfigurationException("ping failed", e);
-        }
-
+        throw new InvalidConfigurationException("Deprecated");
     }
 
     @Override
     public String getDescription() {
-        return "local Unix socket (via TCP proxy)";
+        return "[Deprecated] local Unix socket (via TCP proxy)";
     }
 
 }
