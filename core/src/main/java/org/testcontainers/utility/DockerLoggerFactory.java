7c266ad80fc1278c87c43cf84d2dfab4dec24f2f
Vitaly.Chura <vcvitaly@yahoo.com>
2020-07-11 21:04:47 +0300

    Add logger cache

--- core/src/main/java/org/testcontainers/utility/DockerLoggerFactory.java
+++ core/src/main/java/org/testcontainers/utility/DockerLoggerFactory.java
@@ -1,5 +1,7 @@
 package org.testcontainers.utility;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
@@ -8,8 +10,14 @@ import org.slf4j.LoggerFactory;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DockerLoggerFactory {
 
+    private static final Map<String, Logger> loggerCache = new HashMap<>();
+
     public static Logger getLogger(String dockerImageName) {
 
+        return loggerCache.computeIfAbsent(dockerImageName, DockerLoggerFactory::provideLogger);
+    }
+
+    private static Logger provideLogger(String dockerImageName) {
         final String abbreviatedName;
         if (dockerImageName.contains("@sha256")) {
             abbreviatedName = dockerImageName.substring(0, dockerImageName.indexOf("@sha256") + 14) + "...";
