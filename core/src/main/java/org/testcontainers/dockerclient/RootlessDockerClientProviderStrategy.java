d46fe61016a9b38358634ec788967898cc9d351a
Sergei Egorov <segorov@pivotal.io>
2020-07-27 16:26:06 +0200

    support home path

--- core/src/main/java/org/testcontainers/dockerclient/RootlessDockerClientProviderStrategy.java
+++ core/src/main/java/org/testcontainers/dockerclient/RootlessDockerClientProviderStrategy.java
@@ -2,14 +2,17 @@ package org.testcontainers.dockerclient;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
+import org.jetbrains.annotations.Nullable;
 
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 /**
  *
@@ -21,13 +24,50 @@ public final class RootlessDockerClientProviderStrategy extends DockerClientProv
 
     public static final int PRIORITY = UnixSocketClientProviderStrategy.PRIORITY + 1;
 
-    private Path getSocketPath() {
+    @Getter(lazy = true)
+    @Nullable
+    private final Path socketPath = resolveSocketPath();
+
+    private Path resolveSocketPath() {
+        return tryEnv().orElseGet(() -> {
+            Path homePath = Paths.get(System.getProperty("user.home")).resolve(".docker").resolve("run");
+            return tryFolder(homePath).orElseGet(() -> {
+                Path implicitPath = Paths.get("/run/user/" + LibC.INSTANCE.getuid());
+                return tryFolder(implicitPath).orElse(null);
+            });
+        });
+    }
+
+    private Optional<Path> tryEnv() {
         String xdgRuntimeDir = System.getenv("XDG_RUNTIME_DIR");
         if (StringUtils.isBlank(xdgRuntimeDir)) {
-            xdgRuntimeDir = "/run/user/" + LibC.INSTANCE.getuid();
-            log.debug("$XDG_RUNTIME_DIR is not set. Falling back to {}", xdgRuntimeDir);
+            log.debug("$XDG_RUNTIME_DIR is not set.");
+            return Optional.empty();
         }
-        return Paths.get(xdgRuntimeDir).resolve("docker.sock");
+        Path path = Paths.get(xdgRuntimeDir);
+        if (!Files.exists(path)) {
+            log.debug("$XDG_RUNTIME_DIR is set to '{}' but the folder does not exist.", path);
+            return Optional.empty();
+        }
+        Path socketPath = path.resolve("docker.sock");
+        if (!Files.exists(socketPath)) {
+            log.debug("$XDG_RUNTIME_DIR is set but '{}' does not exist.", socketPath);
+            return Optional.empty();
+        }
+        return Optional.of(socketPath);
+    }
+
+    private Optional<Path> tryFolder(Path path) {
+        if (!Files.exists(path)) {
+            log.debug("'{}' does not exist.", path);
+            return Optional.empty();
+        }
+        Path socketPath = path.resolve("docker.sock");
+        if (!Files.exists(socketPath)) {
+            log.debug("'{}' does not exist.", socketPath);
+            return Optional.empty();
+        }
+        return Optional.of(socketPath);
     }
 
     @Override
@@ -39,7 +79,7 @@ public final class RootlessDockerClientProviderStrategy extends DockerClientProv
 
     @Override
     protected boolean isApplicable() {
-        return SystemUtils.IS_OS_LINUX && Files.exists(getSocketPath());
+        return SystemUtils.IS_OS_LINUX && getSocketPath() != null && Files.exists(getSocketPath());
     }
 
     @Override
