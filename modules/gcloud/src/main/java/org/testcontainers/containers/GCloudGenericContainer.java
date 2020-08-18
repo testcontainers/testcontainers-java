70befd341ed151663e1f1ed63ac114ced7a75145
Eddú Meléndez <eddu.melendez@gmail.com>
2020-08-18 18:58:15 -0500

    Polish

--- modules/gcloud/src/main/java/org/testcontainers/containers/GCloudGenericContainer.java
+++ modules/gcloud/src/main/java/org/testcontainers/containers/GCloudGenericContainer.java
@@ -1,11 +1,7 @@
 package org.testcontainers.containers;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import com.github.dockerjava.api.command.InspectContainerResponse;
 
 /**
  * @author Eddú Meléndez
@@ -18,26 +14,6 @@ public class GCloudGenericContainer<SELF extends GCloudGenericContainer<SELF>> e
 
     public GCloudGenericContainer(String image) {
         super(image);
-    }
-
-    @Override
-    protected void containerIsStarted(InspectContainerResponse containerInfo) {
-        runAdditionalCommands();
-    }
-
-    private void runAdditionalCommands() {
-        this.commands.forEach(cmd -> {
-            try {
-                execInContainer(cmd);
-            } catch (IOException | InterruptedException e) {
-                logger().error("Failed to execute {}. Exception message: {}", cmd, e.getMessage());
-            }
-        });
-    }
-
-    public SELF withAdditionalCommands(String... cmds) {
-        this.commands.addAll(Arrays.asList(cmds));
-        return self();
     }
 
 }
