25df3992db01cdb914448f4e11acad84618a9215
Gregor Trefs <Gregor.Trefs@gmail.com>
2020-09-29 21:03:21 +0200

    Jqwik Support: Adapted mixed lifecycle test

--- modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/MixedLifecycleTests.java
+++ modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/MixedLifecycleTests.java
@@ -1,6 +1,6 @@
 package org.testcontainers.junit.jqwik;
 
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.Property;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
@@ -11,18 +11,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Testcontainers
 class MixedLifecycleTests {
 
-    // will be shared between test methods
+    // will be shared between properties
     @TestContainer
     private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer();
 
-    // will be started before and stopped after each test method
+    // will be started before and stopped after each property
     @TestContainer
     private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
         .withDatabaseName("foo")
         .withUsername("foo")
         .withPassword("secret");
 
-    @Test
+    @Property
     void test() {
         assertTrue(MY_SQL_CONTAINER.isRunning());
         assertTrue(postgresqlContainer.isRunning());
