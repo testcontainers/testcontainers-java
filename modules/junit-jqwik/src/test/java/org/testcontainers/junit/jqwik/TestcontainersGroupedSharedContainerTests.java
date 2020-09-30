e1fce147754c4cf7ab590357a9644b68a86172ed
Gregor Trefs <Gregor.Trefs@gmail.com>
2020-09-30 21:50:23 +0200

    Jqwik Support: Grouped shared containers
    Shared containers are available in a group.

--- modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/TestcontainersGroupedSharedContainerTests.java
+++ modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/TestcontainersGroupedSharedContainerTests.java
@@ -1,7 +1,8 @@
 package org.testcontainers.junit.jqwik;
 
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.Example;
+import net.jqwik.api.Group;
+import net.jqwik.api.lifecycle.BeforeContainer;
 import org.testcontainers.containers.GenericContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testcontainers.junit.jqwik.JUnitJqwikTestImages.HTTPD_IMAGE;
 
 @Testcontainers
-class TestcontainersNestedSharedContainerTests {
+class TestcontainersGroupedSharedContainerTests {
 
     @TestContainer
     private static final GenericContainer<?> TOP_LEVEL_CONTAINER = new GenericContainer<>(HTTPD_IMAGE)
@@ -17,21 +18,25 @@ class TestcontainersNestedSharedContainerTests {
 
     private static String topLevelContainerId;
 
-    @Test
-    void top_level_container_should_be_running() {
-        assertTrue(TOP_LEVEL_CONTAINER.isRunning());
+    @BeforeContainer
+    static void setTopLevelContainer(){
         topLevelContainerId = TOP_LEVEL_CONTAINER.getContainerId();
     }
 
-    @Nested
-    class NestedTestCase {
+    @Example
+    void top_level_container_should_be_running() {
+        assertTrue(TOP_LEVEL_CONTAINER.isRunning());
+    }
 
-        @Test
+    @Group
+    class GroupedExamples {
+
+        @Example
         void top_level_containers_should_be_running() {
             assertTrue(TOP_LEVEL_CONTAINER.isRunning());
         }
 
-        @Test
+        @Example
         void ids_should_not_change() {
             assertEquals(topLevelContainerId, TOP_LEVEL_CONTAINER.getContainerId());
         }
