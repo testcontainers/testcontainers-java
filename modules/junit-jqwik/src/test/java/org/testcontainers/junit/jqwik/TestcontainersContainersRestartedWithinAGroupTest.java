5a5d079782baf526db540ed7d15532fecfe4bc53
Gregor Trefs <Gregor.Trefs@gmail.com>
2020-09-30 22:11:34 +0200

    Jqwik Support: Containers in groups
    Containers in groups are started and stopped.

---
+++ modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/TestcontainersContainersRestartedWithinAGroupTest.java
@@ -0,0 +1,82 @@
+package org.testcontainers.junit.jqwik;
+
+import net.jqwik.api.Example;
+import net.jqwik.api.Group;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.testcontainers.junit.jqwik.JUnitJqwikTestImages.HTTPD_IMAGE;
+
+@Testcontainers
+public class TestcontainersContainersRestartedWithinAGroupTest {
+
+    @TestContainer
+    private final GenericContainer<?> topLevelContainer = new GenericContainer<>(HTTPD_IMAGE)
+        .withExposedPorts(80);
+
+    @Example
+    public void top_level_example(){
+        assertTrue(topLevelContainer.isRunning());
+    }
+
+    @Group
+    public class Group1 {
+
+        @TestContainer
+        private final GenericContainer<?> groupedContainer = new GenericContainer<>(HTTPD_IMAGE)
+            .withExposedPorts(80);
+
+        private String lastGroupedContainerId;
+
+        @Example
+        public void example_with_grouped_container(){
+            assertTrue(groupedContainer.isRunning());
+            if (lastGroupedContainerId == null) {
+                lastGroupedContainerId = groupedContainer.getContainerId();
+            } else {
+                assertNotEquals(lastGroupedContainerId, groupedContainer.getContainerId());
+            }
+        }
+
+        @Example
+        public void other_example_with_grouped_container(){
+            assertTrue(groupedContainer.isRunning());
+            if (lastGroupedContainerId == null) {
+                lastGroupedContainerId = groupedContainer.getContainerId();
+            } else {
+                assertNotEquals(lastGroupedContainerId, groupedContainer.getContainerId());
+            }
+        }
+    }
+
+    @Group
+    public class Group2 {
+
+        @TestContainer
+        private final GenericContainer<?> groupedContainer = new GenericContainer<>(HTTPD_IMAGE)
+            .withExposedPorts(80);
+
+        private String lastGroupedContainerId;
+
+        @Example
+        public void example_with_grouped_container(){
+            assertTrue(groupedContainer.isRunning());
+            if (lastGroupedContainerId == null) {
+                lastGroupedContainerId = groupedContainer.getContainerId();
+            } else {
+                assertNotEquals(lastGroupedContainerId, groupedContainer.getContainerId());
+            }
+        }
+
+        @Example
+        public void other_example_with_grouped_container(){
+            assertTrue(groupedContainer.isRunning());
+            if (lastGroupedContainerId == null) {
+                lastGroupedContainerId = groupedContainer.getContainerId();
+            } else {
+                assertNotEquals(lastGroupedContainerId, groupedContainer.getContainerId());
+            }
+        }
+    }
+}
