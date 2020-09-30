8b23aaf2532e5b2887c6ed045c84b16c0366f330
Sergei Egorov <segorov@pivotal.io>
2020-09-30 21:07:51 +0200

    remove `KafkaContainer#startZookeeper`

--- modules/kafka/src/main/java/org/testcontainers/containers/KafkaContainer.java
+++ modules/kafka/src/main/java/org/testcontainers/containers/KafkaContainer.java
@@ -1,6 +1,5 @@
 package org.testcontainers.containers;
 
-import com.github.dockerjava.api.command.ExecCreateCmdResponse;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import lombok.SneakyThrows;
 import org.testcontainers.images.builder.Transferable;
@@ -8,7 +7,6 @@ import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.TestcontainersConfiguration;
 
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -108,14 +106,18 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
             return;
         }
 
+        String command = "#!/bin/bash\n";
         final String zookeeperConnect;
         if (externalZookeeperConnect != null) {
             zookeeperConnect = externalZookeeperConnect;
         } else {
-            zookeeperConnect = startZookeeper();
+            zookeeperConnect = "localhost:" + ZOOKEEPER_PORT;
+            command += "echo 'clientPort=" + ZOOKEEPER_PORT + "' > zookeeper.properties\n";
+            command += "echo 'dataDir=/var/lib/zookeeper/data' >> zookeeper.properties\n";
+            command += "echo 'dataLogDir=/var/lib/zookeeper/log' >> zookeeper.properties\n";
+            command += "zookeeper-server-start zookeeper.properties &\n";
         }
 
-        String command = "#!/bin/bash \n";
         command += "export KAFKA_ZOOKEEPER_CONNECT='" + zookeeperConnect + "'\n";
         command += "export KAFKA_ADVERTISED_LISTENERS='" + Stream
             .concat(
@@ -133,19 +135,5 @@ public class KafkaContainer extends GenericContainer<KafkaContainer> {
             Transferable.of(command.getBytes(StandardCharsets.UTF_8), 0777),
             STARTER_SCRIPT
         );
-    }
-
-    @SneakyThrows(InterruptedException.class)
-    private String startZookeeper() {
-        ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(getContainerId())
-            .withCmd("sh", "-c", "" +
-                "printf 'clientPort=" + ZOOKEEPER_PORT + "\ndataDir=/var/lib/zookeeper/data\ndataLogDir=/var/lib/zookeeper/log' > zookeeper.properties\n" +
-                "zookeeper-server-start zookeeper.properties\n"
-            )
-            .exec();
-
-        dockerClient.execStartCmd(execCreateCmdResponse.getId()).start().awaitStarted(10, TimeUnit.SECONDS);
-
-        return "localhost:" + ZOOKEEPER_PORT;
     }
 }
