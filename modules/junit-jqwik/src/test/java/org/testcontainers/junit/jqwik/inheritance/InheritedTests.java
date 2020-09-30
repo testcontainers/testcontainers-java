ec7fca24b566c5b4376ff16b7248bc48cf989b8a
Gregor Trefs <Gregor.Trefs@gmail.com>
2020-09-30 22:41:48 +0200

    Jqwik Support: Test inheritance

--- modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/inheritance/InheritedTests.java
+++ modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/inheritance/InheritedTests.java
@@ -1,6 +1,6 @@
 package org.testcontainers.junit.jqwik.inheritance;
 
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.Example;
 import org.testcontainers.junit.jqwik.TestContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,14 +10,14 @@ class InheritedTests extends AbstractTestBase {
     @TestContainer
     private RedisContainer myRedis = new RedisContainer();
 
-    @Test
+    @Example
     void step1() {
         assertEquals(1, redisPerClass.getJedis().incr("key").longValue());
         assertEquals(1, redisPerTest.getJedis().incr("key").longValue());
         assertEquals(1, myRedis.getJedis().incr("key").longValue());
     }
 
-    @Test
+    @Example
     void step2() {
         assertEquals(2, redisPerClass.getJedis().incr("key").longValue());
         assertEquals(1, redisPerTest.getJedis().incr("key").longValue());
