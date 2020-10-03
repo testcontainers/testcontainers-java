c39b70716199be407928f69d2122d318c5631a9e
Gregor Trefs <Gregor.Trefs@gmail.com>
2020-10-03 18:16:33 +0200

    Jqwik Support: Test that containers in group are not running anymore

--- modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/TestcontainersContainersRestartedWithinAGroupTest.java
+++ modules/junit-jqwik/src/test/java/org/testcontainers/junit/jqwik/TestcontainersContainersRestartedWithinAGroupTest.java
@@ -2,6 +2,7 @@ package org.testcontainers.junit.jqwik;
 
 import net.jqwik.api.Example;
 import net.jqwik.api.Group;
+import net.jqwik.api.lifecycle.AfterContainer;
 import org.testcontainers.containers.GenericContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,9 +15,18 @@ public class TestcontainersContainersRestartedWithinAGroupTest {
     private final GenericContainer<?> topLevelContainer = new GenericContainer<>(HTTPD_IMAGE)
         .withExposedPorts(80);
 
+    private static GenericContainer<?> containerOfGroup1;
+    private static GenericContainer<?> containerOfGroup2;
+
     @Example
     public void top_level_example(){
         assertThat(topLevelContainer.isRunning()).isTrue();
+    }
+
+    @AfterContainer
+    static void grouped_containers_should_not_be_running(){
+        assertThat(containerOfGroup1.isRunning()).isFalse();
+        assertThat(containerOfGroup2.isRunning()).isFalse();
     }
 
     @Group
@@ -31,6 +41,8 @@ public class TestcontainersContainersRestartedWithinAGroupTest {
         @Example
         public void example_with_grouped_container(){
             assertThat(groupedContainer.isRunning()).isTrue();
+            containerOfGroup1 = groupedContainer;
+            assertThatContainerOfGroup2IsNotRunning();
             if (lastGroupedContainerId == null) {
                 lastGroupedContainerId = groupedContainer.getContainerId();
             } else {
@@ -41,10 +53,18 @@ public class TestcontainersContainersRestartedWithinAGroupTest {
         @Example
         public void other_example_with_grouped_container(){
             assertThat(groupedContainer.isRunning()).isTrue();
+            containerOfGroup1 = groupedContainer;
+            assertThatContainerOfGroup2IsNotRunning();
             if (lastGroupedContainerId == null) {
                 lastGroupedContainerId = groupedContainer.getContainerId();
             } else {
                 assertThat(lastGroupedContainerId).isNotEqualTo(groupedContainer.getContainerId());
+            }
+        }
+
+        private void assertThatContainerOfGroup2IsNotRunning() {
+            if(containerOfGroup2 != null){
+                assertThat(containerOfGroup2.isRunning()).isFalse();
             }
         }
     }
@@ -61,6 +81,8 @@ public class TestcontainersContainersRestartedWithinAGroupTest {
         @Example
         public void example_with_grouped_container(){
             assertThat(groupedContainer.isRunning()).isTrue();
+            containerOfGroup2 = groupedContainer;
+            assertThatContainerOfGroup1IsNotRunning();
             if (lastGroupedContainerId == null) {
                 lastGroupedContainerId = groupedContainer.getContainerId();
             } else {
@@ -71,10 +93,18 @@ public class TestcontainersContainersRestartedWithinAGroupTest {
         @Example
         public void other_example_with_grouped_container(){
             assertThat(groupedContainer.isRunning()).isTrue();
+            containerOfGroup2 = groupedContainer;
+            assertThatContainerOfGroup1IsNotRunning();
             if (lastGroupedContainerId == null) {
                 lastGroupedContainerId = groupedContainer.getContainerId();
             } else {
                 assertThat(lastGroupedContainerId).isNotEqualTo(groupedContainer.getContainerId());
+            }
+        }
+
+        private void assertThatContainerOfGroup1IsNotRunning() {
+            if(containerOfGroup1 != null){
+                assertThat(containerOfGroup1.isRunning()).isFalse();
             }
         }
     }
