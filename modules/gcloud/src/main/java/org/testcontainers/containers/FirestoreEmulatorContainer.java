0eb4f0728611b9038e2169f122c5947e2633e614
Richard North <rich.north@gmail.com>
2020-10-13 20:00:35 +0100

    Add getEmulatorEndpoint methods to gcloud containers

--- modules/gcloud/src/main/java/org/testcontainers/containers/FirestoreEmulatorContainer.java
+++ modules/gcloud/src/main/java/org/testcontainers/containers/FirestoreEmulatorContainer.java
@@ -15,16 +15,25 @@ public class FirestoreEmulatorContainer extends GenericContainer<FirestoreEmulat
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("gcr.io/google.com/cloudsdktool/cloud-sdk");
 
     private static final String CMD = "gcloud beta emulators firestore start --host-port 0.0.0.0:8080";
+    private static final int PORT = 8080;
 
     public FirestoreEmulatorContainer(final DockerImageName dockerImageName) {
         super(dockerImageName);
 
         dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
 
-        withExposedPorts(8080);
+        withExposedPorts(PORT);
         setWaitStrategy(new LogMessageWaitStrategy()
                 .withRegEx("(?s).*running.*$"));
         withCommand("/bin/sh", "-c", CMD);
     }
 
+    /**
+     * @return a <code>host:port</code> pair corresponding to the address on which the emulator is
+     * reachable from the test host machine. Directly usable as a parameter to the
+     * com.google.cloud.ServiceOptions.Builder#setHost(java.lang.String) method.
+     */
+    public String getEmulatorEndpoint() {
+        return getContainerIpAddress() + ":" + getMappedPort(8080);
+    }
 }
