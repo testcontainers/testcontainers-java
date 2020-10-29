0f51dfa6206162baa610463ce5d56358b5812d60
Richard North <rich.north@gmail.com>
2020-10-29 11:02:48 +0000

    Refactor Testcontainers configuration to allow config by env var

--- core/src/test/java/org/testcontainers/utility/TestcontainersConfigurationTest.java
+++ core/src/test/java/org/testcontainers/utility/TestcontainersConfigurationTest.java
@@ -1,57 +1,107 @@
 package org.testcontainers.utility;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertFalse;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
-import java.util.Properties;
-import java.util.UUID;
-import org.junit.Test;
-
 public class TestcontainersConfigurationTest {
 
-    final Properties environmentProperties = new Properties();
+    private Properties userProperties;
+    private Properties classpathProperties;
+    private Map<String, String> environment;
 
-    final Properties classpathProperties = new Properties();
+    @Before
+    public void setUp() {
+        userProperties = new Properties();
+        classpathProperties = new Properties();
+        environment = new HashMap<>();
+    }
 
     @Test
-    public void shouldReadChecksFromEnvironmentOnly() {
+    public void shouldNotReadChecksFromClasspathProperties() {
         assertFalse("checks enabled by default", newConfig().isDisableChecks());
 
         classpathProperties.setProperty("checks.disable", "true");
         assertFalse("checks are not affected by classpath properties", newConfig().isDisableChecks());
-
-        environmentProperties.setProperty("checks.disable", "true");
-        assertTrue("checks disabled", newConfig().isDisableChecks());
     }
 
     @Test
-    public void shouldReadDockerClientStrategyFromEnvironmentOnly() {
+    public void shouldReadChecksFromUserProperties() {
+        assertFalse("checks enabled by default", newConfig().isDisableChecks());
+
+        userProperties.setProperty("checks.disable", "true");
+        assertTrue("checks disabled via user properties", newConfig().isDisableChecks());
+    }
+
+    @Test
+    public void shouldReadChecksFromEnvironment() {
+        assertFalse("checks enabled by default", newConfig().isDisableChecks());
+
+        userProperties.remove("checks.disable");
+        environment.put("TESTCONTAINERS_CHECKS_DISABLE", "true");
+        assertTrue("checks disabled via env var", newConfig().isDisableChecks());
+    }
+
+    @Test
+    public void shouldNotReadDockerClientStrategyFromClasspathProperties() {
         String currentValue = newConfig().getDockerClientStrategyClassName();
 
         classpathProperties.setProperty("docker.client.strategy", UUID.randomUUID().toString());
         assertEquals("Docker client strategy is not affected by classpath properties", currentValue, newConfig().getDockerClientStrategyClassName());
-
-        environmentProperties.setProperty("docker.client.strategy", "foo");
-        assertEquals("Docker client strategy is changed", "foo", newConfig().getDockerClientStrategyClassName());
     }
 
     @Test
-    public void shouldReadReuseFromEnvironmentOnly() {
+    public void shouldReadDockerClientStrategyFromUserProperties() {
+        userProperties.setProperty("docker.client.strategy", "foo");
+        assertEquals("Docker client strategy is changed by user property", "foo", newConfig().getDockerClientStrategyClassName());
+    }
+
+    @Test
+    public void shouldReadDockerClientStrategyFromEnvironment() {
+               userProperties.remove("docker.client.strategy");
+        environment.put("TESTCONTAINERS_DOCKER_CLIENT_STRATEGY", "foo");
+        assertEquals("Docker client strategy is changed by env var", "foo", newConfig().getDockerClientStrategyClassName());
+    }
+
+    @Test
+    public void shouldNotReadReuseFromClasspathProperties() {
         assertFalse("no reuse by default", newConfig().environmentSupportsReuse());
 
         classpathProperties.setProperty("testcontainers.reuse.enable", "true");
         assertFalse("reuse is not affected by classpath properties", newConfig().environmentSupportsReuse());
+    }
 
-        environmentProperties.setProperty("testcontainers.reuse.enable", "true");
-        assertTrue("reuse enabled", newConfig().environmentSupportsReuse());
+    @Test
+    public void shouldReadReuseFromUserProperties() {
+        assertFalse("no reuse by default", newConfig().environmentSupportsReuse());
 
-        environmentProperties.setProperty("ryuk.container.image", " testcontainersofficial/ryuk:0.3.0 ");
-        assertEquals("trailing whitespace was not removed from image name property", "testcontainersofficial/ryuk:0.3.0",newConfig().getRyukDockerImageName().asCanonicalNameString());
+        userProperties.setProperty("testcontainers.reuse.enable", "true");
+        assertTrue("reuse enabled via user property", newConfig().environmentSupportsReuse());
+    }
+    @Test
+    public void shouldReadReuseFromEnvironment() {
+        assertFalse("no reuse by default", newConfig().environmentSupportsReuse());
 
+        userProperties.remove("testcontainers.reuse.enable");
+        environment.put("TESTCONTAINERS_REUSE_ENABLE", "true");
+        assertTrue("reuse enabled via env var", newConfig().environmentSupportsReuse());
+    }
+
+    @Test
+    public void shouldTrimImageNames() {
+        userProperties.setProperty("ryuk.container.image", " testcontainersofficial/ryuk:0.3.0 ");
+        assertEquals("trailing whitespace was not removed from image name property", "testcontainersofficial/ryuk:0.3.0",newConfig().getRyukImage());
     }
 
     private TestcontainersConfiguration newConfig() {
-        return new TestcontainersConfiguration(environmentProperties, classpathProperties);
+        return new TestcontainersConfiguration(userProperties, classpathProperties, environment);
     }
 }
