e80c52932145e5eca45c82c5b21fba06a9fd8086
Sergei Egorov <segorov@pivotal.io>
2020-11-24 20:40:32 +0100

    revert unnecessary change

--- core/src/test/java/org/testcontainers/DaemonTest.java
+++ core/src/test/java/org/testcontainers/DaemonTest.java
@@ -17,7 +17,7 @@ import static org.testcontainers.TestImages.TINY_IMAGE;
  */
 public class DaemonTest {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Thread mainThread = Thread.currentThread();
 
         GenericContainer<?> genericContainer = null;
@@ -42,10 +42,9 @@ public class DaemonTest {
             }
         } finally {
             if (genericContainer != null) {
-                // genericContainer.stop();
+                genericContainer.stop();
             }
         }
-        Thread.currentThread().join();
     }
 
     @Test
