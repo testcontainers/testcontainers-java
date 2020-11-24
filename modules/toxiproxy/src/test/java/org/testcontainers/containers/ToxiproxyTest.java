b8a9a2ac4870ff9aa1fefc9f318cc6e996946066
Sean C. Sullivan <github@seansullivan.com>
2020-11-24 11:29:36 -0800

    Toxiproxy docker image 2.1.4
    https://hub.docker.com/r/shopify/toxiproxy/

--- modules/toxiproxy/src/test/java/org/testcontainers/containers/ToxiproxyTest.java
+++ modules/toxiproxy/src/test/java/org/testcontainers/containers/ToxiproxyTest.java
@@ -35,7 +35,7 @@ public class ToxiproxyTest {
         .withExposedPorts(6379)
         .withNetwork(network);
 
-    private static final DockerImageName TOXIPROXY_IMAGE = DockerImageName.parse("shopify/toxiproxy:2.1.0");
+    private static final DockerImageName TOXIPROXY_IMAGE = DockerImageName.parse("shopify/toxiproxy:2.1.4");
     // Toxiproxy container, which will be used as a TCP proxy
     @Rule
     public ToxiproxyContainer toxiproxy = new ToxiproxyContainer(TOXIPROXY_IMAGE)
