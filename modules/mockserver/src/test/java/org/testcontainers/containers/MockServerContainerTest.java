dacf62a0c0aae854b12adeb2a479e8215ddb45ce
Kirill Merkushev <lanwen@yandex.ru>
2020-12-11 15:06:55 +0100

    Make MockServer compatible with newer (5.11.x) versions (#3437)
    Co-authored-by: Sergei Egorov <bsideup@gmail.com>

--- modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
+++ modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
@@ -1,56 +1,18 @@
 package org.testcontainers.containers;
 
-import lombok.Cleanup;
-import org.junit.Rule;
 import org.junit.Test;
 import org.mockserver.client.MockServerClient;
 import org.testcontainers.utility.DockerImageName;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertThat;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class MockServerContainerTest {
 
     public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName.parse("jamesdbloom/mockserver:mockserver-5.5.4");
-
-    // creatingProxy {
-    @Rule
-    public MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE);
-    // }
-
-    private static String responseFromMockserver(MockServerContainer mockServer, String path) throws IOException {
-        URLConnection urlConnection = new URL(mockServer.getEndpoint() + path).openConnection();
-        @Cleanup BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-        return reader.readLine();
-    }
-
-    @Test
-    public void shouldReturnExpectation() throws Exception {
-        // testSimpleExpectation {
-        new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
-            .when(request()
-                .withPath("/person")
-                .withQueryStringParameter("name", "peter"))
-            .respond(response()
-                .withBody("Peter the person!"));
-
-        // ...a GET request to '/person?name=peter' returns "Peter the person!"
-        // }
-
-        assertThat("Expectation returns expected response body",
-            responseFromMockserver(mockServer, "/person?name=peter"),
-            containsString("Peter the person")
-        );
-    }
 
     @Test
     public void shouldCallActualMockserverVersion() throws Exception {
@@ -60,14 +22,26 @@ public class MockServerContainerTest {
 
             String expectedBody = "Hello World!";
 
-            new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+            MockServerClient client = new MockServerClient(mockServer.getHost(), mockServer.getServerPort());
+
+            assertTrue("Mockserver running", client.isRunning());
+
+            client
                 .when(request().withPath("/hello"))
                 .respond(response().withBody(expectedBody));
 
             assertThat("MockServer returns correct result",
-                responseFromMockserver(mockServer, "/hello"),
+                SimpleHttpClient.responseFromMockserver(mockServer, "/hello"),
                 equalTo(expectedBody)
             );
+        }
+    }
+
+    @Test
+    public void newVersionStartsWithDefaultWaitStrategy() {
+        DockerImageName dockerImageName = DockerImageName.parse("mockserver/mockserver:mockserver-5.11.2");
+        try (MockServerContainer mockServer = new MockServerContainer(dockerImageName)) {
+            mockServer.start();
         }
     }
 }
