d04ae3a54e89c694b7af003a1a4b6287c41a493d
Oussama BADR <oussamabadr@gmail.com>
2021-02-10 11:08:05 +0100

    Make VNC recorded videos scrollable (#3180)
    This PR adds a second video recording mode, MP4, which is easier to consume and is scrollable.
    
    Fixes #512
    
    Co-authored-by: Kevin Wittek <kiview@users.noreply.github.com>
    Co-authored-by: Sergei Egorov <segorov@vmware.com>

--- modules/selenium/src/main/java/org/testcontainers/containers/DefaultRecordingFileFactory.java
+++ modules/selenium/src/main/java/org/testcontainers/containers/DefaultRecordingFileFactory.java
@@ -4,20 +4,29 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static org.testcontainers.containers.VncRecordingContainer.VncRecordingFormat;
+
 public class DefaultRecordingFileFactory implements RecordingFileFactory {
 
     private static final SimpleDateFormat filenameDateFormat = new SimpleDateFormat("YYYYMMdd-HHmmss");
     private static final String PASSED = "PASSED";
     private static final String FAILED = "FAILED";
-    private static final String FILENAME_FORMAT = "%s-%s-%s.flv";
+    private static final String FILENAME_FORMAT = "%s-%s-%s.%s";
+
 
     @Override
     public File recordingFileForTest(File vncRecordingDirectory, String prefix, boolean succeeded) {
+        return recordingFileForTest(vncRecordingDirectory, prefix, succeeded, VncRecordingContainer.DEFAULT_RECORDING_FORMAT);
+    }
+
+    @Override
+    public File recordingFileForTest(File vncRecordingDirectory, String prefix, boolean succeeded, VncRecordingFormat recordingFormat) {
         final String resultMarker = succeeded ? PASSED : FAILED;
         final String fileName = String.format(FILENAME_FORMAT,
             resultMarker,
             prefix,
-            filenameDateFormat.format(new Date())
+            filenameDateFormat.format(new Date()),
+            recordingFormat.getFilenameExtension()
         );
         return new File(vncRecordingDirectory, fileName);
     }
