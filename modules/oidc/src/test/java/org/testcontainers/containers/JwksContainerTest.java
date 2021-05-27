b141f8398699d3f2dc1740c58d19333a0abe5d14
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-05-27 15:20:19 +0200

    Update Container Tests

--- modules/oidc/src/test/java/org/testcontainers/containers/JwksContainerTest.java
+++ modules/oidc/src/test/java/org/testcontainers/containers/JwksContainerTest.java
@@ -1,12 +1,12 @@
 package org.testcontainers.containers;
 
 import lombok.SneakyThrows;
-import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -20,13 +20,25 @@ public class JwksContainerTest {
 
     @Test
     @SneakyThrows
-    public void testJwksJsonAvailability() {
+    public void testEndpointAvailability() {
         try (JwksContainer container = new JwksContainer()) {
             container.start();
-            Request request = new Request.Builder().url(container.baseUrl() + "/jwks.json").build();
-            Response response = client.newCall(request).execute();
-            assertNotNull(response.body());
-            assertTrue(response.body().string().contains("keys"));
+            Response jwksResponse = get(container, "/jwks.json");
+            assertEquals(200, jwksResponse.code());
+            assertNotNull(jwksResponse.body());
+            assertTrue(jwksResponse.body().string().contains("keys"));
+
+            Response openidResponse = get(container, "/.well-known/openid-configuration");
+            assertEquals(200, openidResponse.code());
+            assertNotNull(openidResponse.body());
+            assertTrue(openidResponse.body().string().contains("jwks_uri"));
         }
+    }
+
+    @SneakyThrows
+    private Response get(JwksContainer container, String path) {
+        Request request = new Request.Builder().url(container.baseUrl() + path).build();
+        return client.newCall(request).execute();
+
     }
 }
