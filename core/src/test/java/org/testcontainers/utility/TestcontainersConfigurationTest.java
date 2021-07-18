bfd61d7eda8502c2bd93542fa45a509fd6ecfea8
Richard North <rich.north@gmail.com>
2021-07-18 15:53:53 +0100

    Use implicit strategy when `docker.host` configuration is set (#4175)

--- core/src/test/java/org/testcontainers/utility/TestcontainersConfigurationTest.java
+++ core/src/test/java/org/testcontainers/utility/TestcontainersConfigurationTest.java
@@ -2,6 +2,7 @@ package org.testcontainers.utility;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.testcontainers.dockerclient.EnvironmentAndSystemPropertyClientProviderStrategy;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -153,6 +154,34 @@ public class TestcontainersConfigurationTest {
         userProperties.remove("docker.client.strategy");
         environment.put("TESTCONTAINERS_DOCKER_CLIENT_STRATEGY", "foo");
         assertEquals("Docker client strategy is changed by env var", "foo", newConfig().getDockerClientStrategyClassName());
+    }
+
+    @Test
+    public void shouldUseImplicitDockerClientStrategyWhenDockerHostPropertyIsSet() {
+        userProperties.remove("docker.client.strategy");
+        userProperties.put("docker.host", "tcp://1.2.3.4:5678");
+        assertEquals("Docker client strategy is implicitly set when docker host property is set", EnvironmentAndSystemPropertyClientProviderStrategy.class.getCanonicalName(), newConfig().getDockerClientStrategyClassName());
+    }
+
+    @Test
+    public void shouldNotUseImplicitDockerClientStrategyWhenDockerHostAndStrategyAreBothSet() {
+        userProperties.put("docker.client.strategy", "foo");
+        userProperties.put("docker.host", "tcp://1.2.3.4:5678");
+        assertEquals("Docker client strategy is can be explicitly set", "foo", newConfig().getDockerClientStrategyClassName());
+
+        userProperties.remove("docker.client.strategy");
+
+        environment.put("TESTCONTAINERS_DOCKER_CLIENT_STRATEGY", "bar");
+        userProperties.put("docker.client.strategy", "foo");
+        assertEquals("Docker client strategy is can be explicitly set", "bar", newConfig().getDockerClientStrategyClassName());
+
+        environment.put("TESTCONTAINERS_DOCKER_CLIENT_STRATEGY", "bar");
+        userProperties.remove("docker.client.strategy");
+        assertEquals("Docker client strategy is can be explicitly set", "bar", newConfig().getDockerClientStrategyClassName());
+
+        environment.remove("TESTCONTAINERS_DOCKER_CLIENT_STRATEGY");
+        userProperties.put("docker.client.strategy", "foo");
+        assertEquals("Docker client strategy is can be explicitly set", "foo", newConfig().getDockerClientStrategyClassName());
     }
 
     @Test
