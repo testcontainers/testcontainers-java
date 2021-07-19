7c0f9140cf9a51c2f057cbe48a10d1fe26fd6e1f
Richard North <rich.north@gmail.com>
2021-07-19 19:27:56 +0100

    Revert to alpine:3.2

--- core/src/test/java/org/testcontainers/junit/DockerfileContainerTest.java
+++ core/src/test/java/org/testcontainers/junit/DockerfileContainerTest.java
@@ -23,7 +23,7 @@ public class DockerfileContainerTest {
     public GenericContainer dslContainer = new GenericContainer(
             new ImageFromDockerfile("tcdockerfile/nginx", false).withDockerfileFromBuilder(builder -> {
                     builder
-                            .from("alpine:3.5")
+                            .from("alpine:3.2")
                             .run("apk add --update nginx")
                             .cmd("nginx", "-g", "daemon off;")
                             .build(); }))
