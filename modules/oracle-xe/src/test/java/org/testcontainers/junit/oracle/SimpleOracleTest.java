e96f8133fc86eec0d4f51118749d1d97a3d7308c
Kevin Wittek <kevin@wittek.dev>
2021-07-21 18:43:46 +0200

    Test Oracle-XE on CI

--- modules/oracle-xe/src/test/java/org/testcontainers/junit/oracle/SimpleOracleTest.java
+++ modules/oracle-xe/src/test/java/org/testcontainers/junit/oracle/SimpleOracleTest.java
@@ -1,6 +1,5 @@
 package org.testcontainers.junit.oracle;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.db.AbstractContainerDatabaseTest;
@@ -10,12 +9,16 @@ import java.sql.SQLException;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
-@Ignore
 public class SimpleOracleTest extends AbstractContainerDatabaseTest {
 
     @Test
     public void testSimple() throws SQLException {
-        try (OracleContainer oracle = new OracleContainer()) {
+
+        // Oracle Express Edition 11g Release 2 on Ubuntu 18.04 LTS
+        String oracleImage = "oracleinanutshell/oracle-xe-11g:1.0.0";
+
+        try (OracleContainer oracle = new OracleContainer(oracleImage)) {
+            oracle.start();
             ResultSet resultSet = performQuery(oracle, "SELECT 1 FROM dual");
 
             int resultSetInt = resultSet.getInt(1);
