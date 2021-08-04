da60c4e2c54aa0afa24430e8ff82d7f9cab3204f
Onur Kagan Ozcan <onurkaganozcan@gmail.com>
2021-08-04 18:51:13 +0300

    Improve readability of KeyStoreBuilder by dividing into methods

--- modules/azure/src/main/java/org/testcontainers/containers/KeyStoreBuilder.java
+++ modules/azure/src/main/java/org/testcontainers/containers/KeyStoreBuilder.java
@@ -1,11 +1,15 @@
 package org.testcontainers.containers;
 
-import okhttp3.*;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.cert.Certificate;
@@ -16,7 +20,24 @@ import java.util.Objects;
 final class KeyStoreBuilder {
 
     static KeyStore buildByDownloadingCertificate(String endpoint, String keyStorePassword) {
-        TrustManager[] trustAllManagers = new TrustManager[] {
+        OkHttpClient client = null;
+        Response response = null;
+        try {
+            TrustManager[] trustAllManagers = buildTrustAllManagers();
+            client = buildTrustAllClient(trustAllManagers);
+            Request request = buildRequest(endpoint);
+            response = client.newCall(request).execute();
+            return buildKeyStore(response.body().byteStream(), keyStorePassword);
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        } finally {
+            closeResponseSilently(response);
+            closeClientSilently(client);
+        }
+    }
+
+    private static TrustManager[] buildTrustAllManagers() {
+        return new TrustManager[] {
                 new X509TrustManager() {
                     @Override
                     public void checkClientTrusted(X509Certificate[] chain, String authType) {
@@ -32,35 +53,31 @@ final class KeyStoreBuilder {
                     }
                 }
         };
-        OkHttpClient client = null;
-        Response response = null;
-        try {
-            client = new OkHttpClient.Builder()
-                    .sslSocketFactory(getTrustAllSSLSocketFactory(trustAllManagers), (X509TrustManager) trustAllManagers[0])
-                    .hostnameVerifier((s, sslSession) -> true)
-                    .build();
-            Request request = new Request.Builder()
-                    .get()
-                    .url(endpoint + "/_explorer/emulator.pem")
-                    .build();
-            response = client.newCall(request).execute();
-            Certificate certificate = CertificateFactory.getInstance("X.509").generateCertificate(response.body().byteStream());
-            KeyStore keystore = KeyStore.getInstance("PKCS12");
-            keystore.load(null, keyStorePassword.toCharArray());
-            keystore.setCertificateEntry("azure-cosmos-emulator", certificate);
-            return keystore;
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        } finally {
-            closeResponseSilently(response);
-            closeClientSilently(client);
-        }
     }
 
-    private static SSLSocketFactory getTrustAllSSLSocketFactory(TrustManager[] trustManagers) throws Exception {
+    private static OkHttpClient buildTrustAllClient(TrustManager[] trustManagers) throws Exception {
         SSLContext sslContext = SSLContext.getInstance("SSL");
         sslContext.init(null, trustManagers, new SecureRandom());
-        return sslContext.getSocketFactory();
+        SSLSocketFactory socketFactory = sslContext.getSocketFactory();
+        return new OkHttpClient.Builder()
+                .sslSocketFactory(socketFactory, (X509TrustManager) trustManagers[0])
+                .hostnameVerifier((s, sslSession) -> true)
+                .build();
+    }
+
+    private static Request buildRequest(String endpoint) {
+        return new Request.Builder()
+                .get()
+                .url(endpoint + "/_explorer/emulator.pem")
+                .build();
+    }
+
+    private static KeyStore buildKeyStore(InputStream certificateStream, String keyStorePassword) throws Exception {
+        Certificate certificate = CertificateFactory.getInstance("X.509").generateCertificate(certificateStream);
+        KeyStore keystore = KeyStore.getInstance("PKCS12");
+        keystore.load(null, keyStorePassword.toCharArray());
+        keystore.setCertificateEntry("azure-cosmos-emulator", certificate);
+        return keystore;
     }
 
     private static void closeResponseSilently(Response response) {
