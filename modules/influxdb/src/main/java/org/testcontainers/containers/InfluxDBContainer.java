53414cac04a8ecc5846e3ef37cb7e2c6b3aaec10
Ramin Gharib <ramingharib@gmail.com>
2021-08-05 18:14:42 +0200

    Update modules/influxdb/src/main/java/org/testcontainers/containers/InfluxDBContainer.java
    Co-authored-by: Sergei Egorov <bsideup@gmail.com>

--- modules/influxdb/src/main/java/org/testcontainers/containers/InfluxDBContainer.java
+++ modules/influxdb/src/main/java/org/testcontainers/containers/InfluxDBContainer.java
@@ -6,7 +6,7 @@ import org.testcontainers.utility.DockerImageName;
  * @deprecated instead use {@link InfluxDBContainerV1} for InfluxDB 1.x or {@link InfluxDBContainerV2} for InfluxDB 2.x instead
  */
 @Deprecated
-public class InfluxDBContainer extends InfluxDBContainerV1 {
+public class InfluxDBContainer<SELF extends InfluxDBContainer<SELF>> extends InfluxDBContainerV1 {
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("influxdb");
     private static final String DEFAULT_TAG = "1.4.3";
 
