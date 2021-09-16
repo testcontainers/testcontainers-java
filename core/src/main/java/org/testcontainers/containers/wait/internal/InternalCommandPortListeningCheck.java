78e7b75fddde9e476aa08970923fd1f6c8681cfe
Sergei Egorov <bsideup@gmail.com>
2021-09-16 12:37:45 +0200

    Reduce the changeset

--- core/src/main/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheck.java
+++ core/src/main/java/org/testcontainers/containers/wait/internal/InternalCommandPortListeningCheck.java
@@ -9,7 +9,8 @@ import org.testcontainers.containers.wait.strategy.WaitStrategyTarget;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Set;
-import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 /**
  * Mechanism for testing that a socket is listening when run from the container being checked.
@@ -23,17 +24,23 @@ public class InternalCommandPortListeningCheck implements java.util.concurrent.C
 
     @Override
     public Boolean call() {
-         String command = internalPorts.stream()
-             .map(it -> String.format("grep -i ':0*%x' /proc/net/tcp*", it))
-             .collect(Collectors.joining(
-             " && ",
-             "while true; do ( ",
-             " ) && exit 0 || sleep 0.1; done"
-         ));
+        StringBuilder command = new StringBuilder("while true; do ( true ");
+
+        for (int internalPort : internalPorts) {
+            command.append(" && ");
+            command.append(" (");
+            command.append(format("grep -i ':0*%x' /proc/net/tcp*", internalPort));
+            command.append(" || ");
+            command.append(format("nc -vz -w 1 localhost %d", internalPort));
+            command.append(" || ");
+            command.append(format("/bin/bash -c '</dev/tcp/localhost/%d'", internalPort));
+            command.append(")");
+        }
+        command.append(" ) && exit 0 || sleep 0.1; done");
 
         Instant before = Instant.now();
         try {
-            ExecResult result = ExecInContainerPattern.execInContainer(waitStrategyTarget.getContainerInfo(), "/bin/sh", "-c", command);
+            ExecResult result = ExecInContainerPattern.execInContainer(waitStrategyTarget.getContainerInfo(), "/bin/sh", "-c", command.toString());
             log.trace("Check for {} took {}. Result code '{}', stdout message: '{}'", internalPorts, Duration.between(before, Instant.now()), result.getExitCode(), result.getStdout());
             int exitCode = result.getExitCode();
             if (exitCode != 0 && exitCode != 1) {
