f55af61e01c1de8b37f1c51a6320134e67d83567
Richard North <rich.north@gmail.com>
2021-10-18 11:55:11 +0100

    Add K3sContainer docs

--- modules/k3s/src/test/java/org/testcontainers/k3s/Fabric8K3sContainerTest.java
+++ modules/k3s/src/test/java/org/testcontainers/k3s/Fabric8K3sContainerTest.java
@@ -13,25 +13,33 @@ import java.io.IOException;
 import java.util.List;
 
 @Slf4j
-public class K3sContainerTest {
+public class Fabric8K3sContainerTest {
 
     @Test
     public void shouldStartAndHaveListableNode() throws IOException {
         try (
+            // starting_k3s {
             K3sContainer k3s = new K3sContainer(DockerImageName.parse("rancher/k3s:v1.21.3-k3s1"))
                 .withLogConsumer(new Slf4jLogConsumer(log))
+            // }
         ) {
             k3s.start();
 
+            // connecting_with_fabric8 {
             String kubeConfigYaml = k3s.getKubeConfigYaml();
 
             Config config = Config.fromKubeconfig(kubeConfigYaml);
-            // workaround for undiagnosed issue; fabric8 seems to not identify the client key algorithm correctly,
-            //  and k3s uses ECDSA keys
+            // workaround for undiagnosed issue; fabric8 seems to not identify
+            // the client key algorithm correctly, so fails to work with K3s
+            // ECDSA keys unless configured explicitly
             config.setClientKeyAlgo("EC");
 
             DefaultKubernetesClient client = new DefaultKubernetesClient(config);
+
+            // interact with the running K3s server, e.g.:
             List<Node> nodes = client.nodes().list().getItems();
+            // }
+
             Assertions.assertThat(nodes).hasSize(1);
         }
     }
