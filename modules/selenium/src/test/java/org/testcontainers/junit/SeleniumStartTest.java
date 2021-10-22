41882ff560866f44e70eef8b1c0f6b6a78ab0511
Tobias Stadler <ts.stadler@gmx.de>
2021-10-22 14:17:56 +0200

    Added started log message used in Selenium 4.0.0 (#4580)

--- modules/selenium/src/test/java/org/testcontainers/junit/SeleniumStartTest.java
+++ modules/selenium/src/test/java/org/testcontainers/junit/SeleniumStartTest.java
@@ -11,11 +11,11 @@ import org.testcontainers.utility.DockerImageName;
  * Simple test to check that readiness detection works correctly across major versions of the containers.
  */
 @RunWith(Parameterized.class)
-public class Selenium3xTest {
+public class SeleniumStartTest {
 
     @Parameterized.Parameters(name = "tag: {0}")
     public static String[] data() {
-        return new String[]{"3.4.0", "2.53.0", "2.45.0"};
+        return new String[]{"4.0.0", "3.4.0", "2.53.0", "2.45.0"};
     }
 
     @Parameterized.Parameter()
@@ -24,7 +24,7 @@ public class Selenium3xTest {
     @Test
     public void testAdditionalStartupString() {
         final DockerImageName imageName = DockerImageName
-            .parse("selenium/standalone-chrome-debug")
+            .parse("selenium/standalone-chrome")
             .withTag(tag);
         try (BrowserWebDriverContainer<?> chrome = new BrowserWebDriverContainer<>(imageName)
                 .withCapabilities(new ChromeOptions())) {
