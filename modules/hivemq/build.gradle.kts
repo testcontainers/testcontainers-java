2b587541a9581b9c1cdfce329c19cdca831c0b1f
yannick <yannick.weber@hivemq.com>
2021-12-23 10:43:29 +0100

    implementation -> shaded

--- modules/hivemq/build.gradle.kts
+++ modules/hivemq/build.gradle.kts
@@ -8,18 +8,18 @@ dependencies {
     api(project(":testcontainers"))
     api("org.jetbrains:annotations:${property("annotations.version")}")
 
-    implementation("org.apache.commons:commons-lang3:${property("commons-lang.version")}")
-    implementation("commons-io:commons-io:${property("commons-io.version")}")
-    implementation("org.javassist:javassist:${property("javassist.version")}")
-    implementation("org.jboss.shrinkwrap:shrinkwrap-api:${property("shrinkwrap.version")}")
-    implementation("org.jboss.shrinkwrap:shrinkwrap-impl-base:${property("shrinkwrap.version")}")
-    implementation("net.lingala.zip4j:zip4j:${property("zip4j.version")}")
+    shaded("org.apache.commons:commons-lang3:${property("commons-lang.version")}")
+    shaded("commons-io:commons-io:${property("commons-io.version")}")
+    shaded("org.javassist:javassist:${property("javassist.version")}")
+    shaded("org.jboss.shrinkwrap:shrinkwrap-api:${property("shrinkwrap.version")}")
+    shaded("org.jboss.shrinkwrap:shrinkwrap-impl-base:${property("shrinkwrap.version")}")
+    shaded("net.lingala.zip4j:zip4j:${property("zip4j.version")}")
 
     /*  This dependency needs to be explicitly added, because shrinkwrap-resolver-api-maven-embedded
         and shrinkwrap-resolver-impl-maven-embedded depend on different versions of it.
         This would lead to issues when the HiveMQ container is included in maven projects. */
-    implementation("org.codehaus.plexus:plexus-utils:3.2.1")
-    implementation("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven-embedded:${property("shrinkwrap-resolver.version")}") {
+    shaded("org.codehaus.plexus:plexus-utils:3.2.1")
+    shaded("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven-embedded:${property("shrinkwrap-resolver.version")}") {
         exclude("org.codehouse.plexus", "plexus-utils")
     }
     runtimeOnly("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven-embedded:${property("shrinkwrap-resolver.version")}") {
