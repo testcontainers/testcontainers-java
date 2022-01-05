5c6f7d2c0e890e6695b43073d66a5dcd06b499ac
yannick <yannick.weber@hivemq.com>
2022-01-05 14:51:45 +0100

    moved versions to build.gradle.kts

--- modules/hivemq/build.gradle.kts
+++ modules/hivemq/build.gradle.kts
@@ -6,31 +6,31 @@ plugins {
 
 dependencies {
     api(project(":testcontainers"))
-    api("org.jetbrains:annotations:${property("annotations.version")}")
+    api("org.jetbrains:annotations:23.0.0")
 
-    shaded("org.apache.commons:commons-lang3:${property("commons-lang.version")}")
-    shaded("commons-io:commons-io:${property("commons-io.version")}")
-    shaded("org.javassist:javassist:${property("javassist.version")}")
-    shaded("org.jboss.shrinkwrap:shrinkwrap-api:${property("shrinkwrap.version")}")
-    shaded("org.jboss.shrinkwrap:shrinkwrap-impl-base:${property("shrinkwrap.version")}")
-    shaded("net.lingala.zip4j:zip4j:${property("zip4j.version")}")
+    shaded("org.apache.commons:commons-lang3:3.12.0")
+    shaded("commons-io:commons-io:2.11.0")
+    shaded("org.javassist:javassist:3.28.0-GA")
+    shaded("org.jboss.shrinkwrap:shrinkwrap-api:1.2.6")
+    shaded("org.jboss.shrinkwrap:shrinkwrap-impl-base:1.2.6")
+    shaded("net.lingala.zip4j:zip4j:2.9.0")
 
     /*  This dependency needs to be explicitly added, because shrinkwrap-resolver-api-maven-embedded
         and shrinkwrap-resolver-impl-maven-embedded depend on different versions of it.
         This would lead to issues when the HiveMQ container is included in maven projects. */
     shaded("org.codehaus.plexus:plexus-utils:3.2.1")
-    shaded("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven-embedded:${property("shrinkwrap-resolver.version")}") {
+    shaded("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven-embedded:3.1.4") {
         exclude("org.codehouse.plexus", "plexus-utils")
     }
-    runtimeOnly("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven-embedded:${property("shrinkwrap-resolver.version")}") {
+    runtimeOnly("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven-embedded:3.1.4") {
         exclude("org.codehouse.plexus", "plexus-utils")
     }
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit5.version")}")
-    testImplementation("com.hivemq:hivemq-extension-sdk:${property("hivemq-extension-sdk.version")}")
-    testImplementation("com.hivemq:hivemq-mqtt-client:${property("hivemq-mqtt-client.version")}")
-    testImplementation("org.apache.httpcomponents:httpclient:${property("httpclient.version")}")
-    testImplementation("ch.qos.logback:logback-classic:${property("logback.version")}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("com.hivemq:hivemq-extension-sdk:4.7.3")
+    testImplementation("com.hivemq:hivemq-mqtt-client:1.3.0")
+    testImplementation("org.apache.httpcomponents:httpclient:4.5.13")
+    testImplementation("ch.qos.logback:logback-classic:1.2.3")
 
 }
 
