9388d3da3c0d065d1677666dc0c376bd1e243239
Falko Modler <famod@users.noreply.github.com>
2022-04-06 17:01:03 +0200

    Fix ScriptUtils.runInitScript() in hierarchical classloader setup by using TCCL (#5167)

--- modules/database-commons/src/main/java/org/testcontainers/ext/ScriptUtils.java
+++ modules/database-commons/src/main/java/org/testcontainers/ext/ScriptUtils.java
@@ -293,10 +293,13 @@ public abstract class ScriptUtils {
 	 */
 	public static void runInitScript(DatabaseDelegate databaseDelegate, String initScriptPath) {
 		try {
-			URL resource = ScriptUtils.class.getClassLoader().getResource(initScriptPath);
+			URL resource = Thread.currentThread().getContextClassLoader().getResource(initScriptPath);
 			if (resource == null) {
-				LOGGER.warn("Could not load classpath init script: {}", initScriptPath);
-				throw new ScriptLoadException("Could not load classpath init script: " + initScriptPath + ". Resource not found.");
+				resource = ScriptUtils.class.getClassLoader().getResource(initScriptPath);
+				if (resource == null) {
+					LOGGER.warn("Could not load classpath init script: {}", initScriptPath);
+					throw new ScriptLoadException("Could not load classpath init script: " + initScriptPath + ". Resource not found.");
+				}
 			}
 			String scripts = IOUtils.toString(resource, StandardCharsets.UTF_8);
 			executeDatabaseScript(databaseDelegate, initScriptPath, scripts);
