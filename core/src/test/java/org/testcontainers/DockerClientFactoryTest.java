48891d9c234c1a6c74a922ec9be1f550c2e40b80
Sergei Egorov <bsideup@gmail.com>
2022-05-17 18:02:08 -0400

    Remove the disk space check (#5381)

--- core/src/test/java/org/testcontainers/DockerClientFactoryTest.java
+++ core/src/test/java/org/testcontainers/DockerClientFactoryTest.java
@@ -2,8 +2,6 @@ package org.testcontainers;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.rnorth.visibleassertions.VisibleAssertions;
-import org.testcontainers.DockerClientFactory.DiskSpaceUsage;
 import org.testcontainers.dockerclient.LogToStringContainerCallback;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MockTestcontainersConfigurationRule;
@@ -37,15 +35,6 @@ public class DockerClientFactoryTest {
                         .toString()
             );
         }
-    }
-
-    @Test
-    public void shouldHandleBigDiskSize() throws Exception {
-        String dfOutput = "/dev/disk1     2982480572 1491240286 2982480572    31%    /";
-        DiskSpaceUsage usage = DockerClientFactory.instance().parseAvailableDiskSpace(dfOutput);
-
-        VisibleAssertions.assertEquals("Available MB is correct", 2982480572L / 1024L, usage.availableMB.orElse(0L));
-        VisibleAssertions.assertEquals("Available percentage is correct", 31, usage.usedPercent.orElse(0));
     }
 
     @Test
