aa24a610331f6eb4e32afc67bc04383c14c010d7
Julien Baillagou <julien@julb.me>
2022-07-29 09:20:23 +0200

    consul-module: take additional remarks into account

--- modules/consul/src/main/java/org/testcontainers/consul/ConsulContainer.java
+++ modules/consul/src/main/java/org/testcontainers/consul/ConsulContainer.java
@@ -1,26 +1,24 @@
 package org.testcontainers.consul;
 
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.Capability;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.github.dockerjava.api.model.Capability;
-
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.utility.DockerImageName;
-
-
 /**
  * GenericContainer subclass for Consul specific configuration and features. The main feature is the
- * withPropertyInConsul method, where users can specify keys and values to be pre-loaded into Consul for
+ * withConsulCommand method, where users can specify consul commands to prepare Consul for
  * their specific test scenario.
  * <p>
  */
-public class ConsulContainer<SELF extends ConsulContainer<SELF>> extends GenericContainer<SELF> {
+public class ConsulContainer extends GenericContainer<ConsulContainer> {
 
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("consul");
 
@@ -29,9 +27,7 @@ public class ConsulContainer<SELF extends ConsulContainer<SELF>> extends Generic
 
     private List<String> initCommands = new ArrayList<>();
 
-    private int httpPort = CONSUL_HTTP_PORT;
-    private int grpcPort = CONSUL_GRPC_PORT;
-    private String[] startConsulCmd = new String[]{"agent", "-dev", "-client", "0.0.0.0"};
+    private String[] startConsulCmd = new String[] { "agent", "-dev", "-client", "0.0.0.0" };
 
     public ConsulContainer(String dockerImageName) {
         this(DockerImageName.parse(dockerImageName));
@@ -39,16 +35,15 @@ public class ConsulContainer<SELF extends ConsulContainer<SELF>> extends Generic
 
     public ConsulContainer(final DockerImageName dockerImageName) {
         super(dockerImageName);
-
         dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
 
         // Use the status leader endpoint to verify if consul is running.
-        setWaitStrategy(Wait.forHttp("/v1/status/leader").forPort(httpPort).forStatusCode(200));
+        setWaitStrategy(Wait.forHttp("/v1/status/leader").forPort(CONSUL_HTTP_PORT).forStatusCode(200));
 
         withCreateContainerCmdModifier(cmd -> cmd.withCapAdd(Capability.IPC_LOCK));
-        withEnv("CONSUL_ADDR", "http://0.0.0.0:" + httpPort);
+        withEnv("CONSUL_ADDR", "http://0.0.0.0:" + CONSUL_HTTP_PORT);
         withCommand(startConsulCmd);
-        withExposedPorts(httpPort, grpcPort);
+        withExposedPorts(CONSUL_HTTP_PORT, CONSUL_GRPC_PORT);
     }
 
     @Override
@@ -58,33 +53,49 @@ public class ConsulContainer<SELF extends ConsulContainer<SELF>> extends Generic
 
     private void runConsulCommands() {
         if (!initCommands.isEmpty()) {
-            String commands = initCommands.stream()
-                                    .map(command -> "consul " + command)
-                                    .collect(Collectors.joining(" && "));
+            String commands = initCommands
+                .stream()
+                .map(command -> "consul " + command)
+                .collect(Collectors.joining(" && "));
             try {
-                ExecResult execResult = this.execInContainer(new String[]{"/bin/sh", "-c", commands});
+                ExecResult execResult = this.execInContainer(new String[] { "/bin/sh", "-c", commands });
                 if (execResult.getExitCode() != 0) {
-                    logger().error("Failed to execute these init commands {}. Exit code {}. Stdout {}. Stderr {}",
-                                    initCommands, execResult.getExitCode(), execResult.getStdout(), execResult.getStderr());
+                    logger()
+                        .error(
+                            "Failed to execute these init commands {}. Exit code {}. Stdout {}. Stderr {}",
+                            initCommands,
+                            execResult.getExitCode(),
+                            execResult.getStdout(),
+                            execResult.getStderr()
+                        );
                 }
             } catch (IOException | InterruptedException e) {
-                logger().error("Failed to execute these init commands {}. Exception message: {}", initCommands, e.getMessage());
+                logger()
+                    .error(
+                        "Failed to execute these init commands {}. Exception message: {}",
+                        initCommands,
+                        e.getMessage()
+                    );
             }
         }
     }
 
     /**
-     * Run initialization commands using the consul cli.
-     * 
+     * Run consul commands using the consul cli.
+     *
      * Useful for enableing more secret engines like:
      * <pre>
-     *     .withInitCommand("secrets enable pki")
-     *     .withInitCommand("secrets enable transit")
+     *     .withConsulCommand("secrets enable pki")
+     *     .withConsulCommand("secrets enable transit")
+     * </pre>
+     * or register specific K/V like:
+     * <pre>
+     *     .withConsulCommand("kv put config/testing1 value123")
      * </pre>
      * @param commands The commands to send to the consul cli
      * @return this
      */
-    public SELF withInitCommand(String... commands) {
+    public ConsulContainer withConsulCommand(String... commands) {
         initCommands.addAll(Arrays.asList(commands));
         return self();
     }
--- modules/consul/src/test/java/org/testcontainers/consul/ConsulContainerTest.java
+++ modules/consul/src/test/java/org/testcontainers/consul/ConsulContainerTest.java
@@ -1,35 +1,33 @@
 package org.testcontainers.consul;
 
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.kv.model.GetValue;
+import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
-import java.nio.charset.StandardCharsets;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.assertj.core.api.Assertions;
-
-import com.ecwid.consul.v1.ConsulClient;
-import com.ecwid.consul.v1.Response;
-import com.ecwid.consul.v1.kv.model.GetValue;
-
 /**
  * This test shows the pattern to use the ConsulContainer @ClassRule for a junit test. It also has tests that ensure
- * the properties were added correctly by reading from Vault with the CLI and over HTTP.
+ * the properties were added correctly by reading from Consul with the CLI and over HTTP.
  */
 public class ConsulContainerTest {
 
     @ClassRule
-    public static ConsulContainer<?> consulContainer = new ConsulContainer<>(ConsulTestImages.CONSUL_IMAGE)
-        .withInitCommand("kv put config/testing1 value123");
+    public static ConsulContainer consulContainer = new ConsulContainer(ConsulTestImages.CONSUL_IMAGE)
+        .withConsulCommand("kv put config/testing1 value123");
 
     @Test
     public void readFirstPropertyPathWithCli() throws IOException, InterruptedException {
@@ -40,16 +38,23 @@ public class ConsulContainerTest {
 
     @Test
     public void readFirstSecretPathOverHttpApi() throws InterruptedException {
-        given().
-            when().
-            get("http://" + getHostAndPort() + "/v1/kv/config/testing1").
-            then().
-            assertThat().body("[0].Value", equalTo(Base64.getEncoder().encodeToString("value123".getBytes(StandardCharsets.UTF_8))));
+        given()
+            .when()
+            .get("http://" + getHostAndPort() + "/v1/kv/config/testing1")
+            .then()
+            .assertThat()
+            .body(
+                "[0].Value",
+                equalTo(Base64.getEncoder().encodeToString("value123".getBytes(StandardCharsets.UTF_8)))
+            );
     }
 
     @Test
     public void writeAndReadMultipleValuesUsingClient() {
-        final ConsulClient consulClient = new ConsulClient(consulContainer.getHost(), consulContainer.getFirstMappedPort());
+        final ConsulClient consulClient = new ConsulClient(
+            consulContainer.getHost(),
+            consulContainer.getFirstMappedPort()
+        );
 
         final Map<String, String> properties = new HashMap<>();
         properties.put("value", "world");
