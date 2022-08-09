fbf6648cdd45845198e81c0ec25b3c1f035a6e54
Aran Moncusí Ramírez <aran.moncusi@wallapop.com>
2022-08-09 12:16:27 +0200

    Add Javadocs for public methods

--- modules/dynamodb/src/main/java/org/testcontainers/containers/DynamoDBContainer.java
+++ modules/dynamodb/src/main/java/org/testcontainers/containers/DynamoDBContainer.java
@@ -103,7 +103,6 @@ public class DynamoDBContainer extends GenericContainer<DynamoDBContainer> {
         }
 
         if (flagInMemory) {
-
             Preconditions.checkState(Objects.isNull(dbPath), "You can't specify both dbPath and inMemory at once.");
 
             commands.add("-inMemory");
@@ -117,91 +116,193 @@ public class DynamoDBContainer extends GenericContainer<DynamoDBContainer> {
         return commands.toArray(new String[] {});
     }
 
+    /**
+     * Get current port for HTTP DynamoDB API
+     *
+     * @return Current mapped port, never null.
+     */
+    @NonNull
     public Integer getPort() {
         return getMappedPort(DEFAULT_HTTP_PORT);
     }
 
-    public DynamoDBContainer withAccessKeys(final String accessKey, final String secretKey) {
-        withEnv("AWS_ACCESS_KEY_ID", accessKey);
-        withEnv("AWS_SECRET_ACCESS_KEY", secretKey);
-
-        return self();
-    }
-
+    /**
+     * Specify the allowed domains for CORS in a comma-separated "raw" format. These values are put directly in the
+     * '-port' option.
+     *
+     * @param cors Raw string with allowed domains.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withCors(final String cors) {
         this.cors = cors;
         return self();
     }
 
+    /**
+     * Default CORS value to allow all domains.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withCors() {
         return withCors("*");
     }
 
+    /**
+     * Array list of allowed domains for CORS.
+     *
+     * @param domains List of allowed domains for CORS.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withCors(final String... domains) {
         return withCors(String.join(",", domains));
     }
 
+    /**
+     * Set the flag '-sharedDb' on container commands.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withFlagSharedDB(final Boolean isActiveFlag) {
         this.flagSharedDb = isActiveFlag;
         return self();
     }
 
+    /**
+     * Enable the flag sharedDb;
+     *
+     * @return self instance.
+     * @see #withFlagSharedDB(Boolean)
+     */
     public DynamoDBContainer withEnableSharedDB() {
         withFlagSharedDB(true);
         return self();
     }
 
+    /**
+     * Disable the flag sharedDb;
+     *
+     * @return self instance.
+     * @see #withFlagSharedDB(Boolean)
+     */
     public DynamoDBContainer withDisableSharedDB() {
         withFlagSharedDB(false);
         return self();
     }
 
+    /**
+     * Set the flag '-delayTransientStatuses' on container commands.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withFlagDelayTransientStatuses(final Boolean isActiveFlag) {
         this.flagDelayTransientStatuses = isActiveFlag;
         return self();
     }
 
+    /**
+     * Enable the flag delayTransientStatuses;
+     *
+     * @return self instance.
+     * @see #withFlagDelayTransientStatuses(Boolean)
+     */
     public DynamoDBContainer withEnableDelayTransientStatuses() {
         withFlagDelayTransientStatuses(true);
         return self();
     }
 
+    /**
+     * Disable the flag delayTransientStatuses;
+     *
+     * @return self instance.
+     * @see #withFlagDelayTransientStatuses(Boolean)
+     */
     public DynamoDBContainer withDisableDelayTransientStatuses() {
         withFlagDelayTransientStatuses(false);
         return self();
     }
 
+    /**
+     * Set the flag '-optimizeDbBeforeStartup' on container commands.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withFlagOptimizeDbBeforeStartup(final Boolean isActiveFlag) {
         this.flagOptimizeDbBeforeStartup = isActiveFlag;
         return self();
     }
 
+    /**
+     * Enable the flag optimizeDbBeforeStartup;
+     *
+     * @return self instance.
+     * @see #withFlagOptimizeDbBeforeStartup(Boolean)
+     */
     public DynamoDBContainer withEnableOptimizeDbBeforeStartup() {
         withFlagOptimizeDbBeforeStartup(true);
         return self();
     }
 
+    /**
+     * Disable the flag optimizeDbBeforeStartup;
+     *
+     * @return self instance.
+     * @see #withFlagOptimizeDbBeforeStartup(Boolean)
+     */
     public DynamoDBContainer withDisableOptimizeDbBeforeStartup() {
         withFlagOptimizeDbBeforeStartup(false);
         return self();
     }
 
+    /**
+     * Enable '-inMemory' flag and set null dbPath attribute.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withInMemory() {
         this.flagInMemory = true;
         this.dbPath = null;
         return self();
     }
 
+    /**
+     * Disable '-inMemory' flag and set the dbPath attribute.
+     *
+     * @param path Expected dbPath. Nullable values puts the default value.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withFilePath(final String path) {
         this.flagInMemory = false;
         this.dbPath = path;
         return self();
     }
 
+    /**
+     * Disable '-inMemory' flag and set default value of dbPath attribute.
+     *
+     * @return self instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.CommandLineOptions">AWS DynamoDB Docs for Downloaded version</a>
+     */
     public DynamoDBContainer withFilePath() {
         return withFilePath(null);
     }
 
+    /**
+     * Build a URL allowed for AWS SDK parameter '--endpoint' attribute.
+     *
+     * @return HTTP URL for current instance.
+     * @see <a href="https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.UsageNotes.html#DynamoDBLocal.Endpoint">AWS DynamoDB Docs for Downloaded version</a>
+     */
     @SuppressWarnings("HttpUrlsUsage")
     public String getEndpointUrl() {
         return "http://" + getHost() + ":" + getPort().toString();
