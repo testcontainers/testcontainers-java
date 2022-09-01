38adf4ce90886517b8cf32eeae4bb2de0060398a
Eddú Meléndez <eddu.melendez@gmail.com>
2022-09-01 12:19:11 -0500

    Rollback 1.0.4 image

--- modules/localstack/src/test/java/org/testcontainers/containers/localstack/LocalstackTestImages.java
+++ modules/localstack/src/test/java/org/testcontainers/containers/localstack/LocalstackTestImages.java
@@ -3,7 +3,7 @@ package org.testcontainers.containers.localstack;
 import org.testcontainers.utility.DockerImageName;
 
 public interface LocalstackTestImages {
-    DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack:1.0.4");
+    DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack:0.12.8");
     DockerImageName LOCALSTACK_0_7_IMAGE = LOCALSTACK_IMAGE.withTag("0.7.0");
     DockerImageName LOCALSTACK_0_10_IMAGE = LOCALSTACK_IMAGE.withTag("0.10.7");
     DockerImageName LOCALSTACK_0_11_IMAGE = LOCALSTACK_IMAGE.withTag("0.11.3");
