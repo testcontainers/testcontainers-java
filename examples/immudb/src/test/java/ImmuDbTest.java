295f10c959bbd0236d8cae4cf7c0900fa060c440
Hari Ohm Prasth Rajagopal <hariohmprasath@gmail.com>
2022-09-24 05:50:35 -0700

    Addressed code review feedback

--- examples/immudb/src/test/java/ImmuDbTest.java
+++ examples/immudb/src/test/java/ImmuDbTest.java
@@ -1,7 +1,3 @@
-import java.util.List;
-
-import com.mycompany.immudb.ImmuDbClient;
-import com.mycompany.immudb.ImmuDbClientImpl;
 import io.codenotary.immudb4j.Entry;
 import io.codenotary.immudb4j.ImmuClient;
 import io.codenotary.immudb4j.exceptions.CorruptedDataException;
@@ -11,7 +7,8 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.wait.strategy.Wait;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Test class for the ImmuDbClient.
@@ -29,10 +26,10 @@ public class ImmuDbTest {
 
     // Test container for the ImmuDb database, with the latest version of the image and exposed port
     @ClassRule
-    public static final GenericContainer<?> immuDbContainer = new GenericContainer<>(DockerImageName.parse("codenotary/immudb:latest"))
-        .withExposedPorts(IMMUDB_PORT).withReuse(true);
-    // Interface for the ImmuDbClient
-    private ImmuDbClient immuDbClient;
+    public static final GenericContainer<?> immuDbContainer = new GenericContainer<>("codenotary/immudb:latest")
+        .withExposedPorts(IMMUDB_PORT).waitingFor(
+            Wait.forLogMessage(".*Web API server enabled.*", 1)
+        );
     // ImmuClient used to interact with the DB
     private ImmuClient immuClient;
 
@@ -47,124 +44,22 @@ public class ImmuDbTest {
             .build();
         this.immuClient.login(IMMUDB_USER, IMMUDB_PASSWORD);
         this.immuClient.useDatabase(IMMUDB_DATABASE);
-
-        // Initialize the ImmuDbClient
-        this.immuDbClient = new ImmuDbClientImpl(this.immuClient);
     }
 
-    /**
-     * Test method for the putValue.
-     */
-    @Test
-    public void testPutValue() {
-        try {
-            this.immuDbClient.putValue("key", "value");
-        }
-        catch (CorruptedDataException e) {
-            Assert.fail();
-        }
-    }
-
-    /**
-     * Test method for the getValue.
-     */
     @Test
     public void testGetValue() {
         try {
-            this.immuDbClient.putValue("test1", "test2");
-            Assert.assertEquals("test2", this.immuDbClient.getValue("test1"));
-        }
-        catch (CorruptedDataException | VerificationException e) {
-            Assert.fail();
-        }
-    }
+            immuClient.set("test1", "test2".getBytes());
 
-    /**
-     * Test method for the getAllValues.
-     */
-    @Test
-    public void testGetAllValues() {
-        try {
-            this.immuDbClient.putValue("test1", "value1");
-            this.immuDbClient.putValue("test2", "value2");
-            this.immuDbClient.putValue("test3", "value3");
+            Entry entry = immuClient.verifiedGet("test1");
 
-            final List<Entry> allValues = this.immuDbClient.getAllValues(List.of("test1", "test2", "test3"));
-            for (Entry e : allValues) {
-                byte[] k = e.getKey();
-                byte[] v = e.getValue();
-
-                String key = new String(k);
-                String value = new String(v);
-                switch (key) {
-                case "test1":
-                    Assert.assertEquals("value1", value);
-                    break;
-                case "test2":
-                    Assert.assertEquals("value2", value);
-                    break;
-                case "test3":
-                    Assert.assertEquals("value3", value);
-                    break;
-                default:
-                    Assert.fail();
-                }
+            if (entry != null) {
+                byte[] value = entry.getValue();
+                assertThat(new String(value)).isEqualTo("test2");
+            } else {
+                Assert.fail();
             }
-        }
-        catch (CorruptedDataException e) {
-            Assert.fail();
-        }
-    }
-
-    /**
-     * Test method for the getHistory.
-     */
-    @Test
-    public void testGetHistoryValues(){
-        try {
-            String historyKey = "history1";
-            this.immuDbClient.putValue(historyKey, "value1");
-            this.immuDbClient.putValue(historyKey, "value2");
-            this.immuDbClient.putValue(historyKey, "value3");
-
-            final List<Entry> allValues = this.immuDbClient.getHistoryValues(historyKey, 10);
-
-            Assert.assertEquals("history1", new String(allValues.get(0).getKey()));
-            Assert.assertEquals("value1", new String(allValues.get(0).getValue()));
-
-            Assert.assertEquals("history1", new String(allValues.get(1).getKey()));
-            Assert.assertEquals("value2", new String(allValues.get(1).getValue()));
-
-            Assert.assertEquals("history1", new String(allValues.get(2).getKey()));
-            Assert.assertEquals("value3", new String(allValues.get(2).getValue()));
-        }
-        catch (CorruptedDataException e) {
-            Assert.fail();
-        }
-    }
-
-    /**
-     * Test method for the scan.
-     */
-    @Test
-    public void testScan(){
-        try {
-            String prefix = "scan";
-            String key1 = prefix + "1";
-            String key2 = prefix + "2";
-
-            this.immuDbClient.putValue(key1, "value1");
-            this.immuDbClient.putValue(key2, "value2");
-
-            final List<Entry> allValues = this.immuDbClient.scanKeys(prefix, 2);
-
-            Assert.assertEquals("scan1", new String(allValues.get(0).getKey()));
-            Assert.assertEquals("value1", new String(allValues.get(0).getValue()));
-
-            Assert.assertEquals("scan2", new String(allValues.get(1).getKey()));
-            Assert.assertEquals("value2", new String(allValues.get(1).getValue()));
-        }
-        catch (CorruptedDataException e) {
+        } catch (CorruptedDataException | VerificationException e) {
             Assert.fail();
         }
     }
