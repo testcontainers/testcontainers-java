26716eae9b3da3b38d5c25c088be519bba352cb0
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-28 15:53:22 +0900

    feat: DemoEntity add lombok annotation

--- examples/spring-boot-mysql/src/main/java/com/example/DemoEntity.java
+++ examples/spring-boot-mysql/src/main/java/com/example/DemoEntity.java
@@ -3,13 +3,20 @@ package com.example;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@Builder
 public class DemoEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
