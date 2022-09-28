95f893c676bf62fb680f20d1978570ed87f71712
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-28 15:54:39 +0900

    test: add schema file used in test module

---
+++ examples/spring-boot-mysql/src/test/resources/schema-test.sql
@@ -0,0 +1,7 @@
+CREATE TABLE demo_entity
+(
+    id      BIGINT         NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255)   NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
