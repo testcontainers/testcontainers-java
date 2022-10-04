84095355305cf9ba58040f4a2cc783f5b3a3441a
Jan-Hendrik Dolling <JanHendrikDolling@users.noreply.github.com>
2022-10-04 22:08:46 +0200

    Remove unnecessary configuration from simple tests (#5973)
    * Remove mysql configuration from simple test
    * mysql custom ini file test also runs on Windows
    
    Co-authored-by: Kevin Wittek <kiview@users.noreply.github.com>

--- modules/mysql/src/test/java/org/testcontainers/junit/mysql/SimpleMySQLTest.java
+++ modules/mysql/src/test/java/org/testcontainers/junit/mysql/SimpleMySQLTest.java
@@ -1,6 +1,5 @@
 package org.testcontainers.junit.mysql;
 
-import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +20,6 @@ import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.junit.Assume.assumeFalse;
 
 public class SimpleMySQLTest extends AbstractContainerDatabaseTest {
 
@@ -48,7 +46,6 @@ public class SimpleMySQLTest extends AbstractContainerDatabaseTest {
     public void testSimple() throws SQLException {
         try (
             MySQLContainer<?> mysql = new MySQLContainer<>(MySQLTestImages.MYSQL_57_IMAGE)
-                .withConfigurationOverride("somepath/mysql_conf_override")
                 .withLogConsumer(new Slf4jLogConsumer(logger))
         ) {
             mysql.start();
@@ -81,8 +78,6 @@ public class SimpleMySQLTest extends AbstractContainerDatabaseTest {
 
     @Test
     public void testMySQLWithCustomIniFile() throws SQLException {
-        assumeFalse(SystemUtils.IS_OS_WINDOWS);
-
         try (
             MySQLContainer<?> mysqlCustomConfig = new MySQLContainer<>(MySQLTestImages.MYSQL_56_IMAGE)
                 .withConfigurationOverride("somepath/mysql_conf_override")
