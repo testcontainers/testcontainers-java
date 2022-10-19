5ea3f6dd7520ce441a9001ad9600647ae293a1d9
pwangryn <pawel.wangryn@icsec.pl>
2022-10-19 09:55:45 +0200

    Add more tests for questDB

--- modules/questdb/src/test/java/org/testcontainers/junit/questdb/SimpleQuestDBTest.java
+++ modules/questdb/src/test/java/org/testcontainers/junit/questdb/SimpleQuestDBTest.java
@@ -1,16 +1,31 @@
 package org.testcontainers.junit.questdb;
 
+import io.questdb.client.Sender;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.testcontainers.QuestDBTestImages;
 import org.testcontainers.containers.QuestDBContainer;
 import org.testcontainers.db.AbstractContainerDatabaseTest;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SimpleQuestDBTest extends AbstractContainerDatabaseTest {
+
+
+    private static final String TABLE_NAME = "mytable";
 
     @Test
     public void testSimple() throws SQLException {
@@ -21,6 +36,42 @@ public class SimpleQuestDBTest extends AbstractContainerDatabaseTest {
 
             int resultSetInt = resultSet.getInt(1);
             assertThat(resultSetInt).as("A basic SELECT query succeeds").isEqualTo(1);
+        }
+    }
+
+    @Test
+    public void testRest() throws IOException {
+        try (QuestDBContainer questdb = new QuestDBContainer(QuestDBTestImages.QUESTDB_IMAGE)) {
+            questdb.start();
+            populateByInfluxLineProtocol(questdb, 1_000);
+            try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+                String encodedSql = URLEncoder.encode("select * from " + TABLE_NAME, "UTF-8");
+                HttpGet httpGet = new HttpGet(questdb.getHttpUrl() + "/exec?query=" + encodedSql);
+                await()
+                    .untilAsserted(() -> {
+                        try (CloseableHttpResponse response = client.execute(httpGet)) {
+                            assertEquals(200, response.getStatusLine().getStatusCode());
+                            String json = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                            assertTrue(
+                                "questdb response '" + json + "' does not contain expected count 1000",
+                                json.contains("\"count\":1000")
+                            );
+                        }
+                    });
+            }
+        }
+    }
+
+    private static void populateByInfluxLineProtocol(QuestDBContainer questdb, int rowCount) {
+        try (Sender sender = Sender.builder().address(questdb.getIlpUrl()).build()) {
+            for (int i = 0; i < rowCount; i++) {
+                sender
+                    .table(TABLE_NAME)
+                    .symbol("sym", "sym1" + i)
+                    .stringColumn("str", "str1" + i)
+                    .longColumn("long", i)
+                    .atNow();
+            }
         }
     }
 }
