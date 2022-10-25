2e8e8ce233c21d93fa5c02155c98c6ee676108e1
Eddú Meléndez <eddu.melendez@gmail.com>
2022-10-25 08:54:08 -0500

    Apply spotless

--- examples/zookeeper/src/test/java/com/example/ZookeeperContainerTest.java
+++ examples/zookeeper/src/test/java/com/example/ZookeeperContainerTest.java
@@ -18,16 +18,19 @@ public class ZookeeperContainerTest {
     public void test() throws Exception {
         String path = "/messages/zk-tc";
         String content = "Running Zookeeper with Testcontainers";
-        try(GenericContainer<?> zookeeper = new GenericContainer<>("zookeeper:3.8.0")
-            .withExposedPorts(ZOOKEEPER_PORT)) {
+        try (
+            GenericContainer<?> zookeeper = new GenericContainer<>("zookeeper:3.8.0").withExposedPorts(ZOOKEEPER_PORT)
+        ) {
             zookeeper.start();
 
             String connectionString = zookeeper.getHost() + ":" + zookeeper.getMappedPort(ZOOKEEPER_PORT);
-            CuratorFramework curatorFramework = CuratorFrameworkFactory.builder().connectString(connectionString)
-                .retryPolicy(new RetryOneTime(100)).build();
+            CuratorFramework curatorFramework = CuratorFrameworkFactory
+                .builder()
+                .connectString(connectionString)
+                .retryPolicy(new RetryOneTime(100))
+                .build();
             curatorFramework.start();
-            curatorFramework.create().creatingParentsIfNeeded().forPath(path,
-                content.getBytes());
+            curatorFramework.create().creatingParentsIfNeeded().forPath(path, content.getBytes());
 
             byte[] bytes = curatorFramework.getData().forPath(path);
             curatorFramework.close();
@@ -35,5 +38,4 @@ public class ZookeeperContainerTest {
             assertThat(new String(bytes, StandardCharsets.UTF_8)).isEqualTo(content);
         }
     }
-
 }
