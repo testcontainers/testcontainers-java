92cb9fcae58763764fcac0acff47a3a36b5eb52e
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2022-10-25 08:52:40 -0500

    Apply spotless and checkstyle to examples (#6061)
    Currently, spotless and checkstyle has been applied to the core
    modules. Now, both will execute to core modules and examples.

--- examples/immudb/src/test/java/ImmuDbTest.java
+++ examples/immudb/src/test/java/ImmuDbTest.java
@@ -8,6 +8,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -17,28 +18,33 @@ public class ImmuDbTest {
 
     // Default port for the ImmuDb server
     private static final int IMMUDB_PORT = 3322;
+
     // Default username for the ImmuDb server
     private final String IMMUDB_USER = "immudb";
+
     // Default password for the ImmuDb server
     private final String IMMUDB_PASSWORD = "immudb";
+
     // Default database name for the ImmuDb server
     private final String IMMUDB_DATABASE = "defaultdb";
 
     // Test container for the ImmuDb database, with the latest version of the image and exposed port
     @ClassRule
     public static final GenericContainer<?> immuDbContainer = new GenericContainer<>("codenotary/immudb:1.3")
-        .withExposedPorts(IMMUDB_PORT).waitingFor(
-            Wait.forLogMessage(".*Web API server enabled.*", 1)
-        );
+        .withExposedPorts(IMMUDB_PORT)
+        .waitingFor(Wait.forLogMessage(".*Web API server enabled.*", 1));
+
     // ImmuClient used to interact with the DB
     private ImmuClient immuClient;
 
     @Before
     public void setUp() {
-        this.immuClient = ImmuClient.newBuilder()
-            .withServerUrl(immuDbContainer.getHost())
-            .withServerPort(immuDbContainer.getMappedPort(IMMUDB_PORT))
-            .build();
+        this.immuClient =
+            ImmuClient
+                .newBuilder()
+                .withServerUrl(immuDbContainer.getHost())
+                .withServerPort(immuDbContainer.getMappedPort(IMMUDB_PORT))
+                .build();
         this.immuClient.login(IMMUDB_USER, IMMUDB_PASSWORD);
         this.immuClient.useDatabase(IMMUDB_DATABASE);
     }
