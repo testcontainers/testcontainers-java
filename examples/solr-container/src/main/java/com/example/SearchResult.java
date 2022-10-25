84b1ad4efd55d53c068328fa823c1b02390831b8
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2022-10-25 08:52:40 -0500

    Apply spotless and checkstyle to examples (#6061)
    Currently, spotless and checkstyle has been applied to the core
    modules. Now, both will execute to core modules and examples.

--- examples/solr-container/src/main/java/com/example/SearchResult.java
+++ examples/solr-container/src/main/java/com/example/SearchResult.java
@@ -1,12 +1,12 @@
 package com.example;
 
-import java.util.List;
-import java.util.Map;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
