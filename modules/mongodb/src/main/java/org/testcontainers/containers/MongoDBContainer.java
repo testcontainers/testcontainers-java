c1331e2d193ec8fdd14024ae61efc2908b7575ec
Bounkong Khamphousone <bounkong@gmail.com>
2022-12-02 22:43:54 +0100

    Fix MongoDBContainer when reuse is enabled (#6235)
    Co-authored-by: Eddú Meléndez <eddu.melendez@gmail.com>

--- modules/mongodb/src/main/java/org/testcontainers/containers/MongoDBContainer.java
+++ modules/mongodb/src/main/java/org/testcontainers/containers/MongoDBContainer.java
@@ -82,8 +82,12 @@ public class MongoDBContainer extends GenericContainer<MongoDBContainer> {
     }
 
     @Override
-    protected void containerIsStarted(InspectContainerResponse containerInfo) {
-        initReplicaSet();
+    protected void containerIsStarted(InspectContainerResponse containerInfo, boolean reused) {
+        if (reused && isReplicationSetAlreadyInitialized()) {
+            log.debug("Replica set already initialized.");
+        } else {
+            initReplicaSet();
+        }
     }
 
     private String[] buildMongoEvalCommand(final String command) {
@@ -150,5 +154,14 @@ public class MongoDBContainer extends GenericContainer<MongoDBContainer> {
         ReplicaSetInitializationException(final String errorMessage) {
             super(errorMessage);
         }
+    }
+
+    @SneakyThrows
+    private boolean isReplicationSetAlreadyInitialized() {
+        // since we are creating a replica set with one node, this node must be primary (state = 1)
+        final ExecResult execCheckRsInit = execInContainer(
+            buildMongoEvalCommand("if(db.adminCommand({replSetGetStatus: 1})['myState'] != 1) quit(900)")
+        );
+        return execCheckRsInit.getExitCode() == CONTAINER_EXIT_CODE_OK;
     }
 }
