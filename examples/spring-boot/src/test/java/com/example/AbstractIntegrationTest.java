894f4dc30d23782b1c6120b0517335eee4fea076
nagkumar <nagendra.raja@tejasoft.com>
2023-05-01 07:14:38 +0530

    spring-boot AbstractIntegrationTest runnder migration complete

--- examples/spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
+++ examples/spring-boot/src/test/java/com/example/AbstractIntegrationTest.java
@@ -1,16 +1,17 @@
 package com.example;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(
     classes = DemoApplication.class,
     webEnvironment = WebEnvironment.RANDOM_PORT,
