cce90eb522e3117044e91537ad85804818bae8da
Denilsson Montoya <dmontoyane@gmail.com>
2023-05-01 23:02:47 -0500

    [Enhancement]: Update waiting strategy for Mockserver container (#6951)
    Replace `Wait.forHttp` for `Wait.forLogMessage`. This change will allow Mockserver to work with HTTP and HTTPS requests.
    
    Fixes #6647

--- modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
+++ modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
@@ -34,6 +34,53 @@ public class MockServerContainerTest {
     }
 
     @Test
+    public void shouldCallMockserverUsingTlsProtocol() throws Exception {
+        try (MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)) {
+            mockServer.start();
+
+            String expectedBody = "Hello World!";
+
+            try (
+                MockServerClient client = new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+                    .withSecure(true)
+            ) {
+                assertThat(client.hasStarted()).as("Mockserver running").isTrue();
+
+                client.when(request().withPath("/hello")).respond(response().withBody(expectedBody));
+
+                assertThat(SimpleHttpClient.secureResponseFromMockserver(mockServer, "/hello"))
+                    .as("MockServer returns correct result")
+                    .isEqualTo(expectedBody);
+            }
+        }
+    }
+
+    @Test
+    public void shouldCallMockserverUsingMutualTlsProtocol() throws Exception {
+        try (
+            MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)
+                .withEnv("MOCKSERVER_TLS_MUTUAL_AUTHENTICATION_REQUIRED", "true")
+        ) {
+            mockServer.start();
+
+            String expectedBody = "Hello World!";
+
+            try (
+                MockServerClient client = new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+                    .withSecure(true)
+            ) {
+                assertThat(client.hasStarted()).as("Mockserver running").isTrue();
+
+                client.when(request().withPath("/hello")).respond(response().withBody(expectedBody));
+
+                assertThat(SimpleHttpClient.secureResponseFromMockserver(mockServer, "/hello"))
+                    .as("MockServer returns correct result")
+                    .isEqualTo(expectedBody);
+            }
+        }
+    }
+
+    @Test
     public void newVersionStartsWithDefaultWaitStrategy() {
         try (MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)) {
             mockServer.start();
