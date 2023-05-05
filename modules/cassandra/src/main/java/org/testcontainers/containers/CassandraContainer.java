4eb105092442eb19c69d526b403fd4469ebfdd91
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-05-05 13:13:06 -0700

    Update modules/cassandra/src/main/java/org/testcontainers/containers/CassandraContainer.java

--- modules/cassandra/src/main/java/org/testcontainers/containers/CassandraContainer.java
+++ modules/cassandra/src/main/java/org/testcontainers/containers/CassandraContainer.java
@@ -21,7 +21,7 @@ import javax.script.ScriptException;
 /**
  * Cassandra container
  *
- * Supports 2.x, 3.x, and 4.x Cassandra versions
+ *  Testcontainers implementation for Apache Cassandra.
  */
 public class CassandraContainer<SELF extends CassandraContainer<SELF>> extends GenericContainer<SELF> {
 
