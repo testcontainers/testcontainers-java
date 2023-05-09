c64aab9fd5e3a452ee0faf793560327eb4da9841
Daniel PETISME <daniel.petisme@gmail.com>
2023-05-10 01:55:59 +0200

    Support Kraft post Confluent Platform 7.4.0 (#7014)
    Fixes #7010
    
    
    Co-authored-by: Eddú Meléndez <eddu.melendez@gmail.com>

--- modules/kafka/src/test/java/org/testcontainers/containers/KafkaContainerTest.java
+++ modules/kafka/src/test/java/org/testcontainers/containers/KafkaContainerTest.java
@@ -133,10 +133,20 @@ public class KafkaContainerTest {
     }
 
     @Test
-    public void testUsageKraft() throws Exception {
+    public void testUsageKraftBeforeConfluentPlatformVersion74() throws Exception {
+        try (
+            KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.0.1")).withKraft()
+        ) {
+            kafka.start();
+            testKafkaFunctionality(kafka.getBootstrapServers());
+        }
+    }
+
+    @Test
+    public void testUsageKraftAfterConfluentPlatformVersion74() throws Exception {
         try (
             // withKraftMode {
-            KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.0.1")).withKraft()
+            KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0")).withKraft()
             // }
         ) {
             kafka.start();
