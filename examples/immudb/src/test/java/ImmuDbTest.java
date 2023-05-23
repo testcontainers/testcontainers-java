ad05c8698f266eb5956dc8029e4b52593e7f97bf
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2023-05-23 07:36:41 +0530

    Update examples/immudb/src/test/java/ImmuDbTest.java
    Co-authored-by: Eddú Meléndez Gonzales <eddu.melendez@gmail.com>

--- examples/immudb/src/test/java/ImmuDbTest.java
+++ examples/immudb/src/test/java/ImmuDbTest.java
@@ -30,7 +30,7 @@ public class ImmuDbTest {
     private final String IMMUDB_DATABASE = "defaultdb";
 
     // Test container for the ImmuDb database, with the latest version of the image and exposed port
-    public static final GenericContainer<?> immuDbContainer =  new GenericContainer<>("codenotary/immudb:1.3")
+    public static final GenericContainer<?> immuDbContainer = new GenericContainer<>("codenotary/immudb:1.3")
         .withExposedPorts(IMMUDB_PORT)
         .waitingFor(Wait.forLogMessage(".*Web API server enabled.*", 1));
 
