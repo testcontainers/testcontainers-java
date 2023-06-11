659ad563f00dd625a3bceab5b7d6a893a573cbab
jito-d <jito.kim@mathpresso.com>
2023-06-11 23:49:20 +0900

    I have made several changes to the "spring-boot-kotlin-redis" example to make it more Kotlin idiomatic.
    Here is a summary of the changes:
    Refactored static fields to companion objects to follow Kotlin conventions.
    Replaced the usage of autowired with constructor injection.
    Converted the build.gradle file from Groovy to Kotlin DSL to align with the project's Kotlin nature.
    
    Signed-off-by: jitokim <pigberger70@gmail.com>

--- examples/spring-boot-kotlin-redis/build.gradle.kts
+++ examples/spring-boot-kotlin-redis/build.gradle.kts
@@ -1,28 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("org.springframework.boot") version "2.7.10"
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.8.20"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.spring") version "1.8.20"
 }
 
-apply plugin: 'io.spring.dependency-management'
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    apply(plugin = "io.spring.dependency-management")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:testcontainers")
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.18.3"))
+    testImplementation("org.testcontainers:testcontainers:1.18.3")
+
+
 }
 
-compileKotlin {
+tasks.withType<KotlinCompile> {
     kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "1.8"
-        freeCompilerArgs = ["-Xjsr305=strict"]
     }
 }
