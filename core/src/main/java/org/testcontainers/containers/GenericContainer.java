9220816be660050281ed79a1712489fa06e08bc2
jeroen-vd-nl <136333578+jeroen-vd-nl@users.noreply.github.com>
2023-06-20 20:30:07 +0200

    Use `SelinuxContext.SHARED` by default for mounting (#7187)
    Co-authored-by: monosoul <Kloz.Klaud@gmail.com>
    Co-authored-by: Eddú Meléndez <eddu.melendez@gmail.com>

--- core/src/main/java/org/testcontainers/containers/GenericContainer.java
+++ core/src/main/java/org/testcontainers/containers/GenericContainer.java
@@ -1290,7 +1290,7 @@ public class GenericContainer<SELF extends GenericContainer<SELF>>
         final String containerPath,
         final BindMode mode
     ) {
-        return withClasspathResourceMapping(resourcePath, containerPath, mode, SelinuxContext.NONE);
+        return withClasspathResourceMapping(resourcePath, containerPath, mode, SelinuxContext.SHARED);
     }
 
     /**
@@ -1305,10 +1305,10 @@ public class GenericContainer<SELF extends GenericContainer<SELF>>
     ) {
         final MountableFile mountableFile = MountableFile.forClasspathResource(resourcePath);
 
-        if (mode == BindMode.READ_ONLY && selinuxContext == SelinuxContext.NONE) {
-            withCopyFileToContainer(mountableFile, containerPath);
-        } else {
+        if (mode == BindMode.READ_WRITE) {
             addFileSystemBind(mountableFile.getResolvedPath(), containerPath, mode, selinuxContext);
+        } else {
+            withCopyFileToContainer(mountableFile, containerPath);
         }
 
         return self();
