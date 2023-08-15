e0670057d173de1ff873650cd2767b759a61df1c
samed-bicer <mbicer14@ku.edu.tr>
2023-08-16 00:31:31 +0100

    Migrate spring-boot examples from junit4 to junit5

--- examples/spring-boot/src/test/java/com/example/DemoControllerTest.java
+++ examples/spring-boot/src/test/java/com/example/DemoControllerTest.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
