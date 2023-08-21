d9519f998bc81d6e3f11b459aab9bfe8776f42f7
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-08-21 10:31:09 -0600

    Add interface to customize CreateContainerCmd (#7421)
    Add `CreateContainerCmdModifier` interface to customize
    `CreateContainerCmd`. Customization is applied after Testcontainers
    configuration is set.

---
+++ core/src/test/java/org/testcontainers/custom/TestCreateContainerCmdModifier.java
@@ -0,0 +1,18 @@
+package org.testcontainers.custom;
+
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import org.testcontainers.core.CreateContainerCmdModifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestCreateContainerCmdModifier implements CreateContainerCmdModifier {
+
+    @Override
+    public CreateContainerCmd modify(CreateContainerCmd createContainerCmd) {
+        Map<String, String> labels = new HashMap<>();
+        labels.put("project", "testcontainers-java");
+        labels.put("scope", "global");
+        return createContainerCmd.withLabels(labels);
+    }
+}
