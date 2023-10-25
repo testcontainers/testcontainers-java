266b08fda357025c3e8cd78af027cfa753fb8860
Eddú Meléndez <eddu.melendez@gmail.com>
2023-10-25 12:19:23 -0600

    Fix checkstyle

--- core/src/main/java/org/testcontainers/containers/ContainerDef.java
+++ core/src/main/java/org/testcontainers/containers/ContainerDef.java
@@ -17,7 +17,14 @@ import org.testcontainers.utility.Base58;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.TestcontainersConfiguration;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Getter
 @UnstableAPI
