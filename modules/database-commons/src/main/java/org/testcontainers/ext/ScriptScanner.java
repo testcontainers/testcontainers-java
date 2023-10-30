9cfa1662d9e9fc3dae619a5250f8c8bb78d67668
Ivan Ponomarev <iponomarev@mail.ru>
2023-10-30 22:47:08 +0200

    Enhance ScriptUtils: fix parser (#7646)
    Co-authored-by: Eddú Meléndez <eddu.melendez@gmail.com>
    Co-authored-by: Kevin Wittek <kiview@users.noreply.github.com>

---
+++ modules/database-commons/src/main/java/org/testcontainers/ext/ScriptScanner.java
@@ -0,0 +1,151 @@
+package org.testcontainers.ext;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Rough lexical parser for SQL scripts.
+ */
+@RequiredArgsConstructor
+class ScriptScanner {
+
+    private final String resource;
+
+    private final String script;
+
+    private final String separator;
+
+    private final String commentPrefix;
+
+    private final String blockCommentStartDelimiter;
+
+    private final String blockCommentEndDelimiter;
+
+    private final Pattern eol = Pattern.compile("[\n\r]+");
+
+    private final Pattern whitespace = Pattern.compile("\\s+");
+
+    private final Pattern identifier = Pattern.compile("[a-z][a-z0-9_]*", Pattern.CASE_INSENSITIVE);
+
+    private final Pattern singleQuotedString = Pattern.compile("'(\\\\'|[^'])*'");
+
+    private final Pattern ansiQuotedString = Pattern.compile("\"(\\\\\"|[^\"])*\"");
+
+    private final Pattern dollarQuotedStringDelimiter = Pattern.compile("\\$\\w*\\$");
+
+    private int offset;
+
+    @Getter
+    private String currentMatch;
+
+    private boolean matches(String substring) {
+        if (script.startsWith(substring, offset)) {
+            currentMatch = substring;
+            offset += currentMatch.length();
+            return true;
+        } else {
+            currentMatch = "";
+            return false;
+        }
+    }
+
+    private boolean matches(Pattern regexp) {
+        Matcher m = regexp.matcher(script);
+        if (m.find(offset) && m.start() == offset) {
+            currentMatch = m.group();
+            offset = m.end();
+            return true;
+        } else {
+            currentMatch = "";
+            return false;
+        }
+    }
+
+    private boolean matchesSingleLineComment() {
+        /* Matches from commentPrefix to the EOL or end of script */
+        if (matches(commentPrefix)) {
+            Matcher m = eol.matcher(script);
+            if (m.find(offset)) {
+                currentMatch = commentPrefix + script.substring(offset, m.end());
+                offset = m.end();
+            } else {
+                currentMatch = commentPrefix + script.substring(offset);
+                offset = script.length();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean matchesMultilineComment() {
+        /* Matches from blockCommentStartDelimiter to the next blockCommentEndDelimiter.
+         * Error, if blockCommentEndDelimiter is not found. */
+        if (matches(blockCommentStartDelimiter)) {
+            int end = script.indexOf(blockCommentEndDelimiter, offset);
+            if (end < 0) {
+                throw new ScriptUtils.ScriptParseException(
+                    String.format("Missing block comment end delimiter [%s].", blockCommentEndDelimiter),
+                    resource
+                );
+            }
+            end += blockCommentEndDelimiter.length();
+            currentMatch = blockCommentStartDelimiter + script.substring(offset, end);
+            offset = end;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean matchesDollarQuotedString() {
+        //Matches $<tag>$ .... $<tag>$
+        if (matches(dollarQuotedStringDelimiter)) {
+            String delimiter = currentMatch;
+            int end = script.indexOf(delimiter, offset);
+            if (end < 0) {
+                throw new ScriptUtils.ScriptParseException(
+                    String.format("Unclosed dollar quoted string [%s].", delimiter),
+                    resource
+                );
+            }
+            end += delimiter.length();
+            currentMatch = delimiter + script.substring(offset, end);
+            offset = end;
+            return true;
+        }
+        return false;
+    }
+
+    Lexem next() {
+        if (offset < script.length()) {
+            if (matches(separator)) {
+                return Lexem.SEPARATOR;
+            } else if (matchesSingleLineComment() || matchesMultilineComment()) {
+                return Lexem.COMMENT;
+            } else if (matches(singleQuotedString) || matches(ansiQuotedString) || matchesDollarQuotedString()) {
+                return Lexem.QUOTED_STRING;
+            } else if (matches(identifier)) {
+                return Lexem.IDENTIFIER;
+            } else if (matches(whitespace)) {
+                return Lexem.WHITESPACE;
+            } else {
+                currentMatch = String.valueOf(script.charAt(offset++));
+                return Lexem.OTHER;
+            }
+        } else {
+            return Lexem.EOF;
+        }
+    }
+
+    enum Lexem {
+        SEPARATOR,
+        COMMENT,
+        QUOTED_STRING,
+        WHITESPACE,
+        IDENTIFIER,
+        OTHER,
+        EOF,
+    }
+}
