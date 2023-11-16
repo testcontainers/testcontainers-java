ce4fd43e1efcaacb0ca19877351ff08a77b07f55
Eddú Meléndez <eddu.melendez@gmail.com>
2023-11-15 20:39:35 -0600

    Add DelombokArgumentProvider.groovy

---
+++ commons-build/src/main/groovy/org/testcontainers/build/DelombokArgumentProvider.groovy
@@ -0,0 +1,31 @@
+package org.testcontainers.build
+
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.process.CommandLineArgumentProvider
+
+/**
+ * Allows build cache relocatability for Delombok task
+ */
+class DelombokArgumentProvider implements CommandLineArgumentProvider {
+
+	@InputFiles
+	@PathSensitive(PathSensitivity.RELATIVE)
+	Set<File> srcDirs
+
+	@OutputDirectory
+	File outputDir
+
+	@Override
+	Iterable<String> asArguments() {
+		return [
+			srcDirs.collect { it.absolutePath }.join(" "),
+			"-d",
+			outputDir.absolutePath,
+			"-f",
+			"generateDelombokComment:skip"
+		]
+	}
+}
