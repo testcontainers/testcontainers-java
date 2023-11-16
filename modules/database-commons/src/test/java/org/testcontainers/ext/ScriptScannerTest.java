testcontainers/testcontainers-java

commit 9cfa1662d9e9fc3dae619a5250f8c8bb78d67668

Enhance ScriptUtils: fix parser (#7646)
Co-authored-by: Eddú Meléndez <eddu.melendez@gmail.com>
Co-authored-by: Kevin Wittek <kiview@users.noreply.github.com>

modules/database-commons/src/main/java/org/testcontainers/ext/ScriptScanner.java
@@ -1,0 +1,151 @@
+package org.testcontainers.ext;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Rough lexical parser for SQL scripts.
+ */
+@RequiredArgsConstructor
+class ScriptScanner {
+
+    private final String resource;
+
+    private final String script;
+
+    private final String separator;
+
+    private final String commentPrefix;
+
+    private final String blockCommentStartDelimiter;
+
+    private final String blockCommentEndDelimiter;
+
+    private final Pattern eol = Pattern.compile("[\n\r]+");
+
+    private final Pattern whitespace = Pattern.compile("\\s+");
+
+    private final Pattern identifier = Pattern.compile("[a-z][a-z0-9_]*", Pattern.CASE_INSENSITIVE);
+
+    private final Pattern singleQuotedString = Pattern.compile("'(\\\\'|[^'])*'");
+
+    private final Pattern ansiQuotedString = Pattern.compile("\"(\\\\\"|[^\"])*\"");
+
+    private final Pattern dollarQuotedStringDelimiter = Pattern.compile("\\$\\w*\\$");
+
+    private int offset;
+
+    @Getter
+    private String currentMatch;
+
+    private boolean matches(String substring) {
+        if (script.startsWith(substring, offset)) {
+            currentMatch = substring;
+            offset += currentMatch.length();
+            return true;
+        } else {
+            currentMatch = "";
+            return false;
+        }
+    }
+
+    private boolean matches(Pattern regexp) {
+        Matcher m = regexp.matcher(script);
+        if (m.find(offset) && m.start() == offset) {
+            currentMatch = m.group();
+            offset = m.end();
+            return true;
+        } else {
+            currentMatch = "";
+            return false;
+        }
+    }
+
+    private boolean matchesSingleLineComment() {
+        /* Matches from commentPrefix to the EOL or end of script */
+        if (matches(commentPrefix)) {
+            Matcher m = eol.matcher(script);
+            if (m.find(offset)) {
+                currentMatch = commentPrefix + script.substring(offset, m.end());
+                offset = m.end();
+            } else {
+                currentMatch = commentPrefix + script.substring(offset);
+                offset = script.length();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean matchesMultilineComment() {
+        /* Matches from blockCommentStartDelimiter to the next blockCommentEndDelimiter.
+         * Error, if blockCommentEndDelimiter is not found. */
+        if (matches(blockCommentStartDelimiter)) {
+            int end = script.indexOf(blockCommentEndDelimiter, offset);
+            if (end < 0) {
+                throw new ScriptUtils.ScriptParseException(
+                    String.format("Missing block comment end delimiter [%s].", blockCommentEndDelimiter),
+                    resource
+                );
+            }
+            end += blockCommentEndDelimiter.length();
+            currentMatch = blockCommentStartDelimiter + script.substring(offset, end);
+            offset = end;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean matchesDollarQuotedString() {
+        //Matches $<tag>$ .... $<tag>$
+        if (matches(dollarQuotedStringDelimiter)) {
+            String delimiter = currentMatch;
+            int end = script.indexOf(delimiter, offset);
+            if (end < 0) {
+                throw new ScriptUtils.ScriptParseException(
+                    String.format("Unclosed dollar quoted string [%s].", delimiter),
+                    resource
+                );
+            }
+            end += delimiter.length();
+            currentMatch = delimiter + script.substring(offset, end);
+            offset = end;
+            return true;
+        }
+        return false;
+    }
+
+    Lexem next() {
+        if (offset < script.length()) {
+            if (matches(separator)) {
+                return Lexem.SEPARATOR;
+            } else if (matchesSingleLineComment() || matchesMultilineComment()) {
+                return Lexem.COMMENT;
+            } else if (matches(singleQuotedString) || matches(ansiQuotedString) || matchesDollarQuotedString()) {
+                return Lexem.QUOTED_STRING;
+            } else if (matches(identifier)) {
+                return Lexem.IDENTIFIER;
+            } else if (matches(whitespace)) {
+                return Lexem.WHITESPACE;
+            } else {
+                currentMatch = String.valueOf(script.charAt(offset++));
+                return Lexem.OTHER;
+            }
+        } else {
+            return Lexem.EOF;
+        }
+    }
+
+    enum Lexem {
+        SEPARATOR,
+        COMMENT,
+        QUOTED_STRING,
+        WHITESPACE,
+        IDENTIFIER,
+        OTHER,
+        EOF,
+    }
+}

modules/database-commons/src/main/java/org/testcontainers/ext/ScriptSplitter.java
@@ -1,0 +1,127 @@
+package org.testcontainers.ext;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.testcontainers.ext.ScriptScanner.Lexem;
+
+import java.util.List;
+
+/**
+ * Performs splitting of an SQL script into statements including
+ * basic clean-up.
+ */
+@RequiredArgsConstructor
+class ScriptSplitter {
+
+    private final ScriptScanner scanner;
+
+    private final List<String> statements;
+
+    private final StringBuilder sb = new StringBuilder();
+
+    /**
+     * Standard parsing:
+     * 1. Remove comments
+     * 2. Shrink whitespace and eols
+     * 3. Split on separator
+     */
+    void split() {
+        Lexem l;
+        while ((l = scanner.next()) != Lexem.EOF) {
+            switch (l) {
+                case SEPARATOR:
+                    flushStringBuilder();
+                    break;
+                case COMMENT:
+                    //skip
+                    break;
+                case WHITESPACE:
+                    if (!sb.toString().endsWith(" ")) {
+                        sb.append(' ');
+                    }
+                    break;
+                case IDENTIFIER:
+                    appendMatch();
+                    if ("begin".equalsIgnoreCase(scanner.getCurrentMatch())) {
+                        compoundStatement(false);
+                        flushStringBuilder();
+                    }
+                    break;
+                default:
+                    appendMatch();
+            }
+        }
+        flushStringBuilder();
+    }
+
+    /**
+     * Compound statement ('create procedure') mode:
+     * 1. Do not remove comments
+     * 2. Do not shrink whitespace
+     * 3. Do not split on separators
+     * 3. This mode can be recursive
+     */
+    private void compoundStatement(boolean recursive) {
+        Lexem l;
+        while ((l = scanner.next()) != Lexem.EOF) {
+            appendMatch();
+            if (Lexem.IDENTIFIER.equals(l)) {
+                if ("begin".equalsIgnoreCase(scanner.getCurrentMatch())) {
+                    compoundStatement(true);
+                } else if ("end".equalsIgnoreCase(scanner.getCurrentMatch())) {
+                    if (endOfBlock(recursive)) {
+                        return;
+                    }
+                }
+            }
+        }
+        flushStringBuilder();
+    }
+
+    private boolean endOfBlock(boolean recursive) {
+        Lexem l;
+        StringBuilder temporary = new StringBuilder();
+        while ((l = scanner.next()) != Lexem.EOF) {
+            switch (l) {
+                case COMMENT:
+                case WHITESPACE:
+                    temporary.append(scanner.getCurrentMatch());
+                    break;
+                case SEPARATOR:
+                    //Only whitespace and comments preceded the separator: true end of block
+                    //If it's an internal block, append everything
+                    if (recursive) {
+                        sb.append(temporary);
+                        appendMatch();
+                    }
+                    return true;
+                default:
+                    // Semicolon is not recognized as separator: this means that a custom
+                    // separator is used. Still, 'END;' should be a valid end of block
+                    if (";".equals(scanner.getCurrentMatch())) {
+                        if (recursive) {
+                            sb.append(temporary);
+                            appendMatch();
+                        }
+                        return true;
+                    }
+                    sb.append(temporary);
+                    appendMatch();
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private void appendMatch() {
+        sb.append(scanner.getCurrentMatch());
+    }
+
+    private void flushStringBuilder() {
+        final String s = sb.toString().trim();
+        if (StringUtils.isNotEmpty(s)) {
+            statements.add(s);
+        }
+        sb.setLength(0);
+    }
+}

modules/database-commons/src/main/java/org/testcontainers/ext/ScriptUtils.java
@@ -119,191 +119,18 @@ public abstract class ScriptUtils {
             "blockCommentEndDelimiter must not be null or empty"
         );
 
-        StringBuilder sb = new StringBuilder();
-        boolean inEscape = false;
-        boolean inLineComment = false;
-        boolean inBlockComment = false;
-        Character currentLiteralDelimiter = null;
-
-        int compoundStatementDepth = 0;
-        final String lowerCaseScriptContent = script.toLowerCase();
-        char[] content = script.toCharArray();
-        for (int i = 0; i < script.length(); i++) {
-            char c = content[i];
-            if (inEscape) {
-                inEscape = false;
-                sb.append(c);
-                continue;
-            }
-            // MySQL style escapes
-            if (c == '\\') {
-                inEscape = true;
-                sb.append(c);
-                continue;
-            }
-            // Determine whether we're entering/leaving a string literal
-            if (!inBlockComment && !inLineComment && (c == '\'' || c == '"' || c == '`')) {
-                if (currentLiteralDelimiter == null) { // ignore delimiters within an existing string literal
-                    currentLiteralDelimiter = c;
-                } else if (currentLiteralDelimiter == c) { // find end of string literal
-                    currentLiteralDelimiter = null;
-                }
-            }
-            final boolean inLiteral = currentLiteralDelimiter != null;
-
-            if (!inLiteral && containsSubstringAtOffset(lowerCaseScriptContent, commentPrefix, i)) {
-                inLineComment = true;
-            }
-            if (inLineComment && c == '\n') {
-                inLineComment = false;
-            }
-            if (!inLiteral && containsSubstringAtOffset(lowerCaseScriptContent, blockCommentStartDelimiter, i)) {
-                inBlockComment = true;
-            }
-            if (
-                !inLiteral &&
-                inBlockComment &&
-                containsSubstringAtOffset(lowerCaseScriptContent, blockCommentEndDelimiter, i)
-            ) {
-                inBlockComment = false;
-            }
-            final boolean inComment = inLineComment || inBlockComment;
-
-            if (
-                !inLiteral &&
-                !inComment &&
-                containsKeywordsAtOffset(
-                    lowerCaseScriptContent,
-                    "BEGIN",
-                    i,
-                    separator,
-                    commentPrefix,
-                    blockCommentStartDelimiter
-                )
-            ) {
-                compoundStatementDepth++;
-            }
-            if (
-                !inLiteral &&
-                !inComment &&
-                containsKeywordsAtOffset(
-                    lowerCaseScriptContent,
-                    "END",
-                    i,
-                    separator,
-                    commentPrefix,
-                    blockCommentStartDelimiter
-                )
-            ) {
-                compoundStatementDepth--;
-            }
-            final boolean inCompoundStatement = compoundStatementDepth != 0;
-
-            if (!inLiteral && !inCompoundStatement) {
-                if (script.startsWith(separator, i)) {
-                    // we've reached the end of the current statement
-                    sb = flushStringBuilder(sb, statements);
-                    i += separator.length() - 1;
-                    continue;
-                } else if (script.startsWith(commentPrefix, i)) {
-                    // skip over any content from the start of the comment to the EOL
-                    int indexOfNextNewline = script.indexOf("\n", i);
-                    if (indexOfNextNewline > i) {
-                        i = indexOfNextNewline;
-                        continue;
-                    } else {
-                        // if there's no EOL, we must be at the end
-                        // of the script, so stop here.
-                        break;
-                    }
-                } else if (script.startsWith(blockCommentStartDelimiter, i)) {
-                    // skip over any block comments
-                    int indexOfCommentEnd = script.indexOf(blockCommentEndDelimiter, i);
-                    if (indexOfCommentEnd > i) {
-                        i = indexOfCommentEnd + blockCommentEndDelimiter.length() - 1;
-                        inBlockComment = false;
-                        continue;
-                    } else {
-                        throw new ScriptParseException(
-                            String.format("Missing block comment end delimiter [%s].", blockCommentEndDelimiter),
-                            resource
-                        );
-                    }
-                } else if (c == ' ' || c == '\n' || c == '\t' || c == '\r') {
-                    // avoid multiple adjacent whitespace characters
-                    if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
-                        c = ' ';
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            sb.append(c);
-        }
-        flushStringBuilder(sb, statements);
-    }
-
-    private static StringBuilder flushStringBuilder(StringBuilder sb, List<String> statements) {
-        if (sb.length() == 0) {
-            return sb;
-        }
-
-        final String s = sb.toString().trim();
-        if (StringUtils.isNotEmpty(s)) {
-            statements.add(s);
-        }
-
-        return new StringBuilder();
-    }
-
-    private static boolean isSeperator(
-        char c,
-        String separator,
-        String commentPrefix,
-        String blockCommentStartDelimiter
-    ) {
-        return (
-            c == ' ' ||
-            c == '\r' ||
-            c == '\n' ||
-            c == '\t' ||
-            c == separator.charAt(0) ||
-            c == separator.charAt(separator.length() - 1) ||
-            c == commentPrefix.charAt(0) ||
-            c == blockCommentStartDelimiter.charAt(0) ||
-            c == blockCommentStartDelimiter.charAt(blockCommentStartDelimiter.length() - 1)
-        );
-    }
-
-    private static boolean containsSubstringAtOffset(String lowercaseString, String substring, int offset) {
-        String lowercaseSubstring = substring.toLowerCase();
-
-        return lowercaseString.startsWith(lowercaseSubstring, offset);
-    }
-
-    private static boolean containsKeywordsAtOffset(
-        String lowercaseString,
-        String keywords,
-        int offset,
-        String separator,
-        String commentPrefix,
-        String blockCommentStartDelimiter
-    ) {
-        String lowercaseKeywords = keywords.toLowerCase();
-
-        boolean backSeperated =
-            (offset == 0) ||
-            isSeperator(lowercaseString.charAt(offset - 1), separator, commentPrefix, blockCommentStartDelimiter);
-        boolean frontSeperated =
-            (offset >= (lowercaseString.length() - keywords.length())) ||
-            isSeperator(
-                lowercaseString.charAt(offset + keywords.length()),
+        new ScriptSplitter(
+            new ScriptScanner(
+                resource,
+                script,
                 separator,
                 commentPrefix,
-                blockCommentStartDelimiter
-            );
-
-        return backSeperated && frontSeperated && lowercaseString.startsWith(lowercaseKeywords, offset);
+                blockCommentStartDelimiter,
+                blockCommentEndDelimiter
+            ),
+            statements
+        )
+            .split();
     }
 
     private static void checkArgument(boolean expression, String errorMessage) {
@@ -318,13 +145,45 @@ public abstract class ScriptUtils {
      * @param delim String delimiting each statement - typically a ';' character
      */
     public static boolean containsSqlScriptDelimiters(String script, String delim) {
-        boolean inLiteral = false;
-        char[] content = script.toCharArray();
-        for (int i = 0; i < script.length(); i++) {
-            if (content[i] == '\'') {
-                inLiteral = !inLiteral;
-            }
-            if (!inLiteral && script.startsWith(delim, i)) {
+        return containsSqlScriptDelimiters(
+            "",
+            script,
+            DEFAULT_COMMENT_PREFIX,
+            delim,
+            DEFAULT_BLOCK_COMMENT_START_DELIMITER,
+            DEFAULT_BLOCK_COMMENT_END_DELIMITER
+        );
+    }
+
+    /**
+     * Does the provided SQL script contain the specified delimiter?
+     *
+     * @param script                     the SQL script
+     * @param delim                      String delimiting each statement - typically a ';' character
+     * @param commentPrefix              the prefix that identifies comments in the SQL script,
+     *                                   typically "--"
+     * @param blockCommentStartDelimiter block comment start delimiter
+     * @param blockCommentEndDelimiter   block comment end delimiter
+     */
+    public static boolean containsSqlScriptDelimiters(
+        String scriptPath,
+        String script,
+        String commentPrefix,
+        String delim,
+        String blockCommentStartDelimiter,
+        String blockCommentEndDelimiter
+    ) {
+        ScriptScanner scanner = new ScriptScanner(
+            scriptPath,
+            script,
+            delim,
+            commentPrefix,
+            blockCommentStartDelimiter,
+            blockCommentEndDelimiter
+        );
+        ScriptScanner.Lexem l;
+        while ((l = scanner.next()) != ScriptScanner.Lexem.EOF) {
+            if (ScriptScanner.Lexem.SEPARATOR.equals(l)) {
                 return true;
             }
         }
@@ -420,7 +279,16 @@ public abstract class ScriptUtils {
             if (separator == null) {
                 separator = DEFAULT_STATEMENT_SEPARATOR;
             }
-            if (!containsSqlScriptDelimiters(script, separator)) {
+            if (
+                !containsSqlScriptDelimiters(
+                    scriptPath,
+                    script,
+                    commentPrefix,
+                    separator,
+                    blockCommentStartDelimiter,
+                    blockCommentEndDelimiter
+                )
+            ) {
                 separator = FALLBACK_STATEMENT_SEPARATOR;
             }
 

commit 46f03d2ab3d32d96df33776c048de9c94f92962e

fix bugs introduced by #7646

modules/database-commons/src/main/java/org/testcontainers/ext/ScriptScanner.java
@@ -28,11 +28,7 @@ class ScriptScanner {
 
     private final Pattern whitespace = Pattern.compile("\\s+");
 
-    private final Pattern identifier = Pattern.compile("[a-z][a-z0-9_]*", Pattern.CASE_INSENSITIVE);
-
-    private final Pattern singleQuotedString = Pattern.compile("'(\\\\'|[^'])*'");
-
-    private final Pattern ansiQuotedString = Pattern.compile("\"(\\\\\"|[^\"])*\"");
+    private final Pattern identifier = Pattern.compile("[a-z][a-z0-9_$]*", Pattern.CASE_INSENSITIVE);
 
     private final Pattern dollarQuotedStringDelimiter = Pattern.compile("\\$\\w*\\$");
 
@@ -54,7 +50,8 @@ class ScriptScanner {
 
     private boolean matches(Pattern regexp) {
         Matcher m = regexp.matcher(script);
-        if (m.find(offset) && m.start() == offset) {
+        m.region(offset, script.length());
+        if (m.lookingAt()) {
             currentMatch = m.group();
             offset = m.end();
             return true;
@@ -99,6 +96,27 @@ class ScriptScanner {
         return false;
     }
 
+
+    private boolean matchesQuotedString(final char quote) {
+        if (script.charAt(offset) == quote) {
+            boolean escaped = false;
+            for (int i = offset + 1; i < script.length(); i++) {
+                char c = script.charAt(i);
+                if (escaped) {
+                    //just skip the escaped character and drop the flag
+                    escaped = false;
+                } else if (c == '\\') {
+                    escaped = true;
+                } else if (c == quote) {
+                    currentMatch = script.substring(offset, i + 1);
+                    offset = i + 1;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     private boolean matchesDollarQuotedString() {
         //Matches $<tag>$ .... $<tag>$
         if (matches(dollarQuotedStringDelimiter)) {
@@ -124,7 +142,7 @@ class ScriptScanner {
                 return Lexem.SEPARATOR;
             } else if (matchesSingleLineComment() || matchesMultilineComment()) {
                 return Lexem.COMMENT;
-            } else if (matches(singleQuotedString) || matches(ansiQuotedString) || matchesDollarQuotedString()) {
+            } else if (matchesQuotedString('\'') || matchesQuotedString('"') || matchesDollarQuotedString()) {
                 return Lexem.QUOTED_STRING;
             } else if (matches(identifier)) {
                 return Lexem.IDENTIFIER;

modules/database-commons/src/main/java/org/testcontainers/ext/Test.java
@@ -1,0 +1,25 @@
+package org.testcontainers.ext;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        Path p = Paths.get("C:\\ideaspace\\synthesized\\testing-suite\\engine\\executor\\src\\e2e\\resources\\testspecs\\mysql\\prepare_test_data.sql");
+
+        String s = Files.lines(p).collect(Collectors.joining("\n"));
+        ArrayList<String> l = new ArrayList<>();
+        ScriptUtils.splitSqlScript("a", s, ";",
+            ScriptUtils.DEFAULT_COMMENT_PREFIX, ScriptUtils.DEFAULT_BLOCK_COMMENT_START_DELIMITER,
+            ScriptUtils.DEFAULT_BLOCK_COMMENT_END_DELIMITER, l);
+        l.forEach(line -> {
+            System.out.println(line);
+            System.out.println("----");
+        });
+
+    }
+}

modules/database-commons/src/test/java/org/testcontainers/ext/ScriptScannerTest.java
@@ -1,0 +1,49 @@
+package org.testcontainers.ext;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScriptScannerTest {
+    @Test
+    public void testHugeStringLiteral() {
+        String script = "/* a comment */    \"" +  StringUtils.repeat('~', 10000) + "\";";
+        ScriptScanner scanner = scanner(script);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.COMMENT);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.WHITESPACE);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.QUOTED_STRING);
+        assertThat(scanner.getCurrentMatch()).matches(Pattern.compile("\"~+\""));
+    }
+
+    @Test
+    public void testPgIdentifierWithDollarSigns() {
+        ScriptScanner scanner = scanner("this$is$a$valid$postgreSQL$identifier  " +
+            "$a$While this is a quoted string$a$$ --just followed by a dollar sign");
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.IDENTIFIER);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.WHITESPACE);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.QUOTED_STRING);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.OTHER);
+    }
+
+    @Test
+    public void testQuotedLiterals(){
+        ScriptScanner scanner = scanner("'this \\'is a literal' \"this \\\" is a literal\"");
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.QUOTED_STRING);
+        assertThat(scanner.getCurrentMatch()).isEqualTo("'this \\'is a literal'");
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.WHITESPACE);
+        assertThat(scanner.next()).isEqualTo(ScriptScanner.Lexem.QUOTED_STRING);
+        assertThat(scanner.getCurrentMatch()).isEqualTo("\"this \\\" is a literal\"");
+    }
+
+    private static ScriptScanner scanner(String script) {
+        return new ScriptScanner("dummy",
+            script,
+            ScriptUtils.DEFAULT_STATEMENT_SEPARATOR,
+            ScriptUtils.DEFAULT_COMMENT_PREFIX,
+            ScriptUtils.DEFAULT_BLOCK_COMMENT_START_DELIMITER,
+            ScriptUtils.DEFAULT_BLOCK_COMMENT_END_DELIMITER);
+    }
+}