9de26f11b0904c376ae367db8c715ca2d9d12e9d
Chad Wilson <chadw@thoughtworks.com>
2024-01-24 06:48:12 +0800

    Fix default configuration file in mysql (#8131)
    * Remove use of deprecated (removed in 8.3) MySQL config option
    
    * Update MySQL default images to latest
    
    - 5.6 went EOL in Feb 2021. Stop testing against it.
    - 5.7 went EOL in Oct 2023. Retain tests for a bit, but stop using it as the default.
    - 8.0.34+ is now LTS rather than rolling - use this as the default for most tests
    - Add 8.3.0 as a candidate "innovation" version which will eventually become LTS
    
    * Updates various docs to use 8.0 in the examples rather than EOL 5.7.
    
    * Update MariaDB images to latest 10.3 patch release
    
    Fixes #8130
    
    ---------
    
    Co-authored-by: Eddú Meléndez <eddu.melendez@gmail.com>

--- modules/mysql/src/test/java/org/testcontainers/jdbc/mysql/MySQLJDBCDriverTest.java
+++ modules/mysql/src/test/java/org/testcontainers/jdbc/mysql/MySQLJDBCDriverTest.java
@@ -20,29 +20,29 @@ public class MySQLJDBCDriverTest extends AbstractJDBCDriverTest {
                     EnumSet.of(Options.ScriptedSchema, Options.JDBCParams),
                 },
                 {
-                    "jdbc:tc:mysql:5.7.34://hostname/databasename?user=someuser&TC_INITFUNCTION=org.testcontainers.jdbc.AbstractJDBCDriverTest::sampleInitFunction",
+                    "jdbc:tc:mysql:8.0.36://hostname/databasename?user=someuser&TC_INITFUNCTION=org.testcontainers.jdbc.AbstractJDBCDriverTest::sampleInitFunction",
                     EnumSet.of(Options.ScriptedSchema, Options.JDBCParams),
                 },
                 {
-                    "jdbc:tc:mysql:5.7.34://hostname/databasename?user=someuser&password=somepwd&TC_INITSCRIPT=somepath/init_mysql.sql",
+                    "jdbc:tc:mysql:8.0.36://hostname/databasename?user=someuser&password=somepwd&TC_INITSCRIPT=somepath/init_mysql.sql",
                     EnumSet.of(Options.ScriptedSchema, Options.JDBCParams),
                 },
                 {
-                    "jdbc:tc:mysql:5.7.34://hostname/databasename?user=someuser&password=somepwd&TC_INITSCRIPT=file:sql/init_mysql.sql",
+                    "jdbc:tc:mysql:8.0.36://hostname/databasename?user=someuser&password=somepwd&TC_INITSCRIPT=file:sql/init_mysql.sql",
                     EnumSet.of(Options.ScriptedSchema, Options.JDBCParams),
                 },
                 {
-                    "jdbc:tc:mysql:5.7.34://hostname/databasename?user=someuser&password=somepwd&TC_INITFUNCTION=org.testcontainers.jdbc.AbstractJDBCDriverTest::sampleInitFunction",
+                    "jdbc:tc:mysql:8.0.36://hostname/databasename?user=someuser&password=somepwd&TC_INITFUNCTION=org.testcontainers.jdbc.AbstractJDBCDriverTest::sampleInitFunction",
                     EnumSet.of(Options.ScriptedSchema, Options.JDBCParams),
                 },
                 {
-                    "jdbc:tc:mysql:5.7.34://hostname/databasename?TC_INITSCRIPT=somepath/init_unicode_mysql.sql&useUnicode=yes&characterEncoding=utf8",
+                    "jdbc:tc:mysql:8.0.36://hostname/databasename?TC_INITSCRIPT=somepath/init_unicode_mysql.sql&useUnicode=yes&characterEncoding=utf8",
                     EnumSet.of(Options.CharacterSet),
                 },
-                { "jdbc:tc:mysql:5.7.34://hostname/databasename", EnumSet.noneOf(Options.class) },
-                { "jdbc:tc:mysql:5.7.34://hostname/databasename?useSSL=false", EnumSet.noneOf(Options.class) },
+                { "jdbc:tc:mysql:8.0.36://hostname/databasename", EnumSet.noneOf(Options.class) },
+                { "jdbc:tc:mysql:8.0.36://hostname/databasename?useSSL=false", EnumSet.noneOf(Options.class) },
                 {
-                    "jdbc:tc:mysql:5.6.51://hostname/databasename?TC_MY_CNF=somepath/mysql_conf_override",
+                    "jdbc:tc:mysql:8.0.36://hostname/databasename?TC_MY_CNF=somepath/mysql_conf_override",
                     EnumSet.of(Options.CustomIniFile),
                 },
             }
