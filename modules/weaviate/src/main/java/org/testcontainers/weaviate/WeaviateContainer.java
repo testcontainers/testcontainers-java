7264c69d034b51c40756d89493c8fd20f62938d6
Eddú Meléndez <eddu.melendez@gmail.com>
2024-02-19 17:26:30 -0600

    Add javadoc

--- modules/weaviate/src/main/java/org/testcontainers/weaviate/WeaviateContainer.java
+++ modules/weaviate/src/main/java/org/testcontainers/weaviate/WeaviateContainer.java
@@ -3,6 +3,17 @@ package org.testcontainers.weaviate;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
+/**
+ * Testcontainers implementation of Weaviate.
+ * <p>
+ * Supported image: {@code semitechnologies/weaviate}
+ * <p>
+ * Exposed ports:
+ * <ul>
+ *     <li>HTTP: 8080</li>
+ *     <li>gRPC: 50051</li>
+ * </ul>
+ */
 public class WeaviateContainer extends GenericContainer<WeaviateContainer> {
 
     private static final String WEAVIATE_IMAGE = "semitechnologies/weaviate";
@@ -14,7 +25,7 @@ public class WeaviateContainer extends GenericContainer<WeaviateContainer> {
     public WeaviateContainer(DockerImageName dockerImageName) {
         super(dockerImageName);
         dockerImageName.assertCompatibleWith(DockerImageName.parse(WEAVIATE_IMAGE));
-        withExposedPorts(8080);
+        withExposedPorts(8080, 50051);
         withEnv("AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED", "true");
         withEnv("PERSISTENCE_DATA_PATH", "/var/lib/weaviate");
     }
