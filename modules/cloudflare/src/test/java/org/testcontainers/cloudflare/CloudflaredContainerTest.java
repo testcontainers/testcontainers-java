4af797aef5bb43042c8276ff3dc07bfa5137bd48
shelajev <shelajev@gmail.com>
2024-02-20 17:10:20 +0200

    Add CloudflaredContainer module for public tunnels

---
+++ modules/cloudflare/src/test/java/org/testcontainers/cloudflare/CloudflaredContainerTest.java
@@ -0,0 +1,64 @@
+package org.testcontainers.cloudflare;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CloudflaredContainerTest {
+
+    @Test
+    public void shouldStartAndTunnelToHelloWorld() throws IOException {
+        try (GenericContainer<?> helloworld = new GenericContainer<>(
+            DockerImageName.parse("testcontainers/helloworld:1.1.0")
+        )
+            .withNetworkAliases("helloworld")
+            .withExposedPorts(8080, 8081)
+            .waitingFor(new HttpWaitStrategy()) ) {
+
+            helloworld.start();
+
+            try (CloudflaredContainer cloudflare = new CloudflaredContainer(DockerImageName.parse("cloudflare/cloudflared:latest"), helloworld.getFirstMappedPort());) {
+                cloudflare.start();
+                String url = cloudflare.getPublicUrl();
+
+                assertThat(url)
+                    .as("Public url contains 'cloudflare'")
+                    .contains("cloudflare");
+                String body = readUrl(url);
+
+                assertThat(body.trim())
+                    .as("the index page contains the title 'Hello world'")
+                    .contains("Hello world");
+            }
+
+        }
+
+    }
+
+    private String readUrl(String url) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+
+        StringBuilder sb = new StringBuilder();
+        String inputLine = null;
+        while ((inputLine = in.readLine()) != null) {
+            sb.append(inputLine);
+        }
+        in.close();
+
+        return sb.toString();
+    }
+}
