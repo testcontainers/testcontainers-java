625dfec5bedb86537e9116df2250bbc9de617c19
Eddú Meléndez <eddu.melendez@gmail.com>
2024-02-29 19:52:06 -0600

    Add createImage

--- modules/ollama/src/main/java/org/testcontainers/ollama/OllamaContainer.java
+++ modules/ollama/src/main/java/org/testcontainers/ollama/OllamaContainer.java
@@ -1,14 +1,25 @@
 package org.testcontainers.ollama;
 
+import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.DeviceRequest;
+import com.github.dockerjava.api.model.Image;
 import com.github.dockerjava.api.model.Info;
 import com.github.dockerjava.api.model.RuntimeInfo;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Testcontainers implementation for Ollama.
+ * <p>
+ * Supported image: {@code ollama/ollama}
+ * <p>
+ * Exposed ports: 11434
+ */
 public class OllamaContainer extends GenericContainer<OllamaContainer> {
 
     private static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("ollama/ollama");
@@ -29,14 +40,37 @@ public class OllamaContainer extends GenericContainer<OllamaContainer> {
                     cmd
                         .getHostConfig()
                         .withDeviceRequests(
-                            Arrays.asList(
-                                new DeviceRequest().withCapabilities(Arrays.asList(Arrays.asList("gpu"))).withCount(-1)
+                            Collections.singletonList(
+                                new DeviceRequest()
+                                    .withCapabilities(Collections.singletonList(Collections.singletonList("gpu")))
+                                    .withCount(-1)
                             )
                         );
                 });
             }
         }
         withExposedPorts(11434);
+    }
+
+    /**
+     * Create a new image from the container
+     * @param imageName the name of the new image
+     */
+    public void createImage(String imageName) {
+        DockerImageName dockerImageName = DockerImageName.parse(getDockerImageName());
+        if (!dockerImageName.equals(DockerImageName.parse(imageName))) {
+            DockerClient dockerClient = DockerClientFactory.instance().client();
+            List<Image> images = dockerClient.listImagesCmd().withReferenceFilter(imageName).exec();
+            if (images.isEmpty()) {
+                DockerImageName imageModel = DockerImageName.parse(imageName);
+                dockerClient
+                    .commitCmd(getContainerId())
+                    .withRepository(imageModel.getUnversionedPart())
+                    .withLabels(Collections.singletonMap("org.testcontainers.sessionId", ""))
+                    .withTag(imageModel.getVersionPart())
+                    .exec();
+            }
+        }
     }
 
     public String getEndpoint() {
--- modules/ollama/src/test/java/org/testcontainers/ollama/OllamaContainerTest.java
+++ modules/ollama/src/test/java/org/testcontainers/ollama/OllamaContainerTest.java
@@ -1,6 +1,10 @@
 package org.testcontainers.ollama;
 
 import org.junit.Test;
+import org.testcontainers.utility.Base58;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,6 +25,36 @@ public class OllamaContainerTest {
                 .jsonPath()
                 .get("version");
             assertThat(version).isEqualTo("0.1.26");
+        }
+    }
+
+    @Test
+    public void downloadModelAndCreateImage() throws IOException, InterruptedException {
+        String newImageName = "tc-ollama-allminilm-" + Base58.randomString(4).toLowerCase();
+        try (OllamaContainer ollama = new OllamaContainer("ollama/ollama:0.1.26")) {
+            ollama.start();
+            ollama.execInContainer("ollama", "pull", "all-minilm");
+
+            String modelName = given()
+                .baseUri(ollama.getEndpoint())
+                .get("/api/tags")
+                .jsonPath()
+                .getString("models[0].name");
+            assertThat(modelName).contains("all-minilm");
+            ollama.createImage(newImageName);
+        }
+        try (
+            OllamaContainer ollama = new OllamaContainer(
+                DockerImageName.parse(newImageName).asCompatibleSubstituteFor("ollama/ollama")
+            )
+        ) {
+            ollama.start();
+            String modelName = given()
+                .baseUri(ollama.getEndpoint())
+                .get("/api/tags")
+                .jsonPath()
+                .getString("models[0].name");
+            assertThat(modelName).contains("all-minilm");
         }
     }
 }
