bb03938c586807d5d5e0166ee9ddc5e7bcd04c8a
Ignasi <ignasi.lopez.luna@gmail.com>
2024-06-11 17:00:09 +0200

    Added example of how to run Hugging Face models through Ollama using TC

---
+++ examples/ollama-hugging-face/src/test/java/com/example/ollamahf/OllamaHuggingFaceContainer.java
@@ -0,0 +1,86 @@
+package com.example.ollamahf;
+
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import org.testcontainers.containers.ContainerLaunchException;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+
+public class OllamaHuggingFaceContainer extends OllamaContainer {
+
+    private final String imageName;
+
+    private final HuggingFaceModel huggingFaceModel;
+
+    public OllamaHuggingFaceContainer(String imageName, HuggingFaceModel model) {
+        super(DockerImageName.parse("ollama/ollama:0.1.42"));
+        this.imageName = imageName;
+        this.huggingFaceModel = model;
+    }
+
+    @Override
+    protected void configure() {
+        super.configure();
+
+        if (huggingFaceModel != null) {
+            this.setImage(new ImageFromDockerfile()
+                .withDockerfileFromBuilder(builder -> {
+                    builder
+                        .from(this.getDockerImageName())
+                        .run("apt-get update && apt-get upgrade -y && apt-get install -y python3-pip")
+                        .run("pip install huggingface-hub")
+                        .build();
+                })
+            );
+        }
+    }
+
+    @Override
+    protected void containerIsStarted(InspectContainerResponse containerInfo) {
+        super.containerIsStarted(containerInfo);
+
+        if (huggingFaceModel == null) {
+            return;
+        }
+
+        try {
+            ExecResult downloadModelFromHF = execInContainer("huggingface-cli", "download",
+                huggingFaceModel.repository,
+                huggingFaceModel.model,
+                "--local-dir", ".");
+            if (downloadModelFromHF.getExitCode() > 0) {
+                throw new ContainerLaunchException("Failed to download model: " + downloadModelFromHF.getStderr());
+            }
+
+            ExecResult fillModelFile = execInContainer("sh", "-c", String.format("echo '%s' > Modelfile", huggingFaceModel.modelfileContent));
+            if (fillModelFile.getExitCode() > 0) {
+                throw new ContainerLaunchException("Failed to fill Modelfile: " + fillModelFile.getStderr());
+            }
+
+            ExecResult buildModel = execInContainer("ollama", "create", huggingFaceModel.model, "-f", "Modelfile");
+            if (buildModel.getExitCode() > 0) {
+                throw new ContainerLaunchException("Failed to build model: " + buildModel.getStderr());
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new ContainerLaunchException(e.getMessage());
+        }
+
+        commitToImage(imageName);
+    }
+
+    public static class HuggingFaceModel {
+        public final String repository;
+
+        public final String model;
+
+        public String modelfileContent;
+
+        public HuggingFaceModel(String repository, String model) {
+            this.repository = repository;
+            this.model = model;
+            this.modelfileContent = "FROM " + model;
+        }
+    }
+}
---
+++ examples/ollama-hugging-face/src/test/java/com/example/ollamahf/OllamaHuggingFaceTest.java
@@ -0,0 +1,61 @@
+package com.example.ollamahf;
+
+import io.restassured.http.Header;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.ContainerFetchException;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OllamaHuggingFaceTest {
+
+    @Test
+    public void embeddingModelWithHuggingFace() {
+        String repository = "CompendiumLabs/bge-small-en-v1.5-gguf";
+        String model = "bge-small-en-v1.5-q4_k_m.gguf";
+        String imageName = "embedding-model-from-hf";
+        try (OllamaContainer ollama = new OllamaContainer(DockerImageName.parse(imageName).asCompatibleSubstituteFor("ollama/ollama:0.1.42"))) {
+            try {
+                ollama.start();
+            } catch (ContainerFetchException ex) {
+                // Create the image
+                try(OllamaHuggingFaceContainer huggingFaceContainer = new OllamaHuggingFaceContainer(imageName,
+                    new OllamaHuggingFaceContainer.HuggingFaceModel(repository, model))) {
+                    huggingFaceContainer.start();
+                }
+            }
+
+            String modelName = given()
+                .baseUri(ollama.getEndpoint())
+                .get("/api/tags")
+                .jsonPath()
+                .getString("models[0].name");
+            assertThat(modelName).contains(model +":latest");
+
+            List<Float> embedding = given()
+                .baseUri(ollama.getEndpoint())
+                .header(new Header("Content-Type", "application/json"))
+                .body(new EmbeddingRequest(model + ":latest", "Hello from Testcontainers!"))
+                .post("/api/embeddings")
+                .jsonPath().getList("embedding");
+
+            assertThat(embedding).isNotNull();
+            assertThat(embedding.isEmpty()).isFalse();
+        }
+    }
+
+    public static class EmbeddingRequest {
+        public final String model;
+
+        public final String prompt;
+
+        public EmbeddingRequest(String model, String prompt) {
+            this.model = model;
+            this.prompt = prompt;
+        }
+    }
+}
