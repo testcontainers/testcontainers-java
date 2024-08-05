1ce0b1d7ba9e2c4ac2e896799678cf7be620d34e
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2024-08-05 14:55:24 -0500

    Check if docker-compose is available (#9060)

--- core/src/test/java/org/testcontainers/containers/DockerComposeProfilesOptionTest.java
+++ core/src/test/java/org/testcontainers/containers/DockerComposeProfilesOptionTest.java
@@ -1,8 +1,11 @@
 package org.testcontainers.containers;
 
+import org.assertj.core.api.Assumptions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.testcontainers.utility.CommandLine;
 
 import java.io.File;
 
@@ -17,16 +20,26 @@ public class DockerComposeProfilesOptionTest {
     }
 
     @Parameterized.Parameter
-    public boolean local;
+    public boolean localMode;
 
     public static final File COMPOSE_FILE = new File("src/test/resources/compose-profile-option/compose-test.yml");
+
+    @Before
+    public void setUp() {
+        if (this.localMode) {
+            Assumptions
+                .assumeThat(CommandLine.executableExists(DockerComposeContainer.COMPOSE_EXECUTABLE))
+                .as("docker-compose executable exists")
+                .isTrue();
+        }
+    }
 
     @Test
     public void testProfileOption() {
         try (
             DockerComposeContainer<?> compose = new DockerComposeContainer<>(COMPOSE_FILE)
                 .withOptions("--profile=cache")
-                .withLocalCompose(this.local)
+                .withLocalCompose(this.localMode)
         ) {
             compose.start();
             assertThat(compose.listChildContainers()).hasSize(1);
