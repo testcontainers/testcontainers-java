b7bde7298dc67e24ed887932cd79d2fddd3bc491
Eddú Meléndez <eddu.melendez@gmail.com>
2024-08-23 11:42:41 -0600

    Add javadocs

--- modules/kafka/src/main/java/org/testcontainers/kafka/ConfluentKafkaContainer.java
+++ modules/kafka/src/main/java/org/testcontainers/kafka/ConfluentKafkaContainer.java
@@ -73,12 +73,63 @@ public class ConfluentKafkaContainer extends GenericContainer<ConfluentKafkaCont
         copyFileToContainer(Transferable.of(command, 0777), KafkaHelper.STARTER_SCRIPT);
     }
 
+    /**
+     * Add a listener in the format {@code host:port}.
+     * Host will be included as a network alias.
+     * <p>
+     * Use it to register additional connections to the Kafka broker within the same container network.
+     * <p>
+     * The listener will be added to the list of default listeners.
+     * <p>
+     * Default listeners:
+     * <ul>
+     *     <li>0.0.0.0:9092</li>
+     *     <li>0.0.0.0:9093</li>
+     *     <li>0.0.0.0:9094</li>
+     * </ul>
+     * <p>
+     * The listener will be added to the list of default advertised listeners.
+     * <p>
+     * Default advertised listeners:
+     * <ul>
+     *      <li>{@code container.getConfig().getHostName():9092}</li>
+     *      <li>{@code container.getHost():container.getMappedPort(9093)}</li>
+     * </ul>
+     * @param listener a listener with format {@code host:port}
+     * @return this {@link ConfluentKafkaContainer} instance
+     */
     public ConfluentKafkaContainer withListener(String listener) {
         this.listeners.add(listener);
         this.advertisedListeners.add(() -> listener);
         return this;
     }
 
+    /**
+     * Add a listener in the format {@code host:port} and a {@link Supplier} for the advertised listener.
+     * Host from listener will be included as a network alias.
+     * <p>
+     * Use it to register additional connections to the Kafka broker from outside the container network
+     * <p>
+     * The listener will be added to the list of default listeners.
+     * <p>
+     * Default listeners:
+     * <ul>
+     *     <li>0.0.0.0:9092</li>
+     *     <li>0.0.0.0:9093</li>
+     *     <li>0.0.0.0:9094</li>
+     * </ul>
+     * <p>
+     * The {@link Supplier} will be added to the list of default advertised listeners.
+     * <p>
+     * Default advertised listeners:
+     * <ul>
+     *      <li>{@code container.getConfig().getHostName():9092}</li>
+     *      <li>{@code container.getHost():container.getMappedPort(9093)}</li>
+     * </ul>
+     * @param listener a supplier that will provide a listener
+     * @param advertisedListener a supplier that will provide a listener
+     * @return this {@link ConfluentKafkaContainer} instance
+     */
     public ConfluentKafkaContainer withListener(String listener, Supplier<String> advertisedListener) {
         this.listeners.add(listener);
         this.advertisedListeners.add(advertisedListener);
