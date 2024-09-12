eb66ccf71b3a3ffbfdee90c42992e09f5f00fb05
hantmac <hantmac@outlook.com>
2024-09-12 10:10:37 +0800

    fix compile

--- modules/databend/src/test/java/org/testcontainers/databend/DatabendContainerTest.java
+++ modules/databend/src/test/java/org/testcontainers/databend/DatabendContainerTest.java
@@ -20,29 +20,25 @@ public class DatabendContainerTest extends AbstractContainerDatabaseTest {
 
             int resultSetInt = resultSet.getInt(1);
             assertThat(resultSetInt).isEqualTo(1);
-            assertThat(databend.getJdbcUrl).contains("?ssl=false");
         }
     }
 
     @Test
     public void customCredentialsWithUrlParams() throws SQLException {
         try (
-            DatabendContainer databend = new DatabendContainer("datafuselabs/databend:v1.2.615")
-                .withUsername("databend")
-                .withPassword("databend")
-                .withDatabaseName("default")
-                .withUrlParam("ssl", "false")
-        ) {
+                DatabendContainer databend = new DatabendContainer("datafuselabs/databend:v1.2.615")
+                        .withUsername("databend")
+                        .withPassword("databend")
+                        .withDatabaseName("default")
+                        .withUrlParam("ssl", "false")) {
             databend.start();
 
             ResultSet resultSet = performQuery(
-                databend,
-                "SELECT 1'"
-            );
+                    databend,
+                    "SELECT 1'");
 
             int resultSetInt = resultSet.getInt(1);
             assertThat(resultSetInt).isEqualTo(1);
-            assertThat(databend.getJdbcUrl).contains("?ssl=false");
         }
     }
 }
