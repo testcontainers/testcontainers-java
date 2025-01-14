684a274014745790cfc6ca9cee53be853ea20b61
Marvin B. Lillehaug <marvin.lillehaug@gmail.com>
2025-01-14 20:00:17 +0100

    Tests pass, needed azure-messaging-servicebus:7.17.8

--- modules/azure/src/main/java/org/testcontainers/containers/ServicebusEmulatorContainer.java
+++ modules/azure/src/main/java/org/testcontainers/containers/ServicebusEmulatorContainer.java
@@ -3,12 +3,12 @@ package org.testcontainers.containers;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
-public class ServicebusEmulatorContainer extends GenericContainer<ServicebusEmulatorContainer> {
+public class ServicebusEmulatorContainer<SELF extends ServicebusEmulatorContainer<SELF>> extends GenericContainer<SELF> {
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse(
         "mcr.microsoft.com/azure-messaging/servicebus-emulator"
     );
 
-    private static final int PORT = 1433;
+    private static final int PORT = 5672;
 
     /**
      * @param dockerImageName specified docker image name to run
@@ -17,16 +17,31 @@ public class ServicebusEmulatorContainer extends GenericContainer<ServicebusEmul
         super(dockerImageName);
         dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
         withExposedPorts(PORT);
-        waitingFor(Wait.forLogMessage(".*Emulator Service is Successfully Up!\\r\\n$", 1));
-        Network network = Network.newNetwork();
-        withNetwork(network);
+
+        waitingFor(Wait.forLogMessage(".*Emulator Service is Successfully Up!.*", 1));
+
+        withNetwork(Network.SHARED);
         withNetworkAliases("sb-emulator");
+        MSSQLServerContainer mssqlServerContainer = new MSSQLServerContainer(DockerImageName.parse("mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04"))
+            .acceptLicense();
+        String mssqlNetworkAlias = "sqledge";
         dependsOn(
-             new SqlEdgeContainer(DockerImageName.parse("mcr.microsoft.com/azure-sql-edge:latest"))
-                 .withNetwork(network)
-                 .withNetworkAliases("sqledge")
+             mssqlServerContainer
+                 .withNetwork(Network.SHARED)
+                 .withNetworkAliases(mssqlNetworkAlias)
         );
+        addEnv("MSSQL_SA_PASSWORD", mssqlServerContainer.getPassword());
+        addEnv("SQL_SERVER", mssqlNetworkAlias);
+        acceptLicense();
+        addEnv("CONFIG_PATH", "/home/marvin.lillehaug/src/testcontainers-java/modules/azure/src/test/resources/servicebus-config.json");
+    }
+
+    /**
+     * Accepts the license for the Azure Service Bus Emulator container by setting the ACCEPT_EULA=Y
+     * variable as described at <a href="https://github.com/Azure/azure-service-bus-emulator-installer/blob/main/README.md">https://github.com/Azure/azure-service-bus-emulator-installer/blob/main/README.md#license</a>
+     */
+    public SELF acceptLicense() {
         addEnv("ACCEPT_EULA", "Y");
-        addEnv("CONFIG_PATH", "/home/marv/Downloads/asb/config.json");
+        return self();
     }
 }
--- modules/azure/src/test/java/org/testcontainers/containers/ServicebusEmulatorContainerTest.java
+++ modules/azure/src/test/java/org/testcontainers/containers/ServicebusEmulatorContainerTest.java
@@ -1,8 +1,21 @@
 package org.testcontainers.containers;
 
+import com.azure.core.util.IterableStream;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
+import com.azure.messaging.servicebus.ServiceBusReceiverClient;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ServicebusEmulatorContainerTest {
     @Rule
@@ -11,7 +24,27 @@ public class ServicebusEmulatorContainerTest {
     );
 
     @Test
-    public void testWIthASBClient() {
-        String containerId = servicebusEmulatorContainer.getContainerId();
+    public void testWithASBClient() {
+        Integer mappedPort = servicebusEmulatorContainer.getMappedPort(5672);
+        List<String> sentMessages = Arrays.asList("Hello World");
+        try(ServiceBusSenderClient sender = new ServiceBusClientBuilder()
+            .connectionString("Endpoint=sb://localhost:" + mappedPort + ";SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;")
+            .sender()
+            .queueName("queue.1")
+            .buildClient()) {
+            for (String m : sentMessages) {
+                sender.sendMessage(new ServiceBusMessage(m));
+            }
+        }
+        try(ServiceBusReceiverClient reciever = new ServiceBusClientBuilder()
+            .connectionString("Endpoint=sb://localhost:" + mappedPort + ";SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;")
+            .receiver()
+            .queueName("queue.1")
+            .receiveMode(ServiceBusReceiveMode.RECEIVE_AND_DELETE)
+            .buildClient()) {
+            IterableStream<ServiceBusReceivedMessage> messagesStream = reciever.receiveMessages(sentMessages.size());
+            List<String> recievedMessages = messagesStream.stream().map(m -> m.getBody().toString()).collect(Collectors.toList());
+            assertThat(recievedMessages).isEqualTo(sentMessages);
+        }
     }
 }
