d97d51bae048871c0437fa97f431b79e62e2d8ca
Kyle Aure <KyleJAure@gmail.com>
2025-03-18 10:39:18 -0500

    fix: spotless apply

--- core/src/test/java/org/testcontainers/containers/GenericContainerTest.java
+++ core/src/test/java/org/testcontainers/containers/GenericContainerTest.java
@@ -32,8 +32,8 @@ import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -44,14 +44,11 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
-import static org.rnorth.visibleassertions.VisibleAssertions.assertNotNull;
-import static org.rnorth.visibleassertions.VisibleAssertions.assertThrows;
-import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
-import static org.testcontainers.TestImages.TINY_IMAGE;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.assertj.core.api.Assumptions.assumeThat;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertNotNull;
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class GenericContainerTest {
 
@@ -242,12 +239,9 @@ public class GenericContainerTest {
     @Test
     public void shouldHonorUDPPorts() {
         ImageFromDockerfile image = new ImageFromDockerfile("publish-multiple")
-        .withDockerfileFromBuilder(builder ->
-            builder
-                .from("testcontainers/helloworld:1.1.0")
-                .expose(8080, 8081)
-                .build()
-        );
+            .withDockerfileFromBuilder(builder ->
+                builder.from("testcontainers/helloworld:1.1.0").expose(8080, 8081).build()
+            );
 
         try (
             GenericContainer container = new GenericContainer<>(image)
@@ -265,20 +259,25 @@ public class GenericContainerTest {
                     Ports ports = cmd.getPortBindings();
                     ports.bind(ExposedPort.udp(99), Ports.Binding.empty());
                     cmd.withPortBindings(ports);
-                }
-            )
+                })
         ) {
-
             container.start();
             ExposedPort expectedPort = ExposedPort.udp(99);
-            Map<ExposedPort, Ports.Binding[]> map = container.getContainerInfo().getNetworkSettings().getPorts().getBindings();
+            Map<ExposedPort, Ports.Binding[]> map = container
+                .getContainerInfo()
+                .getNetworkSettings()
+                .getPorts()
+                .getBindings();
 
             assertEquals("Two TCP ports should have been exposed.", 2, container.getExposedPorts().size());
             assertNotNull("withExposedPorts should have exposed UDP port", map.get(expectedPort));
-            assertTrue("UDP port 99 should have been mapped to a different port", 99 != Integer.valueOf(map.get(expectedPort)[0].getHostPortSpec()));
+            assertTrue(
+                "UDP port 99 should have been mapped to a different port",
+                99 != Integer.valueOf(map.get(expectedPort)[0].getHostPortSpec())
+            );
         }
     }
-    
+
     @Test
     public void shouldReturnTheProvidedImage() {
         GenericContainer container = new GenericContainer(TestImages.REDIS_IMAGE);
