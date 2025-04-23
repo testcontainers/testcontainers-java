5ef360ae20b241f595a6d29ca0df10a6ff9ffb48
Jay DeLuca <jaydeluca4@gmail.com>
2025-04-22 22:06:46 -0400

    add tempo port to lgtm container

--- modules/grafana/src/test/java/org/testcontainers/grafana/LgtmStackContainerTest.java
+++ modules/grafana/src/test/java/org/testcontainers/grafana/LgtmStackContainerTest.java
@@ -5,6 +5,15 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.registry.otlp.OtlpConfig;
 import io.micrometer.registry.otlp.OtlpMeterRegistry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.awaitility.Awaitility;
@@ -12,15 +21,16 @@ import org.junit.Test;
 import uk.org.webcompere.systemstubs.SystemStubs;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LgtmStackContainerTest {
 
     @Test
-    public void shouldPublishMetric() throws Exception {
+    public void shouldPublishMetricAndTrace() throws Exception {
         try ( // container {
-            LgtmStackContainer lgtm = new LgtmStackContainer("grafana/otel-lgtm:0.6.0")
+            LgtmStackContainer lgtm = new LgtmStackContainer("grafana/otel-lgtm:0.11.0")
             // }
         ) {
             lgtm.start();
@@ -29,7 +39,9 @@ public class LgtmStackContainerTest {
                 .get(String.format("http://%s:%s/api/health", lgtm.getHost(), lgtm.getMappedPort(3000)))
                 .jsonPath()
                 .get("version");
-            assertThat(version).isEqualTo("11.0.0");
+            assertThat(version).isEqualTo("11.6.0");
+
+            generateTrace(lgtm);
 
             OtlpConfig otlpConfig = createOtlpConfig(lgtm);
             MeterRegistry meterRegistry = SystemStubs
@@ -52,7 +64,50 @@ public class LgtmStackContainerTest {
                     assertThat(response.getStatusCode()).isEqualTo(200);
                     assertThat(response.body().jsonPath().getList("data.result[0].value")).contains("2");
                 });
+
+            Awaitility
+                .given()
+                .pollInterval(Duration.ofSeconds(2))
+                .atMost(Duration.ofSeconds(5))
+                .ignoreExceptions()
+                .untilAsserted(() -> {
+                    Response response = RestAssured
+                        .given()
+                        .get(String.format("%s/api/search", lgtm.getTempoUrl()))
+                        .prettyPeek()
+                        .thenReturn();
+                    assertThat(response.getStatusCode()).isEqualTo(200);
+                    assertThat(response.body().jsonPath().getString("traces[0].rootServiceName"))
+                        .isEqualTo("test-service");
+                });
         }
+    }
+
+    private void generateTrace(LgtmStackContainer lgtm) {
+        OtlpGrpcSpanExporter exporter = OtlpGrpcSpanExporter
+            .builder()
+            .setTimeout(Duration.ofSeconds(1))
+            .setEndpoint(lgtm.getOtlpGrpcUrl())
+            .build();
+
+        BatchSpanProcessor spanProcessor = BatchSpanProcessor
+            .builder(exporter)
+            .setScheduleDelay(500, TimeUnit.MILLISECONDS)
+            .build();
+
+        SdkTracerProvider tracerProvider = SdkTracerProvider
+            .builder()
+            .addSpanProcessor(spanProcessor)
+            .setResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "test-service")))
+            .build();
+
+        OpenTelemetrySdk openTelemetry = OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
+
+        Tracer tracer = openTelemetry.getTracer("test");
+        Span span = tracer.spanBuilder("test").startSpan();
+        span.end();
+
+        openTelemetry.shutdown();
     }
 
     private static OtlpConfig createOtlpConfig(LgtmStackContainer lgtm) {
