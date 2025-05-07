99ee59dc8ae5c31c4f0677ef9b0bf839602d6884
Jay DeLuca <jaydeluca4@gmail.com>
2025-05-07 05:52:43 -0400

    undo test refactoring

--- modules/grafana/src/test/java/org/testcontainers/grafana/LgtmStackContainerTest.java
+++ modules/grafana/src/test/java/org/testcontainers/grafana/LgtmStackContainerTest.java
@@ -21,9 +21,7 @@ import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.awaitility.Awaitility;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import uk.org.webcompere.systemstubs.SystemStubs;
 
 import java.time.Duration;
@@ -33,124 +31,125 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LgtmStackContainerTest {
 
-    private static final LgtmStackContainer lgtm = new LgtmStackContainer("grafana/otel-lgtm:0.11.1");
-
-    private static OpenTelemetrySdk openTelemetry;
-
-    @BeforeAll
-    static void setup() {
-        lgtm.start();
-        OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter
-            .builder()
-            .setTimeout(Duration.ofSeconds(1))
-            .setEndpoint(lgtm.getOtlpGrpcUrl())
-            .build();
-
-        OtlpGrpcLogRecordExporter logExporter = OtlpGrpcLogRecordExporter
-            .builder()
-            .setTimeout(Duration.ofSeconds(1))
-            .setEndpoint(lgtm.getOtlpGrpcUrl())
-            .build();
-
-        BatchSpanProcessor spanProcessor = BatchSpanProcessor
-            .builder(spanExporter)
-            .setScheduleDelay(500, TimeUnit.MILLISECONDS)
-            .build();
-
-        SdkTracerProvider tracerProvider = SdkTracerProvider
-            .builder()
-            .addSpanProcessor(spanProcessor)
-            .setResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "test-service")))
-            .build();
-
-        SdkLoggerProvider loggerProvider = SdkLoggerProvider
-            .builder()
-            .addLogRecordProcessor(SimpleLogRecordProcessor.create(logExporter))
-            .build();
-
-        openTelemetry =
-            OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).setLoggerProvider(loggerProvider).build();
-    }
-
-    @AfterAll
-    static void cleanup() {
-        openTelemetry.shutdown();
-        lgtm.stop();
-    }
-
     @Test
-    void shouldPublishMetric() throws Exception {
-        String version = RestAssured
-            .get(String.format("http://%s:%s/api/health", lgtm.getHost(), lgtm.getMappedPort(3000)))
-            .jsonPath()
-            .get("version");
-        assertThat(version).isEqualTo("12.0.0");
+    public void shouldPublishMetricsTracesAndLogs() throws Exception {
+        try ( // container {
+            LgtmStackContainer lgtm = new LgtmStackContainer("grafana/otel-lgtm:0.11.1")
+            // }
+        ) {
+            lgtm.start();
 
-        OtlpConfig otlpConfig = createOtlpConfig(lgtm);
-        MeterRegistry meterRegistry = SystemStubs
-            .withEnvironmentVariable("OTEL_SERVICE_NAME", "testcontainers")
-            .execute(() -> new OtlpMeterRegistry(otlpConfig, Clock.SYSTEM));
-        Counter.builder("test.counter").register(meterRegistry).increment(2);
+            OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter
+                .builder()
+                .setTimeout(Duration.ofSeconds(1))
+                .setEndpoint(lgtm.getOtlpGrpcUrl())
+                .build();
 
-        Awaitility
-            .given()
-            .pollInterval(Duration.ofSeconds(2))
-            .atMost(Duration.ofSeconds(5))
-            .ignoreExceptions()
-            .untilAsserted(() -> {
-                Response response = RestAssured
-                    .given()
-                    .queryParam("query", "test_counter_total{job=\"testcontainers\"}")
-                    .get(String.format("%s/api/v1/query", lgtm.getPrometheusHttpUrl()))
-                    .prettyPeek()
-                    .thenReturn();
-                assertThat(response.getStatusCode()).isEqualTo(200);
-                assertThat(response.body().jsonPath().getList("data.result[0].value")).contains("2");
-            });
-    }
+            OtlpGrpcLogRecordExporter logExporter = OtlpGrpcLogRecordExporter
+                .builder()
+                .setTimeout(Duration.ofSeconds(1))
+                .setEndpoint(lgtm.getOtlpGrpcUrl())
+                .build();
 
-    @Test
-    void shouldPublishLog() {
-        Logger logger = openTelemetry.getSdkLoggerProvider().loggerBuilder("test").build();
-        logger.logRecordBuilder().setBody("Test log!").setAttribute(AttributeKey.stringKey("job"), "test-job").emit();
+            BatchSpanProcessor spanProcessor = BatchSpanProcessor
+                .builder(spanExporter)
+                .setScheduleDelay(500, TimeUnit.MILLISECONDS)
+                .build();
 
-        Awaitility
-            .given()
-            .pollInterval(Duration.ofSeconds(2))
-            .atMost(Duration.ofSeconds(5))
-            .ignoreExceptions()
-            .untilAsserted(() -> {
-                Response response = RestAssured
-                    .given()
-                    .queryParam("query", "{service_name=\"unknown_service:java\"}")
-                    .get(String.format("%s/loki/api/v1/query_range", lgtm.getLokiUrl()))
-                    .prettyPeek()
-                    .thenReturn();
-                assertThat(response.getStatusCode()).isEqualTo(200);
-                assertThat(response.body().jsonPath().getString("data.result[0].values[0][1]")).isEqualTo("Test log!");
-            });
-    }
+            SdkTracerProvider tracerProvider = SdkTracerProvider
+                .builder()
+                .addSpanProcessor(spanProcessor)
+                .setResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "test-service")))
+                .build();
 
-    @Test
-    void shouldPublishTrace() {
-        Tracer tracer = openTelemetry.getTracer("test");
-        Span span = tracer.spanBuilder("test").startSpan();
-        span.end();
+            SdkLoggerProvider loggerProvider = SdkLoggerProvider
+                .builder()
+                .addLogRecordProcessor(SimpleLogRecordProcessor.create(logExporter))
+                .build();
 
-        Awaitility
-            .given()
-            .pollInterval(Duration.ofSeconds(2))
-            .atMost(Duration.ofSeconds(5))
-            .ignoreExceptions()
-            .untilAsserted(() -> {
-                Response response = RestAssured
-                    .given()
-                    .get(String.format("%s/api/search", lgtm.getTempoUrl()))
-                    .prettyPeek()
-                    .thenReturn();
-                assertThat(response.getStatusCode()).isEqualTo(200);
-                assertThat(response.body().jsonPath().getString("traces[0].rootServiceName")).isEqualTo("test-service");
-            });
+            OpenTelemetrySdk openTelemetry = OpenTelemetrySdk
+                .builder()
+                .setTracerProvider(tracerProvider)
+                .setLoggerProvider(loggerProvider)
+                .build();
+
+            // Metrics
+            String version = RestAssured
+                .get(String.format("http://%s:%s/api/health", lgtm.getHost(), lgtm.getMappedPort(3000)))
+                .jsonPath()
+                .get("version");
+            assertThat(version).isEqualTo("12.0.0");
+
+            OtlpConfig otlpConfig = createOtlpConfig(lgtm);
+            MeterRegistry meterRegistry = SystemStubs
+                .withEnvironmentVariable("OTEL_SERVICE_NAME", "testcontainers")
+                .execute(() -> new OtlpMeterRegistry(otlpConfig, Clock.SYSTEM));
+            Counter.builder("test.counter").register(meterRegistry).increment(2);
+
+            Awaitility
+                .given()
+                .pollInterval(Duration.ofSeconds(2))
+                .atMost(Duration.ofSeconds(5))
+                .ignoreExceptions()
+                .untilAsserted(() -> {
+                    Response response = RestAssured
+                        .given()
+                        .queryParam("query", "test_counter_total{job=\"testcontainers\"}")
+                        .get(String.format("%s/api/v1/query", lgtm.getPrometheusHttpUrl()))
+                        .prettyPeek()
+                        .thenReturn();
+                    assertThat(response.getStatusCode()).isEqualTo(200);
+                    assertThat(response.body().jsonPath().getList("data.result[0].value")).contains("2");
+                });
+
+            // Logs
+            Logger logger = openTelemetry.getSdkLoggerProvider().loggerBuilder("test").build();
+            logger
+                .logRecordBuilder()
+                .setBody("Test log!")
+                .setAttribute(AttributeKey.stringKey("job"), "test-job")
+                .emit();
+
+            Awaitility
+                .given()
+                .pollInterval(Duration.ofSeconds(2))
+                .atMost(Duration.ofSeconds(5))
+                .ignoreExceptions()
+                .untilAsserted(() -> {
+                    Response response = RestAssured
+                        .given()
+                        .queryParam("query", "{service_name=\"unknown_service:java\"}")
+                        .get(String.format("%s/loki/api/v1/query_range", lgtm.getLokiUrl()))
+                        .prettyPeek()
+                        .thenReturn();
+                    assertThat(response.getStatusCode()).isEqualTo(200);
+                    assertThat(response.body().jsonPath().getString("data.result[0].values[0][1]"))
+                        .isEqualTo("Test log!");
+                });
+
+            // Traces
+            Tracer tracer = openTelemetry.getTracer("test");
+            Span span = tracer.spanBuilder("test").startSpan();
+            span.end();
+
+            Awaitility
+                .given()
+                .pollInterval(Duration.ofSeconds(2))
+                .atMost(Duration.ofSeconds(5))
+                .ignoreExceptions()
+                .untilAsserted(() -> {
+                    Response response = RestAssured
+                        .given()
+                        .get(String.format("%s/api/search", lgtm.getTempoUrl()))
+                        .prettyPeek()
+                        .thenReturn();
+                    assertThat(response.getStatusCode()).isEqualTo(200);
+                    assertThat(response.body().jsonPath().getString("traces[0].rootServiceName"))
+                        .isEqualTo("test-service");
+                });
+
+            openTelemetry.close();
+        }
     }
 
     private static OtlpConfig createOtlpConfig(LgtmStackContainer lgtm) {
