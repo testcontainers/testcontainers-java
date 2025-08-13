fe095081c38e5c372192f6c850cee569fbe1cca7
SBushmelev <kkopka35@gmail.com>
2025-08-13 19:37:21 +0400

    Corrected the comments for org.testcontainers.influxdb.InfluxDBContainer

--- modules/influxdb/src/main/java/org/testcontainers/influxdb/InfluxDBContainer.java
+++ modules/influxdb/src/main/java/org/testcontainers/influxdb/InfluxDBContainer.java
@@ -1,23 +1,28 @@
-package org.testcontainers.containers;
+package org.testcontainers.influxdb;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.HttpResponseException;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.classic.methods.HttpPost;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.impl.classic.HttpClients;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
+import lombok.Getter;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
-import java.util.Set;
 
 import static java.lang.String.format;
 
 /**
  * Testcontainers implementation for InfluxDB 3 (InfluxDB IOx).
+ * <p>
+ * Supported image: {@code influxdb}
+ * <p>
+ * Exposed ports: 8181
  * <p>
  * This container provides a instance of InfluxDB 3.x for integration testing.
  * It supports both authenticated and non-authenticated modes.
@@ -26,7 +31,7 @@ import static java.lang.String.format;
  * <p>
  * <strong>Example usage:</strong>
  * <pre>{@code
- * try (InfluxDBContainerV3<?> influxDB = new InfluxDBContainerV3<>("influxdb:3-core")) {
+ * try (InfluxDBContainer influxDB = new InfluxDBContainer("influxdb:3-core")) {
  *     influxDB.start();
  *     String url = influxDB.getUrl();
  *     String token = influxDB.getToken();
@@ -35,23 +40,21 @@ import static java.lang.String.format;
  * }</pre>
  * </p>
  */
-public class InfluxDBContainerV3<SELF extends InfluxDBContainerV3<SELF>> extends GenericContainer<SELF> {
+public class InfluxDBContainer extends GenericContainer<InfluxDBContainer> {
 
     /**
      * The default port exposed by InfluxDB 3.
      */
-    public static final Integer INFLUXDB_PORT = 8181;
+    private static final Integer INFLUXDB_PORT = 8181;
 
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("influxdb");
 
     /**
-     * The authentication token for InfluxDB 3. Lazily initialized and thread-safe.
+     * The authentication token for InfluxDB 3.
      */
-    private volatile String token;
+    @Getter
+    private String token;
 
-    /**
-     * Flag indicating whether authentication is disabled.
-     */
     private boolean isAuthDisable;
 
     /**
@@ -59,7 +62,7 @@ public class InfluxDBContainerV3<SELF extends InfluxDBContainerV3<SELF>> extends
      *
      * @param dockerImageName the name of the Docker image
      */
-    public InfluxDBContainerV3(final DockerImageName dockerImageName) {
+    public InfluxDBContainer(final DockerImageName dockerImageName) {
         super(dockerImageName);
         dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
 
@@ -78,7 +81,7 @@ public class InfluxDBContainerV3<SELF extends InfluxDBContainerV3<SELF>> extends
      *
      * @return the generated authentication token
      * @throws IllegalArgumentException if the token cannot be created due to HTTP or IO errors
-     * @throws HttpResponseException if the InfluxDB server returns a non-201 status code
+     * @throws HttpResponseException    if the InfluxDB server returns a non-201 status code
      */
     private String createToken() {
         HttpPost httpPost = new HttpPost(format("%s/api/v3/configure/token/admin", getUrl()));
@@ -115,14 +118,6 @@ public class InfluxDBContainerV3<SELF extends InfluxDBContainerV3<SELF>> extends
     }
 
     /**
-     * @return a singleton set containing the mapped InfluxDB port
-     */
-    @Override
-    public Set<Integer> getLivenessCheckPortNumbers() {
-        return Collections.singleton(getMappedPort(INFLUXDB_PORT));
-    }
-
-    /**
      * Disables authentication for this InfluxDB instance.
      * <p>
      * When authentication is disabled, no token is required to access the database.
@@ -130,8 +125,8 @@ public class InfluxDBContainerV3<SELF extends InfluxDBContainerV3<SELF>> extends
      *
      * @return this container instance for method chaining
      */
-    public InfluxDBContainerV3<SELF> withDisableAuth() {
-        isAuthDisable = true;
+    public InfluxDBContainer withAuthDisabled() {
+        this.isAuthDisable = true;
         return this;
     }
 
@@ -147,26 +142,10 @@ public class InfluxDBContainerV3<SELF extends InfluxDBContainerV3<SELF>> extends
         return "http://" + getHost() + ":" + getMappedPort(INFLUXDB_PORT);
     }
 
-    /**
-     * Gets the authentication token for this InfluxDB instance.
-     * <p>
-     * The token is lazily initialized on first use and cached for subsequent calls.
-     * This method is thread-safe.
-     * </p>
-     *
-     * @return the authentication token
-     * @throws IllegalArgumentException if authentication is disabled or token creation fails
-     */
-    public String getToken() {
-        String localToken = token;
-        if (localToken == null) {
-            synchronized (this) {
-                localToken = token;
-                if (localToken == null) {
-                    token = localToken = createToken();
-                }
-            }
+    @Override
+    protected void containerIsStarted(InspectContainerResponse containerInfo) {
+        if (!isAuthDisable) {
+            this.token = createToken();
         }
-        return localToken;
     }
 }
