5b8fdbc1816ac99d665ffb601e7dd1cd893b9674
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2025-09-02 15:35:42 -0500

    Move GCloud tests to JUnit Jupiter (#10736)

--- modules/gcloud/src/test/java/org/testcontainers/containers/FirestoreEmulatorContainerTest.java
+++ modules/gcloud/src/test/java/org/testcontainers/containers/FirestoreEmulatorContainerTest.java
@@ -8,8 +8,7 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.HashMap;
@@ -18,46 +17,47 @@ import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FirestoreEmulatorContainerTest {
-
-    @Rule
-    // emulatorContainer {
-    public FirestoreEmulatorContainer emulator = new FirestoreEmulatorContainer(
-        DockerImageName.parse("gcr.io/google.com/cloudsdktool/google-cloud-cli:441.0.0-emulators")
-    );
-
-    // }
+class FirestoreEmulatorContainerTest {
 
     // testWithEmulatorContainer {
     @Test
-    public void testSimple() throws ExecutionException, InterruptedException {
-        FirestoreOptions options = FirestoreOptions
-            .getDefaultInstance()
-            .toBuilder()
-            .setHost(emulator.getEmulatorEndpoint())
-            .setCredentials(NoCredentials.getInstance())
-            .setProjectId("test-project")
-            .build();
-        Firestore firestore = options.getService();
+    void testSimple() throws ExecutionException, InterruptedException {
+        try (
+            // emulatorContainer {
+            FirestoreEmulatorContainer emulator = new FirestoreEmulatorContainer(
+                DockerImageName.parse("gcr.io/google.com/cloudsdktool/google-cloud-cli:441.0.0-emulators")
+            );
+            // }
+        ) {
+            emulator.start();
+            FirestoreOptions options = FirestoreOptions
+                .getDefaultInstance()
+                .toBuilder()
+                .setHost(emulator.getEmulatorEndpoint())
+                .setCredentials(NoCredentials.getInstance())
+                .setProjectId("test-project")
+                .build();
+            Firestore firestore = options.getService();
 
-        CollectionReference users = firestore.collection("users");
-        DocumentReference docRef = users.document("alovelace");
-        Map<String, Object> data = new HashMap<>();
-        data.put("first", "Ada");
-        data.put("last", "Lovelace");
-        ApiFuture<WriteResult> result = docRef.set(data);
-        result.get();
+            CollectionReference users = firestore.collection("users");
+            DocumentReference docRef = users.document("alovelace");
+            Map<String, Object> data = new HashMap<>();
+            data.put("first", "Ada");
+            data.put("last", "Lovelace");
+            ApiFuture<WriteResult> result = docRef.set(data);
+            result.get();
 
-        ApiFuture<QuerySnapshot> query = users.get();
-        QuerySnapshot querySnapshot = query.get();
+            ApiFuture<QuerySnapshot> query = users.get();
+            QuerySnapshot querySnapshot = query.get();
 
-        assertThat(querySnapshot.getDocuments().get(0).getData()).containsEntry("first", "Ada");
+            assertThat(querySnapshot.getDocuments().get(0).getData()).containsEntry("first", "Ada");
+        }
     }
 
     // }
 
     @Test
-    public void testWithFlags() {
+    void testWithFlags() {
         try (
             FirestoreEmulatorContainer emulator = new FirestoreEmulatorContainer(
                 "gcr.io/google.com/cloudsdktool/google-cloud-cli:465.0.0-emulators"
