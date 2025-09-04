c5ce70dcd955ad7d75aae1d73cefef22c8d006bd
Eddú Meléndez <eddu.melendez@gmail.com>
2025-09-03 19:32:05 -0600

    Move MockServer tests to JUnit Jupiter

--- modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
+++ modules/mockserver/src/test/java/org/testcontainers/containers/MockServerContainerTest.java
@@ -3,7 +3,7 @@ package org.testcontainers.containers;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.configuration.Configuration;
 import org.mockserver.logging.MockServerLogger;
@@ -15,14 +15,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
-public class MockServerContainerTest {
+class MockServerContainerTest {
 
     public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
         .parse("mockserver/mockserver")
         .withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
 
     @Test
-    public void shouldCallActualMockserverVersion() throws Exception {
+    void shouldCallActualMockserverVersion() throws Exception {
         try (MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)) {
             mockServer.start();
 
@@ -41,7 +41,7 @@ public class MockServerContainerTest {
     }
 
     @Test
-    public void shouldCallMockserverUsingTlsProtocol() throws Exception {
+    void shouldCallMockserverUsingTlsProtocol() throws Exception {
         try (MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)) {
             mockServer.start();
 
@@ -63,7 +63,7 @@ public class MockServerContainerTest {
     }
 
     @Test
-    public void shouldCallMockserverUsingMutualTlsProtocol() throws Exception {
+    void shouldCallMockserverUsingMutualTlsProtocol() throws Exception {
         try (
             MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)
                 .withEnv("MOCKSERVER_TLS_MUTUAL_AUTHENTICATION_REQUIRED", "true")
@@ -88,7 +88,7 @@ public class MockServerContainerTest {
     }
 
     @Test
-    public void newVersionStartsWithDefaultWaitStrategy() {
+    void newVersionStartsWithDefaultWaitStrategy() {
         try (MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE)) {
             mockServer.start();
         }
