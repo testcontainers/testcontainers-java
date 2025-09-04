69083da9688dd24f1ccd3582862a3cfc23ada747
Eddú Meléndez <eddu.melendez@gmail.com>
2025-09-04 16:56:12 -0600

    Move Solr assertions to assertj

--- modules/solr/src/test/java/org/testcontainers/containers/SolrContainerTest.java
+++ modules/solr/src/test/java/org/testcontainers/containers/SolrContainerTest.java
@@ -4,38 +4,33 @@ import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.Http2SolrClient;
 import org.apache.solr.client.solrj.response.SolrPingResponse;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(Parameterized.class)
 public class SolrContainerTest {
 
-    @Parameterized.Parameters(name = "{0}")
+    private SolrClient client = null;
+
     public static String[] getVersionsToTest() {
         return new String[] { "solr:8.11.4", "solr:9.8.0" };
     }
 
-    @Parameterized.Parameter
-    public String solrImage;
-
-    private SolrClient client = null;
-
-    @After
-    public void stopRestClient() throws IOException {
+    @AfterEach
+    void stopRestClient() throws IOException {
         if (client != null) {
             client.close();
             client = null;
         }
     }
 
-    @Test
-    public void solrCloudTest() throws IOException, SolrServerException {
+    @ParameterizedTest
+    @MethodSource("getVersionsToTest")
+    void solrCloudTest(String solrImage) throws IOException, SolrServerException {
         try (SolrContainer container = new SolrContainer(solrImage)) {
             container.start();
             SolrPingResponse response = getClient(container).ping("dummy");
@@ -44,8 +39,9 @@ public class SolrContainerTest {
         }
     }
 
-    @Test
-    public void solrStandaloneTest() throws IOException, SolrServerException {
+    @ParameterizedTest
+    @MethodSource("getVersionsToTest")
+    void solrStandaloneTest(String solrImage) throws IOException, SolrServerException {
         try (SolrContainer container = new SolrContainer(solrImage).withZookeeper(false)) {
             container.start();
             SolrPingResponse response = getClient(container).ping("dummy");
@@ -54,8 +50,9 @@ public class SolrContainerTest {
         }
     }
 
-    @Test
-    public void solrCloudPingTest() throws IOException, SolrServerException {
+    @ParameterizedTest
+    @MethodSource("getVersionsToTest")
+    void solrCloudPingTest(String solrImage) throws IOException, SolrServerException {
         // solrContainerUsage {
         // Create the solr container.
         SolrContainer container = new SolrContainer(solrImage);
