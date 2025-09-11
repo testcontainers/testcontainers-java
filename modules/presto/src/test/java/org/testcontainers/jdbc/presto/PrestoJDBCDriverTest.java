b570f26432c1c22f55521138192e15c9eb83904e
Eddú Meléndez <eddu.melendez@gmail.com>
2025-09-11 10:28:34 -0600

    Move Presto tests to JUnit Jupiter

--- modules/presto/src/test/java/org/testcontainers/jdbc/presto/PrestoJDBCDriverTest.java
+++ modules/presto/src/test/java/org/testcontainers/jdbc/presto/PrestoJDBCDriverTest.java
@@ -1,16 +1,12 @@
 package org.testcontainers.jdbc.presto;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.testcontainers.jdbc.AbstractJDBCDriverTest;
 
 import java.util.Arrays;
 import java.util.EnumSet;
 
-@RunWith(Parameterized.class)
-public class PrestoJDBCDriverTest extends AbstractJDBCDriverTest {
+class PrestoJDBCDriverTest extends AbstractJDBCDriverTest {
 
-    @Parameterized.Parameters(name = "{index} - {0}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(
             new Object[][] { //
