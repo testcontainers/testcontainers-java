e73cc7fab89d47c930bf24997165ab6cfab57698
Eddú Meléndez <eddu.melendez@gmail.com>
2025-09-11 10:25:42 -0600

    Move CrateDB tests to JUnit Jupiter

--- modules/cratedb/src/test/java/org/testcontainers/jdbc/cratedb/CrateDBJDBCDriverTest.java
+++ modules/cratedb/src/test/java/org/testcontainers/jdbc/cratedb/CrateDBJDBCDriverTest.java
@@ -1,16 +1,12 @@
 package org.testcontainers.jdbc.cratedb;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.testcontainers.jdbc.AbstractJDBCDriverTest;
 
 import java.util.Arrays;
 import java.util.EnumSet;
 
-@RunWith(Parameterized.class)
-public class CrateDBJDBCDriverTest extends AbstractJDBCDriverTest {
+class CrateDBJDBCDriverTest extends AbstractJDBCDriverTest {
 
-    @Parameterized.Parameters(name = "{index} - {0}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(
             new Object[][] {
