ba27f37fdb5397731c9ca45f01657005b8cdb0b4
Eddú Meléndez <eddu.melendez@gmail.com>
2025-09-24 12:07:07 -0600

    Polish

--- modules/gcloud/src/test/java/org/testcontainers/containers/BigQueryEmulatorContainerTest.java
+++ modules/gcloud/src/test/java/org/testcontainers/containers/BigQueryEmulatorContainerTest.java
@@ -1,5 +1,6 @@
 package org.testcontainers.containers;
 
+import com.google.api.core.ApiFuture;
 import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.api.gax.grpc.GrpcTransportChannel;
 import com.google.api.gax.rpc.FixedTransportChannelProvider;
@@ -17,16 +18,23 @@ import com.google.cloud.bigquery.TableDefinition;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableInfo;
 import com.google.cloud.bigquery.TableResult;
+import com.google.cloud.bigquery.storage.v1.AppendRowsResponse;
+import com.google.cloud.bigquery.storage.v1.BatchCommitWriteStreamsRequest;
+import com.google.cloud.bigquery.storage.v1.BatchCommitWriteStreamsResponse;
 import com.google.cloud.bigquery.storage.v1.BigQueryWriteClient;
 import com.google.cloud.bigquery.storage.v1.BigQueryWriteSettings;
 import com.google.cloud.bigquery.storage.v1.CreateWriteStreamRequest;
+import com.google.cloud.bigquery.storage.v1.FinalizeWriteStreamRequest;
+import com.google.cloud.bigquery.storage.v1.FinalizeWriteStreamResponse;
+import com.google.cloud.bigquery.storage.v1.JsonStreamWriter;
 import com.google.cloud.bigquery.storage.v1.TableName;
 import com.google.cloud.bigquery.storage.v1.WriteStream;
 import io.grpc.ManagedChannelBuilder;
-import org.threeten.bp.Duration;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.threeten.bp.Duration;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,17 +42,6 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BigQueryEmulatorContainerTest {
-
-    private BigQuery getBigQuery(BigQueryEmulatorContainer container) {
-        String url = container.getEmulatorHttpEndpoint();
-        return BigQueryOptions
-            .newBuilder()
-            .setProjectId(container.getProjectId())
-            .setHost(url)
-            .setLocation(url)
-            .setCredentials(NoCredentials.getInstance())
-            .build().getService();
-    }
 
     @Test
     void testHttpEndpoint() throws Exception {
@@ -56,7 +53,15 @@ class BigQueryEmulatorContainerTest {
             container.start();
 
             // bigQueryClient {
-            BigQuery bigQuery = getBigQuery(container);
+            String url = container.getEmulatorHttpEndpoint();
+            BigQueryOptions options = BigQueryOptions
+                .newBuilder()
+                .setProjectId(container.getProjectId())
+                .setHost(url)
+                .setLocation(url)
+                .setCredentials(NoCredentials.getInstance())
+                .build();
+            BigQuery bigQuery = options.getService();
             // }
 
             String fn =
@@ -76,21 +81,19 @@ class BigQueryEmulatorContainerTest {
     }
 
     @Test
-    void testGrcpEndpoint() throws IOException {
-        try (BigQueryEmulatorContainer container = new BigQueryEmulatorContainer("ghcr.io/goccy/bigquery-emulator:0.6.5")) {
+    void testGrcpEndpoint() throws Exception {
+        try (
+            BigQueryEmulatorContainer container = new BigQueryEmulatorContainer("ghcr.io/goccy/bigquery-emulator:0.6.5")
+        ) {
             container.start();
 
-            // Test setup.
-            // Create a table the "regular" way. We need this to verify we can connect a writestream
-            BigQuery bigQuery =  getBigQuery(container);
+            BigQuery bigQuery = getBigQuery(container);
             String tableName = "test-table";
             String datasetName = "test-dataset";
 
             bigQuery.create(DatasetInfo.of(DatasetId.of(container.getProjectId(), datasetName)));
 
-            Schema schema = Schema.of(
-                Field.of("name", StandardSQLTypeName.STRING)
-            );
+            Schema schema = Schema.of(Field.of("name", StandardSQLTypeName.STRING));
 
             TableId tableId = TableId.of(datasetName, tableName);
             TableDefinition tableDefinition = StandardTableDefinition.of(schema);
@@ -98,37 +101,104 @@ class BigQueryEmulatorContainerTest {
 
             bigQuery.create(tableInfo);
 
-            // Actual test.
-            // BigQueryWriteSettings requires a HTTP/2 connection, not provided by the originally exposed endpoint.
             BigQueryWriteSettings.Builder bigQueryWriteSettingsBuilder = BigQueryWriteSettings.newBuilder();
 
-            bigQueryWriteSettingsBuilder.createWriteStreamSettings()
-                .setRetrySettings(bigQueryWriteSettingsBuilder.createWriteStreamSettings()
-                    .getRetrySettings()
-                    .toBuilder()
-                    .setTotalTimeout(Duration.ofSeconds(60))
-                    .build());
+            bigQueryWriteSettingsBuilder
+                .createWriteStreamSettings()
+                .setRetrySettings(
+                    bigQueryWriteSettingsBuilder
+                        .createWriteStreamSettings()
+                        .getRetrySettings()
+                        .toBuilder()
+                        .setTotalTimeout(Duration.ofSeconds(60))
+                        .build()
+                );
 
-            // Use the now exposed grpcPort to get a working connection.
             BigQueryWriteClient bigQueryWriteClient = BigQueryWriteClient.create(
-                bigQueryWriteSettingsBuilder.setTransportChannelProvider(FixedTransportChannelProvider.create(GrpcTransportChannel.create(
-                        ManagedChannelBuilder.forAddress(container.getHost(), container.getEmulatorGrpcPort()).usePlaintext().build())))
+                bigQueryWriteSettingsBuilder
+                    .setTransportChannelProvider(
+                        FixedTransportChannelProvider.create(
+                            GrpcTransportChannel.create(
+                                ManagedChannelBuilder
+                                    .forAddress(container.getHost(), container.getEmulatorGrpcPort())
+                                    .usePlaintext()
+                                    .build()
+                            )
+                        )
+                    )
                     .setCredentialsProvider(NoCredentialsProvider.create())
                     .build()
             );
 
             TableName parentTable = TableName.of(container.getProjectId(), datasetName, tableName);
-            CreateWriteStreamRequest createWriteStreamRequest = CreateWriteStreamRequest.newBuilder()
+            CreateWriteStreamRequest createWriteStreamRequest = CreateWriteStreamRequest
+                .newBuilder()
                 .setParent(parentTable.toString())
                 .setWriteStream(WriteStream.newBuilder().setType(WriteStream.Type.PENDING))
                 .build();
 
-            // Validate that we can successfully create a write stream. This would not work with http endpoint
-            bigQueryWriteClient.createWriteStream(createWriteStreamRequest);
+            WriteStream writeStream = bigQueryWriteClient.createWriteStream(createWriteStreamRequest);
+
+            JsonStreamWriter writer = JsonStreamWriter
+                .newBuilder(writeStream.getName(), writeStream.getTableSchema(), bigQueryWriteClient)
+                .build();
+
+            JSONArray jsonArray = new JSONArray();
+            JSONObject record1 = new JSONObject();
+            record1.put("name", "Alice");
+            jsonArray.put(record1);
+
+            JSONObject record2 = new JSONObject();
+            record2.put("name", "Bob");
+            jsonArray.put(record2);
+
+            ApiFuture<AppendRowsResponse> future = writer.append(jsonArray);
+            AppendRowsResponse response = future.get();
+
+            FinalizeWriteStreamRequest finalizeRequest = FinalizeWriteStreamRequest
+                .newBuilder()
+                .setName(writeStream.getName())
+                .build();
+            FinalizeWriteStreamResponse finalizeResponse = bigQueryWriteClient.finalizeWriteStream(finalizeRequest);
+
+            BatchCommitWriteStreamsRequest commitRequest = BatchCommitWriteStreamsRequest
+                .newBuilder()
+                .setParent(parentTable.toString())
+                .addWriteStreams(writeStream.getName())
+                .build();
+            BatchCommitWriteStreamsResponse commitResponse = bigQueryWriteClient.batchCommitWriteStreams(commitRequest);
+
+            writer.close();
+
+            String sql = String.format(
+                "SELECT name FROM `%s.%s.%s` ORDER BY name",
+                container.getProjectId(),
+                datasetName,
+                tableName
+            );
+            TableResult result = bigQuery.query(QueryJobConfiguration.newBuilder(sql).build());
+
+            List<String> names = result
+                .streamValues()
+                .map(row -> row.get("name").getStringValue())
+                .collect(Collectors.toList());
+
+            assertThat(names).containsExactly("Alice", "Bob");
 
             bigQueryWriteClient.shutdown();
             bigQueryWriteClient.close();
         }
     }
 
+    private BigQuery getBigQuery(BigQueryEmulatorContainer container) {
+        String url = container.getEmulatorHttpEndpoint();
+        return BigQueryOptions
+            .newBuilder()
+            .setProjectId(container.getProjectId())
+            .setHost(url)
+            .setLocation(url)
+            .setCredentials(NoCredentials.getInstance())
+            .build()
+            .getService();
+    }
 }
