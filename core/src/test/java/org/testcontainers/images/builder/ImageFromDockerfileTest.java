rnorth/testpackage-containers

Do not transfer mountable file when tar name is "." or ".." (docker is unable to use this files) or transferring into directory

core/src/test/java/org/testcontainers/images/builder/ImageFromDockerfileTest.java
@@ -7,7 +7,63 @@ import org.testcontainers.DockerClientFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+@RunWith(Enclosed.class)
 public class ImageFromDockerfileTest {
+    @RunWith(Parameterized.class)
+    public static class InvalidTarPathTests {
+        @Rule
+        public final ExpectedException expectedException = ExpectedException.none();
+
+        @Parameterized.Parameters(name = "{index} - {0}")
+        public static String[] getTestCases() {
+            return new String[]{
+                "..",
+                ".",
+                "../",
+                "./",
+                "xxx/",
+                "yyy/xxx/",
+                "/xxx/",
+                "/yyy/xxx/",
+                "/..",
+                "/.",
+                "/../",
+                "/./",
+                ".",
+                "..",
+                "aa/.",
+                "aa/..",
+                "bb/./",
+                "bb/../",
+                "cc./",
+                "cc../"
+            };
+        }
+
+        @Parameterized.Parameter
+        public String tarPath;
+
+        @Test
+        public void unableToTransferFileWithDotsToDockerDaemon() {
+            expectedException.expect(IllegalArgumentException.class);
+            expectedException.expectMessage("Unable to store file '" +
+                Paths.get("src", "test", "resources", "mappable-resource", "test-resource.txt") +
+                "' to docker path '" + tarPath + "'");
+
+            new ImageFromDockerfile()
+                .withFileFromFile(tarPath, new File("src/test/resources/mappable-resource/test-resource.txt"));
+        }
+    }
 
     @Test
     public void shouldAddDefaultLabels() {

testcontainers/testcontainers-java

core/src/test/java/org/testcontainers/images/builder/ImageFromDockerfileTest.java
@@ -3,10 +3,16 @@ package org.testcontainers.images.builder;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.InspectImageResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.utility.Base58;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ImageFromDockerfileTest {
 
@@ -43,5 +49,45 @@ class ImageFromDockerfileTest {
             // ensure the image is deleted, even if the test fails
             dockerClient.removeImageCmd(imageId).exec();
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = {
+            "..",
+            ".",
+            "../",
+            "./",
+            "xxx/",
+            "yyy/xxx/",
+            "/xxx/",
+            "/yyy/xxx/",
+            "/..",
+            "/.",
+            "/../",
+            "/./",
+            ".",
+            "..",
+            "aa/.",
+            "aa/..",
+            "bb/./",
+            "bb/../",
+            "cc./",
+            "cc../",
+        }
+    )
+    void unableToTransferFileWithDotsToDockerDaemon(String tarPath) {
+        assertThatThrownBy(() -> {
+                new ImageFromDockerfile()
+                    .withFileFromFile(tarPath, new File("src/test/resources/mappable-resource/test-resource.txt"));
+            })
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(
+                "Unable to store file '" +
+                Paths.get("src", "test", "resources", "mappable-resource", "test-resource.txt") +
+                "' to docker path '" +
+                tarPath +
+                "'"
+            );
     }
 }