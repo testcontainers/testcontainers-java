b96ee0740a3b4164bf0fd70dfc6bfddc59992f3d
himani-s993 <himanis993@gmail.com>
2026-04-02 23:39:38 +0200

    Fix NullPointerException in ContainerLessJdbcDelegate when using ScriptUtils

--- modules/jdbc/src/main/java/org/testcontainers/jdbc/JdbcDatabaseDelegate.java
+++ modules/jdbc/src/main/java/org/testcontainers/jdbc/JdbcDatabaseDelegate.java
@@ -40,11 +40,12 @@ public class JdbcDatabaseDelegate extends AbstractDatabaseDelegate<Statement> {
 
     @Override
     public void execute(
-            String statement,
-            String scriptPath,
-            int lineNumber,
-            boolean continueOnError,
-            boolean ignoreFailedDrops) {
+        String statement,
+        String scriptPath,
+        int lineNumber,
+        boolean continueOnError,
+        boolean ignoreFailedDrops
+    ) {
         try {
             boolean rowsAffected = getConnection().execute(statement);
             log.debug("{} returned as updateCount for SQL: {}", rowsAffected, statement);
@@ -52,11 +53,12 @@ public class JdbcDatabaseDelegate extends AbstractDatabaseDelegate<Statement> {
             boolean dropStatement = statement.trim().toLowerCase().startsWith("drop");
             if (continueOnError || (dropStatement && ignoreFailedDrops)) {
                 log.debug(
-                        "Failed to execute SQL script statement at line {} of resource {}: {}",
-                        lineNumber,
-                        scriptPath,
-                        statement,
-                        ex);
+                    "Failed to execute SQL script statement at line {} of resource {}: {}",
+                    lineNumber,
+                    scriptPath,
+                    statement,
+                    ex
+                );
             } else {
                 throw new ScriptUtils.ScriptStatementFailedException(statement, lineNumber, scriptPath, ex);
             }
