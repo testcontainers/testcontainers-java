fb83a3c84d578d9395154cb417795b10985a63cd
Jebastine E <jebastine230906@gmail.com>
2026-09-07 22:25:51 +0530

    fix: support MongoDB init scripts

--- modules/mongodb/src/main/java/org/testcontainers/mongodb/MongoDBContainer.java
+++ modules/mongodb/src/main/java/org/testcontainers/mongodb/MongoDBContainer.java
@@ -6,6 +6,8 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
@@ -41,9 +43,15 @@ public class MongoDBContainer extends GenericContainer<MongoDBContainer> {
 
     private static final String STARTER_SCRIPT = "/testcontainers_start.sh";
 
+    private static final String INIT_SCRIPT_DIR = "/docker-entrypoint-initdb.d";
+
     private boolean shardingEnabled;
 
     private boolean rsEnabled;
+
+    private boolean hasInitScript;
+
+    private boolean customWaitStrategy;
 
     public MongoDBContainer(@NonNull String dockerImageName) {
         this(DockerImageName.parse(dockerImageName));
@@ -54,12 +62,50 @@ public class MongoDBContainer extends GenericContainer<MongoDBContainer> {
         dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME, COMMUNITY_SERVER_IMAGE, ENTERPRISE_SERVER_IMAGE);
 
         withExposedPorts(MONGODB_INTERNAL_PORT);
+        super.waitingFor(Wait.forLogMessage("(?i).*waiting for connections.*", 1));
+    }
+
+    @Override
+    public MongoDBContainer waitingFor(@NonNull WaitStrategy waitStrategy) {
+        this.customWaitStrategy = true;
+        return super.waitingFor(waitStrategy);
+    }
+
+    @Override
+    public MongoDBContainer withCopyFileToContainer(MountableFile mountableFile, String containerPath) {
+        checkInitScript(containerPath);
+        return super.withCopyFileToContainer(mountableFile, containerPath);
+    }
+
+    @Override
+    public MongoDBContainer withCopyToContainer(Transferable transferable, String containerPath) {
+        checkInitScript(containerPath);
+        return super.withCopyToContainer(transferable, containerPath);
+    }
+
+    private static boolean isInitScriptPath(String path) {
+        return path != null && (path.equals(INIT_SCRIPT_DIR) || path.startsWith(INIT_SCRIPT_DIR + "/"));
+    }
+
+    private void checkInitScript(String containerPath) {
+        if (isInitScriptPath(containerPath)) {
+            this.hasInitScript = true;
+        }
+    }
+
+    private boolean hasInitScript() {
+        return (
+            this.hasInitScript ||
+            getBinds().stream().anyMatch(b -> b.getVolume() != null && isInitScriptPath(b.getVolume().getPath()))
+        );
     }
 
     @Override
     protected void containerIsStarting(InspectContainerResponse containerInfo) {
         if (this.shardingEnabled) {
             copyFileToContainer(MountableFile.forClasspathResource("/sharding.sh", 0777), STARTER_SCRIPT);
+        } else if (!this.customWaitStrategy && hasInitScript()) {
+            setWaitStrategy(Wait.forLogMessage("(?i).*waiting for connections.*", 2));
         }
     }
 
@@ -157,7 +203,6 @@ public class MongoDBContainer extends GenericContainer<MongoDBContainer> {
     public MongoDBContainer withReplicaSet() {
         this.rsEnabled = true;
         withCommand("--replSet", "docker-rs");
-        waitingFor(Wait.forLogMessage("(?i).*waiting for connections.*", 1));
         return this;
     }
 
--- modules/mongodb/src/test/java/org/testcontainers/mongodb/MongoDBContainerTest.java
+++ modules/mongodb/src/test/java/org/testcontainers/mongodb/MongoDBContainerTest.java
@@ -1,6 +1,14 @@
 package org.testcontainers.mongodb;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+import org.testcontainers.utility.MountableFile;
+
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,6 +44,58 @@ class MongoDBContainerTest extends AbstractMongo {
             mongoDBContainer.start();
             final String databaseName = "my-db";
             assertThat(mongoDBContainer.getReplicaSetUrl(databaseName)).endsWith(databaseName);
+        }
+    }
+
+    @Test
+    void shouldRunInitScript() {
+        try (
+            MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10")
+                .withCopyFileToContainer(
+                    MountableFile.forClasspathResource("mongo-init.js"),
+                    "/docker-entrypoint-initdb.d/mongo-init.js"
+                )
+        ) {
+            mongoDBContainer.start();
+
+            try (MongoClient mongoClient = MongoClients.create(mongoDBContainer.getConnectionString())) {
+                final Document document = mongoClient
+                    .getDatabase("init-script-db")
+                    .getCollection("messages")
+                    .find()
+                    .first();
+
+                assertThat(document).containsEntry("message", "init script ran");
+            }
+        }
+    }
+
+    @Test
+    void shouldPreserveCustomWaitStrategy() {
+        WaitStrategy customWaitStrategy = Wait
+            .forLogMessage("(?i).*waiting for connections.*", 2)
+            .withStartupTimeout(Duration.ofMinutes(3));
+        try (TestableMongoDBContainer mongoDBContainer = new TestableMongoDBContainer("mongo:4.0.10")) {
+            mongoDBContainer
+                .withCopyFileToContainer(
+                    MountableFile.forClasspathResource("mongo-init.js"),
+                    "/docker-entrypoint-initdb.d/mongo-init.js"
+                )
+                .waitingFor(customWaitStrategy);
+
+            mongoDBContainer.start();
+            assertThat(mongoDBContainer.waitStrategy()).isSameAs(customWaitStrategy);
+        }
+    }
+
+    private static class TestableMongoDBContainer extends MongoDBContainer {
+
+        TestableMongoDBContainer(String dockerImageName) {
+            super(dockerImageName);
+        }
+
+        WaitStrategy waitStrategy() {
+            return getWaitStrategy();
         }
     }
 }
